aa18ae9756b5af1ea3dabb2117bfbd9e596bd992
Kavf.Mukola <Kavf.Mukola@a0fdb904-a088-11dd-a874-8d3a81574e67>
2008-12-03 06:13:27 +0000

    1.Insert values to fxTables 2.Add business objects to permissions 3.Delete unused stored procedures 4.Use Lookup method

--- trunk/DataModel/HttpHandlers/PageHandler.cs
+++ trunk/DataModel/HttpHandlers/PageHandler.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using IUDICO.DataModel.DB;
 
 namespace IUDICO.DataModel.HttpHandlers
 {
@@ -8,19 +10,34 @@ namespace IUDICO.DataModel.HttpHandlers
         private const string urlRegexGroup = "url";
         private static readonly string imageFileRequest = string.Format("DisplayImage.iif?{0}=", imageIdRequestParameter);
 
-        protected static string changeImageUrl(string pageText, int pageRef)
+        protected static string changeImageUrl(string pageText, TblPages page)
         {
             var imageUrlRegex = new Regex(string.Format(@"src=""(?<{0}>(\w+.files/(?<{1}>\w+.\w+)))""", urlRegexGroup, imageRegexGroup));
             MatchCollection matches = imageUrlRegex.Matches(pageText);
             foreach (Match m in matches)
             {
                 string imageName = m.Groups[imageRegexGroup].Value;
-               // int fileId = DaoFactory.FilesDao.SelectId(pageRef, imageName);
-               // string newUrl = string.Concat(imageFileRequest, fileId);
-               // pageText = pageText.Replace(m.Groups[urlRegexGroup].Value, newUrl);
+                var list = ServerModel.DB.LookupIds<TblFiles>(page);
+                var files = ServerModel.DB.Load<TblFiles>(list);
+
+                
+                string newUrl = string.Concat(imageFileRequest, FindFileId(files, imageName));
+                pageText = pageText.Replace(m.Groups[urlRegexGroup].Value, newUrl);
             }
 
             return pageText;
+        }
+
+        private static int FindFileId(IEnumerable<TblFiles> files, string imageName)
+        {
+            foreach (var file in files)
+            {
+                if(file.Name == imageName)
+                {
+                    return file.ID;
+                }
+            }
+            return 0;
         }
     }
 }
