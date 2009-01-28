92eb4906756c467795a75e5eef0e49c052d71269
YaremkoMicha <YaremkoMicha@a0fdb904-a088-11dd-a874-8d3a81574e67>
2009-01-28 16:49:28 +0000

    Curriculum edit page.

---
+++ trunk/Site/CurriculumEdit.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using IUDICO.DataModel.Controllers;
+using IUDICO.DataModel;
+
+public partial class CurriculumEdit : ControlledPage<CurriculumEditController>
+{
+    protected override void BindController(CurriculumEditController c)
+    {
+        base.BindController(c);
+
+        #region Controls binding
+        c.CourseTree = TreeView_Courses;
+        c.CurriculumTree = TreeView_Curriculums;
+
+        c.CreateCurriculumButton = Button_CreateCurriculum;
+        c.CreateStageButton = Button_AddStage;
+        c.AddThemeButton = Button_AddTheme;
+        c.DeleteButton = Button_Delete;
+        c.ModifyButton = Button_Modify;
+
+        c.NameTextBox = TextBox_Name;
+        c.DescriptionTextBox = TextBox_Description;
+        #endregion
+
+        Load += c.PageLoad;   
+    }
+
+
+
+
+}
