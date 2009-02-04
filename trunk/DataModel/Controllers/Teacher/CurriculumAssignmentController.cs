4e34f5f18408da6c07e88767de7207fedf10f334
YaremkoMicha <YaremkoMicha@a0fdb904-a088-11dd-a874-8d3a81574e67>
2009-02-04 14:18:23 +0000

    Some minor issues fixed.
    Also uploading nUnit libraries, for testing.

--- trunk/DataModel/Controllers/Teacher/CurriculumAssignmentController.cs
+++ trunk/DataModel/Controllers/Teacher/CurriculumAssignmentController.cs
@@ -23,30 +23,72 @@ namespace IUDICO.DataModel.Controllers
         public ListBox CurriculumsListBox { get; set; }
         public ListBox GroupsListBox { get; set; }
 
+        public Label NotifyLabel { get; set; }
+
+        [PersistantField]
+        private bool curriculumToGroupView = true;
+
         public void PageLoad(object sender, EventArgs e)
         {
             AssignButton.Click += new EventHandler(AssignButton_Click);
-
+            SwitchViewButton.Click += new EventHandler(SwitchViewButton_Click);
             if (!(sender as Page).IsPostBack)
             {
                 fillGroupsList();
                 fillCurriculumsList();
+                fillAssigmentsTree();
             }
+        }
+
+        void SwitchViewButton_Click(object sender, EventArgs e)
+        {
+            curriculumToGroupView = !curriculumToGroupView;
+            fillAssigmentsTree();
         }
 
         void AssignButton_Click(object sender, EventArgs e)
         {
-            if (GroupsListBox.SelectedValue != null
-                && CurriculumsListBox.SelectedValue != null)
+            if (GroupsListBox.SelectedValue != ""
+                && CurriculumsListBox.SelectedValue != "")
             {
                 int groupID = int.Parse(GroupsListBox.SelectedValue);
                 int curriculumID = int.Parse(CurriculumsListBox.SelectedValue);
+                TblCurriculums curriculum = ServerModel.DB.Load<TblCurriculums>(curriculumID);
+                TblGroups group = ServerModel.DB.Load<TblGroups>(groupID);
 
-                PermissionsManager.Grand(
-                    ServerModel.DB.Load<TblCurriculums>(curriculumID),
-                    FxCurriculumOperations.View
+                PermissionsManager.Grand(curriculum, FxCurriculumOperations.View
                     , null, groupID, DateTimeInterval.Full);
 
+                if (curriculumToGroupView)
+                {
+                    foreach (IdendtityNode curriculumNode in AssigmentsTree.Nodes)
+                    {
+                        if (curriculumNode.ID == curriculumID)
+                        {
+                            IdendtityNode groupNode = new IdendtityNode(group.Name, group.ID);
+                            curriculumNode.ChildNodes.Add(groupNode);
+                            curriculumNode.Expand();
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (IdendtityNode groupNode in AssigmentsTree.Nodes)
+                    {
+                        if (groupNode.ID == groupID)
+                        {
+                            IdendtityNode curriculumNode = new IdendtityNode(curriculum.Name, curriculum.ID);
+                            groupNode.ChildNodes.Add(curriculumNode);
+                            groupNode.Expand();
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                NotifyLabel.Text = "Select a group and curriculum to assign.";
             }
         }
 
@@ -58,25 +100,55 @@ namespace IUDICO.DataModel.Controllers
                 ListItem groupItem = new ListItem(group.Name, group.ID.ToString());
                 GroupsListBox.Items.Add(groupItem);
             }
+            if (GroupsListBox.Items.Count == 0)
+            {
+                NotifyLabel.Text = "You have no groups, create some first.";
+            }
         }
 
         private void fillCurriculumsList()
         {
             CurriculumsListBox.Items.Clear();
+
+            IList<int> myCurriculumsIDs = PermissionsManager.GetObjectsForUser(SECURED_OBJECT_TYPE.CURRICULUM,
+                ServerModel.User.Current.ID, FxCurriculumOperations.Use.ID, DateTime.Now);
+
+            //foreach (TblCurriculums curriculum in ServerModel.DB.Load<TblCurriculums>(myCurriculumsIDs))
             foreach (TblCurriculums curriculum in ServerModel.DB.Query<TblCurriculums>(null))
             {
                 ListItem curriculumItem = new ListItem(curriculum.Name, curriculum.ID.ToString());
                 CurriculumsListBox.Items.Add(curriculumItem);
+            }
+            if (CurriculumsListBox.Items.Count == 0)
+            {
+                NotifyLabel.Text = "You have no curriculums, create some first.";
             }
         }
 
         private void fillAssigmentsTree()
         {
             AssigmentsTree.Nodes.Clear();
-            foreach (TblCurriculums curriculum in ServerModel.DB.Query<TblCurriculums>(null))
+            if (curriculumToGroupView)
             {
-                TreeNode curriculumNode = new TreeNode(curriculum.Name, curriculum.ID.ToString());
-                //CurriculumsListBox.Items.Add(curriculumItem);
+                IList<int> myCurriculumsIDs = PermissionsManager.GetObjectsForUser(SECURED_OBJECT_TYPE.CURRICULUM,
+                ServerModel.User.Current.ID, FxCurriculumOperations.Use.ID, DateTime.Now);
+
+                //foreach (TblCurriculums curriculum in ServerModel.DB.Load<TblCurriculums>(myCurriculumsIDs))
+                foreach (TblCurriculums curriculum in ServerModel.DB.Query<TblCurriculums>(null))
+                {
+                    TreeNode curriculumNode = new IdendtityNode(curriculum.Name, curriculum.ID);
+                    AssigmentsTree.Nodes.Add(curriculumNode);
+                    //get groups for currics
+                }
+            }
+            else
+            {
+                foreach (TblGroups group in ServerModel.DB.Query<TblGroups>(null))
+                {
+                    TreeNode groupNode = new IdendtityNode(group.Name, group.ID);
+                    AssigmentsTree.Nodes.Add(groupNode);
+                    //get currics for groups
+                }
             }
         }
 
--- trunk/Site/Teacher/CurriculumAssignment.aspx
+++ trunk/Site/Teacher/CurriculumAssignment.aspx
@@ -1,7 +1,10 @@
 ﻿<%@ Page Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true"
     CodeFile="CurriculumAssignment.aspx.cs" Inherits="CurriculumAssignment" %>
 
+<%@ Register TagPrefix="iudico" Namespace="IUDICO.DataModel.Controllers" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="Server">
+    <asp:Label ID="Label_Notify" runat="server" Style="position: absolute; top: 60px;
+        left: 0px;"></asp:Label>
     <asp:ListBox ID="ListBox_Groups" runat="server" Style="position: absolute; top: 140px;
         left: 250px;"></asp:ListBox>
     <asp:ListBox ID="ListBox_Curriculums" runat="server" Style="position: absolute; top: 140px;
@@ -10,11 +13,11 @@
         left: 400px;" Text="My Assigments"></asp:Label>
     <asp:Button ID="Button_Unsign" runat="server" Style="position: absolute; top: 340px;
         left: 130px;" Text="Unsign" />
-    <asp:TreeView ID="TreeView_Assigments" runat="server" Style="position: absolute;
-        top: 140px; left: 400px;" ShowCheckBoxes="Leaf">
-    </asp:TreeView>
-    <asp:Label ID="Label_Curriculums" runat="server" Style="position: absolute; top: 120px; left: 0px;"
-        Text="My Curriculums"></asp:Label>
+    <iudico:IdentityTreeView ID="TreeView_Assigments" runat="server" Style="position: absolute;
+        top: 140px; left: 400px;">
+    </iudico:IdentityTreeView>
+    <asp:Label ID="Label_Curriculums" runat="server" Style="position: absolute; top: 120px;
+        left: 0px;" Text="My Curriculums"></asp:Label>
     <asp:Label ID="Label_Groups" runat="server" Style="position: absolute; top: 120px;
         left: 250px;" Text="My Groups"></asp:Label>
     <asp:Button ID="Button_Assign" runat="server" Style="position: absolute; top: 300px;
