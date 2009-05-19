fea4257730cd5a41dffe2a0352f7e2781f5f6c06
kavf.mukola <kavf.mukola@a0fdb904-a088-11dd-a874-8d3a81574e67>
2009-05-19 02:41:46 +0000

    HotFix

---
+++ TestingSystem/TestingSystem/src/Language.cs
@@ -0,0 +1,38 @@
+﻿namespace TestingSystem
+{
+    /// <summary>
+    /// Represents program language.
+    /// </summary>
+    public enum Language
+    {
+        /// <summary>
+        /// Microsoft Visual Studio 6 c++ compiler.
+        /// </summary>
+        Vs6CPlusPlus,
+
+        /// <summary>
+        /// Microsoft Visual Studio 8 (2005) c++ compiler.
+        /// </summary>
+        Vs8CPlusPlus,
+
+        /// <summary>
+        /// Microsoft .NET 2.0 compiler.
+        /// </summary>
+        DotNet2,
+
+        /// <summary>
+        /// Microsoft .NET 3.5 compiler.
+        /// </summary>
+        DotNet3,
+
+        /// <summary>
+        /// Sun's java6 compiler.
+        /// </summary>
+        Java6,
+
+        /// <summary>
+        /// Delphi 7 compiler.
+        /// </summary>
+        Delphi7,
+    };
+}
