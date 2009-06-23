044763d953fb5bfa8db7134e28762bcf3a0fd444
Kavf.Mukola <Kavf.Mukola@a0fdb904-a088-11dd-a874-8d3a81574e67>
2009-06-23 00:16:51 +0000

    Replace HttpHandlers with generic HttpHandlers. And move them to Site

---
+++ DataModel/Common/StudentUtils/ImageHandlerHelper.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Web;
+using IUDICO.DataModel.DB;
+
+namespace IUDICO.DataModel.Common.StudentUtils
+{
+    public class ImageHandlerHelper
+    {
+        private const string ImageRegexGroup = "image";
+        private const string UrlRegexGroup = "url";
+        private const string FolderRegexGroup = "folder";
+        private const string ImageIdRequestParameter = "imageId";
+        private const string ImageFileRequest = HandlerPaths.ImageHandlerPath + "?" + ImageIdRequestParameter + "=";
+
+        public static string ChangeImageUrl(string pageText, TblPages page)
+        {
+            var imageUrlRegex = new Regex(string.Format(@"src=""(?<{0}>(?<{1}>\w+.files)/(?<{2}>\w+.\w+))""", UrlRegexGroup, FolderRegexGroup, ImageRegexGroup));
+            MatchCollection matches = imageUrlRegex.Matches(pageText);
+            foreach (Match m in matches)
+            {
+                string imageName = m.Groups[ImageRegexGroup].Value;
+                string folderName = m.Groups[FolderRegexGroup].Value;
+
+                var list = ServerModel.DB.LookupIds<TblFiles>(page, null);
+                var files = ServerModel.DB.Load<TblFiles>(list);
+
+
+                string newUrl = string.Concat(ImageFileRequest, FindFileId(files, folderName, imageName));
+                pageText = pageText.Replace(m.Groups[UrlRegexGroup].Value, newUrl);
+            }
+
+            return pageText;
+        }
+
+        public static int GetImageIdFromRequest(HttpRequest request)
+        {
+            return int.Parse(request[ImageIdRequestParameter]);
+        }
+
+        private static int FindFolderId(IEnumerable<TblFiles> files, string folderName)
+        {
+            foreach (var folder in files)
+            {
+                if (folder.Name == folderName && (bool)folder.IsDirectory)
+                {
+                    return folder.ID;
+                }
+            }
+            return 0;
+        }
+
+        private static int FindFileId(IEnumerable<TblFiles> files, string folderName, string imageName)
+        {
+            int folderId = FindFolderId(files, folderName);
+
+            foreach (var file in files)
+            {
+                if (file.Name == imageName && file.PID == folderId)
+                {
+                    return file.ID;
+                }
+            }
+            return 0;
+        }
+    }
+}
--- Site/HttpHandlers/IudicoImageHandler.ashx
+++ Site/HttpHandlers/IudicoImageHandler.ashx
@@ -1,23 +1,28 @@
-﻿using System.IO;
+﻿<%@ WebHandler Language="C#" Class="IudicoImageHandler" %>
+
+using System.IO;
 using System.Web;
 using IUDICO.DataModel.Common.StudentUtils;
+using IUDICO.DataModel;
 using IUDICO.DataModel.DB;
 
-namespace IUDICO.DataModel.HttpHandlers
-{
-    public class ImageHandler : IHttpHandler
+
+    public class IudicoImageHandler : IHttpHandler
     {
-        public void ProcessRequest(HttpContext context)
+    
+        public void ProcessRequest (HttpContext context)
         {
-            var imageFileId = int.Parse(context.Request[TestControlHelper.ImageIdRequestParameter]);
+            var imageFileId = ImageHandlerHelper.GetImageIdFromRequest(context.Request);
             var files = ServerModel.DB.Load<TblFiles>(imageFileId);
             context.Response.ContentType = Path.GetExtension(files.Name);
             context.Response.OutputStream.Write(files.File.ToArray(), 0, files.File.Length);
         }
-
+ 
         public bool IsReusable
         {
-            get { return false; }
+            get
+            {
+                return false;
+            }
         }
-    }
-}
+   }
