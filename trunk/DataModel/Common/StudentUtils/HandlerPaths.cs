64a1180a51f0aabe282a218a9987d11e334c4015
Kavf.Mukola <Kavf.Mukola@a0fdb904-a088-11dd-a874-8d3a81574e67>
2009-06-23 00:16:51 +0000

    Replace HttpHandlers with generic HttpHandlers. And move them to Site

---
+++ trunk/DataModel/Common/StudentUtils/HandlerPaths.cs
@@ -0,0 +1,8 @@
+﻿namespace IUDICO.DataModel.Common.StudentUtils
+{
+    class HandlerPaths
+    {
+        public const string ImageHandlerPath = "/Site/HttpHandlers/IudicoImageHandler.ashx";
+        public const string TheoryHandlerPath = "/Site/HttpHandlers/IudicoTheoryHandler.ashx";
+    }
+}
