a4d4aeab764bc8e14a8e3f5737e7a4fcf5a947eb
ShVolodya <ShVolodya@a0fdb904-a088-11dd-a874-8d3a81574e67>
2009-07-01 08:24:30 +0000

    Console support for DBUpdater.

---
+++ trunk/Utils/DBUpdater/frmConnectionString.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace DBUpdater
+{
+    public partial class frmConnectionString : Form
+    {
+        public string ConnectionString
+        {
+            get
+            {
+                return _builder.ConnectionString;
+            }
+        }
+
+        public frmConnectionString()
+        {
+            InitializeComponent();
+
+            _builder = new SqlConnectionStringBuilder();
+            pgConnection.SelectedObject = _builder;
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+            try
+            {
+                _builder.ConnectionString = textBox1.Text;
+                pgConnection.Refresh();
+            }
+            catch
+            {
+            }
+        }
+
+        private void pgConnection_PropertyValueChanged(object s, PropertyValueChangedEventArgs e)
+        {
+            textBox1.Text = _builder.ConnectionString;
+        }
+
+        private readonly SqlConnectionStringBuilder _builder;
+    }
+}
---
+++ trunk/Utils/DBUpdater/frmDBConsole.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Windows.Forms;
+using IUDICO.DBManager;
+using LEX.CONTROLS;
+
+namespace DBUpdater
+{
+    public partial class frmDBConsole : Form
+    {
+        public frmDBConsole()
+        {
+            InitializeComponent();
+        }
+
+        public string ConnectionString { get; set; }
+
+        private void llRun_Click(object sender, EventArgs e)
+        {
+            rtbResult.Enabled = rtbSqlCommands.Enabled = llRun.Enabled = false;
+            UseWaitCursor = true;
+            try
+            {
+                var cmds = DBUpdateManager.SplitSqlCommands(rtbSqlCommands.Text);
+                var result = new StringBuilder();
+                using (var cn = new SqlConnection(ConnectionString))
+                {
+                    cn.Open();
+                    using (var cmd = cn.CreateCommand())
+                    {
+                        foreach (var c in cmds)
+                        {
+                            cmd.CommandText = c;
+                            using (var r = cmd.LexExecuteReader())
+                            {
+                                do
+                                {
+                                    ProcessReader(r, result);
+                                    result.AppendLine();
+                                    result.AppendLine();
+                                } while (r.NextResult());
+                            }
+                        }
+                    }
+                }
+                rtbResult.Text = result.ToString();
+            }
+            catch(Exception ex)
+            {
+                rtbResult.Text = ex.Message;
+            }
+            finally
+            {
+                rtbResult.Enabled = rtbSqlCommands.Enabled = llRun.Enabled = true;
+                UseWaitCursor = false;
+            }
+        }
+
+        private static void ProcessReader(IDataReader r, StringBuilder result)
+        {
+            if (r.RecordsAffected >= 0)
+            {
+                result.AppendLine("Records affected: " + r.RecordsAffected);
+                result.AppendLine();
+            }
+            else
+            {
+                for (var i = 0; i < r.FieldCount; ++i)
+                {
+                    result.Append('[');
+                    result.Append(r.GetName(i));
+                    result.Append("] ");
+                }
+                result.AppendLine();
+
+                while (r.Read())
+                {
+                    for (var i = 0; i < r.FieldCount; ++i)
+                    {
+                        result.Append(r.GetValue(i));
+                        result.Append(' ');
+                    }
+                    result.AppendLine();
+                }
+            }
+            
+        }
+    }
+}
