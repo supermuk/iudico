e4c853119783f80a2f9ed6b2cf86ce4d02f8b949
galamagal86 <galamagal86@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-03-01 10:33:53 +0000

    не то заляв

--- course-editor/Common/Consts.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FireFly.CourseEditor.Common
-{
-    internal static class Consts
-    {
-        internal const string STRING_EDITOR_TYPE_NAME = "System.Windows.Forms.Design.ListControlStringCollectionEditor, System.Design, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a";
-    }
-}
--- course-editor/Common/Delegates.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Windows.Forms;
-
-namespace FireFly.CourseEditor.Common
-{
-    public delegate DialogResult ShowErrorDelegate([NotNull]IWin32Window p1, [NotNull]string p2, [NotNull]string p3, MessageBoxButtons p4, MessageBoxIcon p5);
-}
