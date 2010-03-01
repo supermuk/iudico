742e17b2881107605555935e4de915a05d97bc1d
galamagal86 <galamagal86@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-03-01 09:20:47 +0000

    :)

---
+++ course-editor/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.1.52.118")]
+[assembly: AssemblyFileVersion("1.1.52.18")]
+
+[assembly : AssemblyTitle("FireFly Course Editor")]
+[assembly : AssemblyDescription("")]
+[assembly : AssemblyConfiguration("")]
+[assembly : AssemblyCompany("")]
+[assembly : AssemblyProduct("CourseEditor")]
+[assembly : AssemblyCopyright("")]
+[assembly : AssemblyTrademark("")]
+[assembly : AssemblyCulture("")]
+
+[assembly : ComVisible(false)]
+[assembly : Guid("58506e44-d32a-44b5-9a1f-b14ffae63862")]
+                                      
+[assembly : FileDialogPermission(SecurityAction.RequestMinimum)]
+[assembly : UIPermission(SecurityAction.RequestMinimum)]
+[assembly : EnvironmentPermission(SecurityAction.RequestMinimum)]
+[assembly : ReflectionPermission(SecurityAction.RequestMinimum, ReflectionEmit=false, MemberAccess = true)]
+[assembly : RegistryPermission(SecurityAction.RequestRefuse)]
---
+++ course-editor/Properties/Settings.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using System.Text;
+using FireFly.CourseEditor.GUI;
+using FireFly.CourseEditor.Common;
+
+namespace FireFly.CourseEditor.Properties
+{
+    internal partial class Settings
+    {
+        public LastCoursesXml LastCoursesXml
+        {
+            get
+            {
+                var lc = LastCourses;
+                if (lc.IsNotNull())
+                {
+                    return (LastCoursesXml)LastCoursesXml.Serializer.Deserialize(new StringReader(lc));
+                }
+                return new LastCoursesXml();
+            }
+            set
+            {
+                if (value == null || value.Count == 0)
+                {
+                    LastCourses = null;
+                }
+                else
+                {
+                    var sb = new StringBuilder();
+                    LastCoursesXml.Serializer.Serialize(new StringWriter(sb), value);
+                    LastCourses = sb.ToString();
+                }
+            }
+        }
+    }
+}
