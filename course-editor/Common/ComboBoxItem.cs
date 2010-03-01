46e5df158782a17fe913d49761eb05a7cb1cfd08
galamagal86 <galamagal86@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-03-01 10:38:08 +0000

    move from googlecode course-editor Revision: r29
    Course/LMSIntf.js: Little changes. Not significant, just for beauty:)
    
    Course/Manifest/Item.cs: Removed IsVisibleSpecified Field as it has no meaning
    and is out of the SCORM 2004 4ED
    
    Course/Manifest/ManifestTypes.cs: Added full descriptions to sequencing
    elements. Code formatted. Fixed few bugs with sequencing. Adeed
    <sequencingCollection> element.
    
    Course/Manifest/PosibilityManager.cs: Added  3 new seqencing elements
    maintaince.
    
    Course/SCOObj.js: Added navigation request "continue" after submit button
    pressed.
    
    Course/SequencingManager.cs: Added Theory pageType customizing.
    
    GUI/HtmlEditor/HtmlPage.cs: Set passRank default value to 1.
    
    GUI/HtmlEditor/HtmlEditor/HtmlTestControl.cs: Set Rank default value to 1.
    
    GUI/HtmlEditor/SummaryPage.cs: Fixed little redundancy.
    
    GUI/ManifestBrowser.cs & co.: Added maintenance of 3 new sequencing elements.
    
    !!! 1 element (extended objectives) still needs to be implemented!!!

---
+++ course-editor/Common/ComboBoxItem.cs
@@ -0,0 +1,21 @@
+﻿using FireFly.CourseEditor.Course.Manifest;
+
+namespace FireFly.CourseEditor.Common
+{
+    internal class ComboBoxItem
+    {
+        public ComboBoxItem([NotNull]ITitled value)
+        {
+            Value = value;
+        }
+
+        [NotNull]
+        public override string ToString()
+        {
+            return Value.Title;
+        }
+
+        [NotNull]
+        public readonly ITitled Value;
+    }
+}
---
+++ course-editor/Common/FileUtils.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+
+namespace FireFly.CourseEditor.Common
+{
+    public static class FileUtils
+    {
+        /// <summary>
+        /// This methods should be used instead of <see cref="File.ReadAllText(string)"/> because of encoding should be figgured out automatically
+        /// </summary>
+        /// <param name="path">Path to file</param>
+        /// <returns>String content of the specified file</returns>
+        [NotNull]
+        public static  string ReadAllText([NotNull] string path)
+        {
+            using (var sr = new StreamReader(path))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+
+        public static void WriteAllText([NotNull] string path, [NotNull] string content)
+        {
+            File.WriteAllText(path, content, Encoding.Unicode);
+        }
+    }
+}
