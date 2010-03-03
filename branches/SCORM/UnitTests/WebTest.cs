6956936bf28e6e65676bda8b68e38fec2e9f292a
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-03-03 22:38:33 +0000

    Task 22

---
+++ branches/SCORM/UnitTests/WebTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using IUDICO.UnitTest.Base;
+
+namespace IUDICO.UnitTest
+{
+    [TestFixture]
+    public class WebTest: TestFixtureWeb
+    {
+        public void Test001()
+        {
+            Selenium.Open("http://localhost/TestPage1.htm");
+
+            Assert.AreEqual("Selenium Target Page",
+                            Selenium.GetTitle(),
+                            "Check the title of the browser");
+        }
+    }
+}
