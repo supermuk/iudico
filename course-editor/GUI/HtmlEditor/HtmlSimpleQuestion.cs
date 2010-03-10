54934260bc36a278358908379ffa30ae5985be17
yuriy.dobryanskyy <yuriy.dobryanskyy@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-03-10 19:20:15 +0000

    Feature: * Correct answers are now highlighted by /*BEG*/ and /*END*/ comments

--- course-editor/GUI/HtmlEditor/HtmlSimpleQuestion.cs
+++ course-editor/GUI/HtmlEditor/HtmlSimpleQuestion.cs
@@ -57,7 +57,7 @@ namespace FireFly.CourseEditor.GUI.HtmlEditor
 
         public override string GetScoTestInitializer()
         {
-            return string.Format("new complexTest('{0}','{1}')", Name, CorrectAnswer);
+            return string.Format("new complexTest('{0}',/*BEG*/'{1}'/*END*/)", Name, CorrectAnswer);
         }
 
         protected override Control CreateWindowControl()
--- course-editor/GUI/HtmlEditor/HtmlTextBox.cs
+++ course-editor/GUI/HtmlEditor/HtmlTextBox.cs
@@ -70,7 +70,7 @@ namespace FireFly.CourseEditor.GUI.HtmlEditor
 
         public override string GetScoTestInitializer()
         {
-            return string.Format("new simpleTest('{0}', '{1}')", Name, CorrectAnswer);
+            return string.Format("new simpleTest('{0}', /*BEG*/'{1}'/*END*/)", Name, CorrectAnswer);
         }
 
         protected override void Parse([NotNull]XmlNode node)
