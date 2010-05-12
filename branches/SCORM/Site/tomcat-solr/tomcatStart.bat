0f1085b35800f0b43ce503bfac120e138d414441
boblox12 <boblox12@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-05-12 21:20:26 +0000

    added some new data model and deleted tomcat-solr

--- branches/SCORM/Site/tomcat-solr/tomcatStart.bat
+++
@@ -1,4 +0,0 @@
-set CATALINA_HOME = F:\1rynka\Dyplom\Iudico_prj\Site\tomcat-solr\apache-tomcat-5.5.28
-set JAVA_HOME = C:\Program Files\Java\jdk1.6.0
-cd F:\1rynka\Dyplom\Iudico_prj\Site\tomcat-solr\solr
-F:\1rynka\Dyplom\Iudico_prj\Site\tomcat-solr\apache-tomcat-5.5.28\bin\startup.bat
