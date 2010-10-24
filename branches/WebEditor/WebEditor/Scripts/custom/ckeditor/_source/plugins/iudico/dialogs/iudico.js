6c5902afd96aaf72d356d454c237b455df4d4fa8
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-10-24 21:23:58 +0000

    web editor: - ckeditor - ckeditor iudico plugin

---
+++ branches/WebEditor/WebEditor/Scripts/custom/ckeditor/_source/plugins/iudico/dialogs/iudico.js
@@ -0,0 +1,29 @@
+﻿CKEDITOR.dialog.add('iudico', function (editor) {
+    //var lang = editor.lang.about;
+
+    return {
+        title: 'IUDICO',
+        minWidth: 390,
+        minHeight: 230,
+        contents: [
+			{
+			    id: 'tab1',
+			    label: '',
+			    title: 'Question',
+			    expand: true,
+			    padding: 0,
+			    elements:
+				[
+					{
+					    type: 'html',
+					    html:
+                            '<div class="cke_iudico_container">' +
+								
+                            '</div>'
+					}
+				]
+			}
+		],
+	    buttons: [CKEDITOR.dialog.okButton, CKEDITOR.dialog.cancelButton]
+    };
+});
---
+++ branches/WebEditor/WebEditor/Scripts/custom/ckeditor/_source/plugins/iudico/plugin.js
@@ -0,0 +1,17 @@
+﻿CKEDITOR.plugins.add('iudico-question',
+{
+    requires : [ 'dialog' ],
+    init: function (editor) {
+        var pluginName = 'iudico';
+                
+        CKEDITOR.dialog.add(pluginName, this.path + 'dialogs/iudico.js'); 
+        var command = editor.addCommand(pluginName, new CKEDITOR.dialogCommand(pluginName));
+        command.modes = { wysiwyg:1, source:1 };
+
+        editor.ui.addButton('Test1', 
+        {
+            label: 'Test1',
+            command: pluginName,
+        });
+    }
+});
