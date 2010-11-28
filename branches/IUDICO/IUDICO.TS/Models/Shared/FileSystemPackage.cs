1c3eb38e422dfc590fbeadff9797d521c6479bdf
shopen21 <shopen21@gmail.com>
2010-11-28 13:29:29 +0000

    added package, refactored old package to training model. added few MLC supporting method, helpers, etc.

---
+++ branches/IUDICO/IUDICO.TS/Models/Shared/FileSystemPackage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.LearningComponents;
+using System.IO;
+
+namespace IUDICO.TS.Models.Shared
+{
+    public class FileSystemPackage : Package
+    {
+        #region Properties
+
+        public string FolderPath { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public FileSystemPackage(string folderPath)
+            : base()
+        {
+            this.FolderPath = folderPath;
+        }
+
+        public FileSystemPackage(string folderPath, long? ownerID, DateTime? uploadDateTime, string fileName)
+            : base(ownerID, uploadDateTime, fileName)
+        {
+            this.FolderPath = folderPath;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override PackageReader GetPackageReader()
+        {
+            FileSystemPackageReader reader = new FileSystemPackageReader(this.FolderPath);
+            return reader;
+        }
+
+        #endregion
+    }
+}
