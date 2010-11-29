7b594ff6ecfbd0c29fb18adb0c84fd5bf36b2c5e
sm.lpml@gmail.com <sm.lpml@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-11-29 22:04:10 +0000

    Added Manifest classes.

---
+++ branches/IUDICO/IUDICO.CourseMgt/Models/Manifest/Sequencing.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace IUDICO.CourseMgt.Models.Manifest
+{
+    [Serializable]
+    public class Sequencing
+    {
+    }
+}
