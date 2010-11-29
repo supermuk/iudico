084256ad746b1aa02cbcfcca49e2ecef14d0df47
shopen21 <shopen21@gmail.com>
2010-11-29 18:51:38 +0000

    added simple try-catch to show Error view. removed redundant Course.aspx view from IUDICO.LMS

--- IUDICO.TS/Controllers/TrainingController.cs
+++ IUDICO.TS/Controllers/TrainingController.cs
@@ -15,42 +15,75 @@ namespace IUDICO.TS.Controllers
 
         public ActionResult Index()
         {
-            IEnumerable<Training> trainings = MlcDataProvider.Instance.GetTrainings(1);
-            
-            return View(trainings);
+            try
+            {
+                IEnumerable<Training> trainings = MlcDataProvider.Instance.GetTrainings(1);
+
+                return View(trainings);
+            }
+            catch (Exception exc)
+            {
+                return View("Error", new HandleErrorInfo(exc, "Training", "Index"));
+            }
         }
 
         public ActionResult Play(long id)
         {
-
-            // TODO: redirect to frameset.
-            return new RedirectResult("http://localhost:1339/BasicWebPlayer/Frameset/Frameset.aspx?View=0&AttemptId=" + id.ToString());
+            try
+            {
+                // TODO: redirect to frameset.
+                return new RedirectResult("http://localhost:1339/BasicWebPlayer/Frameset/Frameset.aspx?View=0&AttemptId=" + id.ToString());
+            }
+            catch (Exception exc)
+            {
+                return View("Error", new HandleErrorInfo(exc, "Training", "Play"));
+            }
         }
 
         public ActionResult Create(long id)
         {
-            long attemptID = MlcDataProvider.Instance.CreateAttempt(id);
+            try
+            {
+                long attemptID = MlcDataProvider.Instance.CreateAttempt(id);
 
-            // TODO: redirect to frameset.
-            return View("Index");
+                // TODO: redirect to frameset.
+                return RedirectToAction("Index");
+            }
+            catch (Exception exc)
+            {
+                return View("Error", new HandleErrorInfo(exc, "Training", "Create"));
+            }
         }
 
         public ActionResult Add()
         {
-            ZipPackage package = new ZipPackage("C:\\ZipPackages\\scorm1.zip", 1, DateTime.Now, "scorm1.zip");
-            Training training = MlcDataProvider.Instance.AddPackage(package);
-            
-            //Show success page with detailed info.
-            return View("Details", training);
+            try
+            {
+                ZipPackage package = new ZipPackage("C:\\ZipPackages\\scorm1.zip", 1, DateTime.Now, "scorm1.zip");
+                Training training = MlcDataProvider.Instance.AddPackage(package);
+
+                //Show success page with detailed info.
+                return View("Details", training);
+            }
+            catch (Exception exc)
+            {
+                return View("Error", new HandleErrorInfo(exc, "Training", "Add"));
+            }
         }
 
         public ActionResult Details(long id)
         {
-            // TODO: provide user-based
-            IEnumerable<Training> trainings = MlcDataProvider.Instance.GetTrainings(1);
-            Training training = trainings.Single(tr => tr.PackageID == id);
-
-            return View("Details", training);
+            try
+            {
+                // TODO: provide user-based
+                IEnumerable<Training> trainings = MlcDataProvider.Instance.GetTrainings(1);
+                Training training = trainings.Single(tr => tr.PackageID == id);
+                return View("Details", training);
+            }
+            catch (Exception exc)
+            {
+                return View("Error", new HandleErrorInfo(exc, "Training", "Details"));
+            }
         }
     }
 }
