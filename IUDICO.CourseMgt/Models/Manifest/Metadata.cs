4a05c0fdf86fcfd057c33150909aa0fa43c3a9e0
sm.lpml <sm.lpml@gmail.com>
2010-11-29 22:04:10 +0000

    Added Manifest classes.

---
+++ IUDICO.CourseMgt/Models/Manifest/Metadata.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IUDICO.CourseMgt.Models.Manifest
+{
+    public class Metadata
+    {
+
+
+    }
+}
