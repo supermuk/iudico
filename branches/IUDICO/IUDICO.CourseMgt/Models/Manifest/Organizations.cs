44ca0635cf8b9fa63517f200b34e3e38bd51070f
sm.lpml <sm.lpml@gmail.com>
2010-11-29 22:04:10 +0000

    Added Manifest classes.

---
+++ branches/IUDICO/IUDICO.CourseMgt/Models/Manifest/Organizations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+namespace IUDICO.CourseMgt.Models.Manifest
+{
+    [Serializable]
+    public class Organizations
+    {
+        [XmlIgnore]
+        private Organization DefaultOrganization;
+
+        [XmlAttribute(Strings.Default)]
+        public string Default
+        {
+            get
+            {
+                return DefaultOrganization.Identifier;
+            }
+        }
+
+        [XmlElement(Strings.Organization, Namespace=Strings.ImscpNamespaceV1p3)]
+        public List<Organization> Organizations_;
+
+
+    }
+}
