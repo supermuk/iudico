74cfeeee2d3f96096d94e8891e433351903efc2b
ipetrovych <ipetrovych@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-08 20:00:22 +0000

    Created project for Statistics

---
+++ branches/IUDICO/IUDICO.Statistics/StatisticsRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using MvcContrib.PortableAreas;
+
+namespace IUDICO.Statistics
+{
+    public class StatisticsRegistration : PortableAreaRegistration
+    {
+        public override void RegisterArea(AreaRegistrationContext context, IApplicationBus bus)
+        {
+            context.MapRoute(
+                "Statistics",
+                "Statistics/{action}",
+                new { controller = "Statistics" }
+            );
+
+            RegisterAreaEmbeddedResources();
+        }
+
+        public override string AreaName
+        {
+            get { return "Statistics"; }
+        }
+    }
+}
