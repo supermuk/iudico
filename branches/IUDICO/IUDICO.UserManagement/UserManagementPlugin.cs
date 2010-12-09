cd717d802123adbbf0ddb8cc36ab8fdb1a922698
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-09 22:11:35 +0000

    add missing projects

---
+++ branches/IUDICO/IUDICO.UserManagement/UserManagementPlugin.cs
@@ -0,0 +1,60 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using IUDICO.Common.Models.Plugin;
+using IUDICO.UserManagement.Models.Storage;
+
+namespace IUDICO.UserManagement
+{
+    public class UserManagementPlugin : IWindsorInstaller, IPlugin
+    {
+        #region IWindsorInstaller Members
+
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+            container.Register(
+                AllTypes
+                    .FromThisAssembly()
+                    .BasedOn<IController>()
+                    .Configure(c => c.LifeStyle.Transient
+                                        .Named(c.Implementation.Name))//,
+                //Component.For<IPlugin>().ImplementedBy<CourseManagmentPlugin>().LifeStyle.Is(Castle.Core.LifestyleType.Singleton),
+                //Component.For<ICourseManagment>().ImplementedBy<MixedCourseStorage>().LifeStyle.Is(Castle.Core.LifestyleType.Singleton)
+            );
+
+            HttpContext.Current.Application["UMStorage"] = UMStorageFactory.CreateStorage(UMStorageType.Database);
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        public void RegisterRoutes(System.Web.Routing.RouteCollection routes)
+        {
+            routes.MapRoute(
+                "Account",
+                "Account/{action}",
+                new { controller = "Account" }
+            );
+
+            routes.MapRoute(
+                "Group",
+                "Group/{action}",
+                new { controller = "Group" }
+            );
+
+            routes.MapRoute(
+                "Role",
+                "Role/{action}",
+                new { controller = "Role" }
+            );
+        }
+
+        public void Update(string evt, params object[] data)
+        {
+            // handle events
+        }
+
+        #endregion
+    }
+}
