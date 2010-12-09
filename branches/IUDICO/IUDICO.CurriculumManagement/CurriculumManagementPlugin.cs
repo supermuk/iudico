369991d5c8eb091232aeaac885ca5d1d912e79fd
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-09 22:11:35 +0000

    add missing projects

---
+++ branches/IUDICO/IUDICO.CurriculumManagement/CurriculumManagementPlugin.cs
@@ -0,0 +1,74 @@
+﻿using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using IUDICO.Common.Models.Plugin;
+
+namespace IUDICO.CurriculumManagement
+{
+    public class CurriculumManagementPlugin : IWindsorInstaller, IPlugin
+    {
+        #region IWindsorInstaller Members
+
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+            container.Register(
+                AllTypes
+                    .FromThisAssembly()
+                    .BasedOn<IController>()
+                    .Configure(c => c.LifeStyle.Transient
+                                        .Named(c.Implementation.Name))//,
+                //Component.For<IPlugin>().ImplementedBy<CourseManagmentPlugin>().LifeStyle.Is(Castle.Core.LifestyleType.Singleton),
+                //Component.For<ICourseManagment>().ImplementedBy<MixedCourseStorage>().LifeStyle.Is(Castle.Core.LifestyleType.Singleton)
+            );
+        }
+
+        #endregion
+
+        #region IPlugin Members
+
+        public void RegisterRoutes(System.Web.Routing.RouteCollection routes)
+        {
+            routes.MapRoute(
+                "Curriculum",
+                "Curriculum/{CurriculumID}/{action}",
+                new { controller = "Curriculum" }
+            );
+
+            routes.MapRoute(
+                "Curriculums",
+                "Curriculum/{action}",
+                new { controller = "Curriculum", action = "Index" }
+            );
+
+            routes.MapRoute(
+               "Stage",
+               "Stage/{StageId}/{action}",
+               new { controller = "Stage" }
+            );
+
+            routes.MapRoute(
+                "Stages",
+                "Curriculum/{CurriculumId}/Stage/{action}",
+                new { controller = "Stage" }
+            );
+
+            routes.MapRoute(
+               "Theme",
+               "Theme/{ThemeId}/{action}",
+               new { controller = "Theme" }
+            );
+
+            routes.MapRoute(
+                "Themes",
+                "Stage/{StageId}/Theme/{action}",
+                new { controller = "Theme" }
+            );
+        }
+
+        public void Update(string evt, params object[] data)
+        {
+            // handle events
+        }
+
+        #endregion
+    }
+}
---
+++ branches/IUDICO/IUDICO.CurriculumManagement/Models/Storage/ICurriculumStorage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IUDICO.Common.Models;
+
+namespace IUDICO.CurriculumManagement.Models.Storage
+{
+    public interface ICurriculumStorage
+    {
+        #region Curriculum methods
+
+        IEnumerable<Curriculum> GetCurriculums();
+        IEnumerable<Curriculum> GetCurriculums(IEnumerable<int> ids);
+        Curriculum GetCurriculum(int id);
+        int AddCurriculum(Curriculum curriculum);
+        void UpdateCurriculum(Curriculum curriculum);
+        void DeleteCurriculum(int id);
+        void DeleteCurriculums(IEnumerable<int> ids);
+
+        #endregion
+
+        #region Stage methods
+
+        IEnumerable<Stage> GetStages(int curriculumId);
+        IEnumerable<Stage> GetStages(IEnumerable<int> ids);
+        int AddStage(Stage stage);
+        Stage GetStage(int id);
+        void UpdateStage(Stage stage);
+        void DeleteStage(int id);
+        void DeleteStages(IEnumerable<int> ids);
+
+        #endregion
+
+        #region Theme methods
+
+        IEnumerable<Theme> GetThemes(int stageId);
+        IEnumerable<Theme> GetThemes(IEnumerable<int> ids);
+        Theme GetTheme(int id);
+        int AddTheme(Theme theme);
+        void UpdateTheme(Theme theme);
+        void DeleteTheme(int id);
+        void DeleteThemes(IEnumerable<int> ids);
+        Theme ThemeUp(int themeId);
+        Theme ThemeDown(int themeId);
+
+        #endregion
+
+        #region Temporary
+
+        Course GetCourse(int id);
+        List<Course> GetCourses();
+
+        #endregion
+    }
+}
