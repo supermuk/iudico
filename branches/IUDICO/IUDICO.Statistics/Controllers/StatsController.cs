1936822004a61b8de681084ace1b12be876cd1c4
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-10 13:23:32 +0000

    Fixed problems with embedded resources Added Roman's Stats project

---
+++ branches/IUDICO/IUDICO.Statistics/Controllers/StatsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IUDICO.Common.Controllers;
+
+namespace IUDICO.Statistics.Controllers
+{
+    public class StatsController : PluginController
+    {
+        //
+        // GET: /Stats/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
