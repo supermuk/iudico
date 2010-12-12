a42d82691696c118f664c78595d2e2f30fd66f0c
sm.lpml <sm.lpml@gmail.com>
2010-12-12 15:24:33 +0000

    CourseManagement project. Namespaces renamed.

---
+++ branches/IUDICO/IUDICO.CourseManagment/Models/ManifestModels/OrganizationModels/Map.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace IUDICO.CourseManagement.Models.ManifestModels.OrganizationModels
+{
+    [Serializable]
+    public class Map
+    {
+        [XmlAttribute("targetID")]
+        public string TargetID;
+
+        [XmlAttribute("readSharedData")]
+        public string readSharedData; // = "true";
+
+        [XmlAttribute("writeSharedData")]
+        public string WriteSharedData; // = "true";
+    }
+}
