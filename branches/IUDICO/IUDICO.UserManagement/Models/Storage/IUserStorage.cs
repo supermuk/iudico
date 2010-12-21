70c968bd2943953dfec5be5bb8ae8589b1b6f8ce
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-21 19:57:59 +0000

    adding missing files

---
+++ branches/IUDICO/IUDICO.UserManagement/Models/Storage/IUserStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IUDICO.Common.Models;
+
+namespace IUDICO.UserManagement.Models.Storage
+{
+    public interface IUserStorage
+    {
+        #region Role methods
+
+        IEnumerable<Role> GetRoles();
+        bool CreateRole(Role role);
+        Role GetRole(int id);
+        bool EditRole(int id, Role role);
+        bool DeleteRole(int id);
+
+        #endregion
+
+        #region User methods
+
+
+
+        #endregion
+
+        #region Group Methods
+
+        IEnumerable<Group> GetGroups();
+        bool CreateGroup(Group group);
+        Group GetGroup(int id);
+        bool EditGroup(int id, Group group);
+        bool DeleteGroup(int id);
+
+        #endregion
+    }
+}
