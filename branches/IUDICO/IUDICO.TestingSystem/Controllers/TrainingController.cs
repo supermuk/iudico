55dfde5eb59015add9d2ee329e52b7f811599042
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-21 21:41:43 +0000

    removed try... catch... from TrainingController

--- branches/IUDICO/IUDICO.TestingSystem/Controllers/TrainingController.cs
+++ branches/IUDICO/IUDICO.TestingSystem/Controllers/TrainingController.cs
@@ -16,87 +16,52 @@ namespace IUDICO.TestingSystem.Controllers
 
         public ActionResult Index()
         {
-            try
-            {
-                IEnumerable<Training> trainings = MlcDataProvider.Instance.GetTrainings(1);
+            IEnumerable<Training> trainings = MlcDataProvider.Instance.GetTrainings(1);
                 
-                return View(trainings);
-            }
-            catch (Exception exc)
-            {
-                return View("Error", new HandleErrorInfo(exc, "Training", "Index"));
-            }
+            return View(trainings);
         }
 
         public ActionResult Play(long id)
         {
-            try
-            {
-                // TODO: redirect to frameset.
-                return new RedirectResult("http://localhost:1339/BasicWebPlayer/Frameset/Frameset.aspx?View=0&AttemptId=" + id.ToString());
-            }
-            catch (Exception exc)
-            {
-                return View("Error", new HandleErrorInfo(exc, "Training", "Play"));
-            }
+            // TODO: redirect to frameset.
+            return new RedirectResult("http://localhost:1339/BasicWebPlayer/Frameset/Frameset.aspx?View=0&AttemptId=" + id.ToString());
         }
 
         public ActionResult Create(long id)
         {
-            try
-            {
-                var attemptId = MlcDataProvider.Instance.CreateAttempt(id);
+            var attemptId = MlcDataProvider.Instance.CreateAttempt(id);
 
-                // TODO: redirect to frameset.
-                return RedirectToAction("Index");
-            }
-            catch (Exception exc)
-            {
-                return View("Error", new HandleErrorInfo(exc, "Training", "Create"));
-            }
+            // TODO: redirect to frameset.
+            return RedirectToAction("Index");
         }
 
         public ActionResult Add()
         {
-            try
-            {
-                var package = new ZipPackage("C:\\ZipPackages\\scorm1.zip", 1, DateTime.Now, "scorm1.zip");
-                var training = MlcDataProvider.Instance.AddPackage(package);
+            var package = new ZipPackage("C:\\ZipPackages\\scorm1.zip", 1, DateTime.Now, "scorm1.zip");
+            var training = MlcDataProvider.Instance.AddPackage(package);
 
-                //Show success page with detailed info.
-                return View("Details", training);
-            }
-            catch (Exception exc)
-            {
-                return View("Error", new HandleErrorInfo(exc, "Training", "Add"));
-            }
+            //Show success page with detailed info.
+            return View("Details", training);
         }
 
         public ActionResult Details(long packageId, long attemptId = -1)
         {
-            try
+            // TODO: provide user-based
+            var trainings = MlcDataProvider.Instance.GetTrainings(1);
+            var trainingsById = trainings.Where(tr => tr.PackageId == packageId);
+
+            Training training;
+
+            if (attemptId > 0 && trainingsById.Count() > 1)
             {
-                // TODO: provide user-based
-                var trainings = MlcDataProvider.Instance.GetTrainings(1);
-                var trainingsById = trainings.Where(tr => tr.PackageId == packageId);
-
-                Training training;
-
-                if (attemptId > 0 && trainingsById.Count() > 1)
-                {
-                    training = trainingsById.SingleOrDefault(tr => tr.AttemptId == attemptId);
-                }
-                else
-                {
-                    training = trainingsById.First();
-                }
-
-                return View("Details", training);
+                training = trainingsById.SingleOrDefault(tr => tr.AttemptId == attemptId);
             }
-            catch (Exception exc)
+            else
             {
-                return View("Error", new HandleErrorInfo(exc, "Training", "Details"));
+                training = trainingsById.First();
             }
+
+            return View("Details", training);
         }
     }
 }
