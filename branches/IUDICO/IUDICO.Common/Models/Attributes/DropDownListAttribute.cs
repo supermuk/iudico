7efc132e5d8f258e345c3769327ec59e5e4ddc26
ipetrovych <ipetrovych@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-21 23:01:42 +0000

    DropDownList done

--- branches/IUDICO/IUDICO.Common/Models/Attributes/DropDownListAttribute.cs
+++ branches/IUDICO/IUDICO.Common/Models/Attributes/DropDownListAttribute.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace IUDICO.Common.Models.Attributes
 {
