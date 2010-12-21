f3b56e55e5852a665a6f5dab91c25e6fa16f469c
shopen21 <shopen21@gmail.com>
2010-12-21 19:24:25 +0000

    - Provided base api with fake data implementation for statistics from testing system

--- branches/IUDICO/IUDICO.TestingSystem/Models/TestingService.cs
+++ branches/IUDICO/IUDICO.TestingSystem/Models/TestingService.cs
@@ -1,8 +1,20 @@
-﻿using IUDICO.Common.Models.Services;
+﻿using System;
+using System.Collections.Generic;
+using IUDICO.Common.Models.Services;
+using IUDICO.Common.Models.Shared.Statistics;
 
 namespace IUDICO.TestingSystem.Models
 {
     public class TestingService : ITestingService
     {
+        #region ITestingService interface implementation
+
+        public IEnumerable<AttemptResult> GetResults(Guid userID, int courseID)
+        {
+            throw new NotImplementedException();
+            return new List<AttemptResult>();
+        }
+
+        #endregion
     }
 }
