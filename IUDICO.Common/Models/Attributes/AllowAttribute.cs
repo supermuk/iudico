1f1a6f487f2092f9f1c79a3f6ed17625205f7546
ipetrovych <ipetrovych@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-25 20:20:46 +0000

    AllowAttribute

---
+++ IUDICO.Common/Models/Attributes/AllowAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IUDICO.Common.Models.Attributes
+{
+    public class AllowAttribute : AuthorizeAttribute
+    {
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated)
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+            else
+            {
+                filterContext.Result = new ViewResult { ViewName = "AccessDenied" };
+            }
+        }
+    }
+}
