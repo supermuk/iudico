ff2108a2efc4140a241c53dfa3524786638df3d8
boblox12 <boblox12@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-25 20:40:18 +0000

    some changes to ICurriculumStorage and ICurriculumService so that they can work

---
+++ branches/IUDICO/IUDICO.CurriculumManagement/Models/CurriculumService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IUDICO.Common.Models.Services;
+using IUDICO.Common.Models;
+using IUDICO.CurriculumManagement.Models.Storage;
+
+namespace IUDICO.CurriculumManagement.Models
+{
+    public class CurriculumService : ICurriculumService
+    {
+        private ICurriculumStorage curriculumStorage;
+
+        public CurriculumService(ICurriculumStorage curriculumStorage)
+        {
+            this.curriculumStorage = curriculumStorage;
+        }
+
+        #region ICurriculumService Members
+
+        public IEnumerable<Curriculum> GetCurriculums()
+        {
+            return curriculumStorage.GetCurriculums();
+        }
+
+        public IEnumerable<Curriculum> GetCurriculums(IEnumerable<int> ids)
+        {
+            return curriculumStorage.GetCurriculums(ids);
+        }
+
+        public Curriculum GetCurriculum(int id)
+        {
+            return curriculumStorage.GetCurriculum(id);
+        }
+
+        public IEnumerable<Stage> GetStages(int curriculumId)
+        {
+            return curriculumStorage.GetStages(curriculumId);
+        }
+
+        public IEnumerable<Stage> GetStages(IEnumerable<int> ids)
+        {
+            return curriculumStorage.GetStages(ids);
+        }
+
+        public Stage GetStage(int id)
+        {
+            return curriculumStorage.GetStage(id);
+        }
+
+        public IEnumerable<Theme> GetThemes(int stageId)
+        {
+            return curriculumStorage.GetThemes(stageId);
+        }
+
+        public IEnumerable<Theme> GetThemes(IEnumerable<int> ids)
+        {
+            return curriculumStorage.GetThemes(ids);
+        }
+
+        public Theme GetTheme(int id)
+        {
+            return curriculumStorage.GetTheme(id);
+        }
+
+        public IEnumerable<Timeline> GetTimelines()
+        {
+            return curriculumStorage.GetTimelines();
+        }
+
+        #endregion
+    }
+}
