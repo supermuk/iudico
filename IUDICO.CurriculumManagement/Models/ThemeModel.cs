0fb7a3f76835464474aa471f56ce0eaeb5a44e62
boblox12 <boblox12@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-27 02:07:28 +0000

    added new Sql/12.curriculum_and_curriculum_assign_fix.sql(Curriculum and Curriculum Assignment tables fix, added new table ThemeType) fixed code in Curriculum project

---
+++ IUDICO.CurriculumManagement/Models/ThemeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IUDICO.Common.Models;
+
+namespace IUDICO.CurriculumManagement.Models
+{
+    public class ThemeModel
+    {
+        public IEnumerable<SelectListItem> Courses { get; set; }
+        public IEnumerable<Theme> Themes { get; set; }
+        public int CourseId { get; set; }
+        public int StageId { get; set; }
+        public int ThemeId { get; set; }
+    }
+}
