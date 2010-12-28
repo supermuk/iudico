883acb62172b392ecdabb1671f58de8deaf43c72
RomanKravets <RomanKravets@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-28 13:05:21 +0000

    Added: - new Controllers/StatsController.cs - new StatisticsStorage.cs - start statistic page Index.aspx (to select groups) - SelectCurriculums.aspx (to select curriculums) - ShowCurriculumStatistic.aspx - show statistic for selected group and curriculums.
    Shows statistics used methods implemented in the services.
    Added file...

---
+++ branches/IUDICO/IUDICO.Statistics/Controllers/StatsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using IUDICO.Common.Models;
+using IUDICO.Common.Models.Attributes;
+using IUDICO.Common.Models.Services;
+using IUDICO.Common.Models.Shared.Statistics;
+using IUDICO.Common.Controllers;
+using IUDICO.Statistics.Models;
+using IUDICO.Statistics.Models.Storage;
+
+namespace IUDICO.Statistics.Controllers
+{
+    public class StatsController : PluginController
+    {
+        private readonly IStatisticsStorage _storage;
+
+        public StatsController(IStatisticsStorage statsStorage)
+        {
+            _storage = statsStorage;
+        }
+
+        //pages
+
+        public ActionResult Index()
+        {
+            IEnumerable<Group> groups = _storage.GetAllGroups();
+
+            return View(groups);
+        }
+
+        [HttpPost]
+        public ActionResult SelectCurriculums(Int32 id)
+        {
+            IEnumerable<Curriculum> curriculums = _storage.GetCurrilulumsByGroupId(id);
+            HttpContext.Session["SelectedGroupId"] = id;
+
+            return View(curriculums);
+        }
+
+        [HttpPost]
+        public ActionResult ShowCurriculumStatistic(Int32[] selectCurriculumId)
+        {
+            StatisticsStorage curriculumResult = new StatisticsStorage();
+            curriculumResult.Curriculums = _storage.GetSelectedCurriclums(selectCurriculumId);
+            curriculumResult.Students = _storage.GetUsersBySelectedGroup(LmsService.FindService<IUserService>().GetGroup((int)HttpContext.Session["SelectedGroupId"]));
+            curriculumResult.Themes = _storage.GetAllThemes((int)HttpContext.Session["SelectedGroupId"]);
+            ViewData["selectCurriculumId"] = selectCurriculumId;
+
+            #region Students result by themes
+            List<KeyValuePair<KeyValuePair<User, Theme>, double?>> results = new List<KeyValuePair<KeyValuePair<User, Theme>, double?>>();
+            foreach (User user in curriculumResult.Students)
+            {
+                foreach (Curriculum curr in curriculumResult.Curriculums)
+                {
+                    foreach (KeyValuePair<List<Theme>, int> themeAndCurrId in curriculumResult.Themes)
+                    {
+                        if (themeAndCurrId.Value == curr.Id)
+                        {
+                            foreach (Theme th in themeAndCurrId.Key)
+                            {
+                                double? res = _storage.GetResults(user, th).First(x => x.User == user & x.Theme == th).Score.ToPercents();
+                                results.Add(new KeyValuePair<KeyValuePair<User, Theme>, double?>(new KeyValuePair<User, Theme>(user, th), res));
+                            }
+                        }
+                    }
+                }
+            }
+            ViewData["points"] = results;
+            #endregion
+
+            return View(curriculumResult);
+        }
+
+    }
+}
---
+++ branches/IUDICO/IUDICO.Statistics/Views/Stats/Index.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<IEnumerable<IUDICO.Common.Models.Group>>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	Index
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Group list</h2>
+
+    <fieldset>
+    <legend>Please, select one group : </legend>
+    <form action="/Stats/SelectCurriculums/" method="post">
+
+    <% foreach (IUDICO.Common.Models.Group item in Model)
+       {%>
+       <div>
+        <input type="checkbox" name="id" value="<%: item.Id %>" />
+            <%: Html.Label(item.Name)%>
+       </div>
+    <% } %>
+
+    <input type="submit" value="Show" />
+
+    </form>
+    
+    </fieldset>
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="HeadContent" runat="server">
+
+</asp:Content>
