ad7900f244d5a847a47e0b658aa6e4da441a8346
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2010-12-30 14:57:06 +0000

    added database project configured publishing

---
+++ branches/IUDICO/IUDICO.Database/Schema Objects/Database Level Objects/Storage/Files/IUDICO.sqlfile.sql
@@ -0,0 +1,3 @@
+﻿ALTER DATABASE [$(DatabaseName)]
+    ADD FILE (NAME = [IUDICO], FILENAME = '$(DefaultDataPath)$(DatabaseName).mdf', SIZE = 2048 KB, FILEGROWTH = 1024 KB) TO FILEGROUP [PRIMARY];
+
---
+++ branches/IUDICO/IUDICO.Database/Schema Objects/Schemas/dbo/Tables/User.table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]         UNIQUEIDENTIFIER NOT NULL,
+    [Username]   NVARCHAR (100)   COLLATE Ukrainian_CI_AS NOT NULL,
+    [Password]   NVARCHAR (50)    COLLATE Ukrainian_CI_AS NOT NULL,
+    [Email]      NVARCHAR (100)   COLLATE Ukrainian_CI_AS NOT NULL,
+    [OpenId]     NVARCHAR (200)   COLLATE Ukrainian_CI_AS NOT NULL,
+    [Name]       NVARCHAR (200)   COLLATE Ukrainian_CI_AS NOT NULL,
+    [IsApproved] BIT              NOT NULL,
+    [RoleRef]    INT              NOT NULL,
+    [Deleted]    BIT              NOT NULL
+);
+
