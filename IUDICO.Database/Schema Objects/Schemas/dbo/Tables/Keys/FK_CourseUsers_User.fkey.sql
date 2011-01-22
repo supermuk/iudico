e742fa3376b632744426a93d81bbf5cfda052915
sm.lpml <sm.lpml@gmail.com>
2011-01-22 10:40:02 +0000

    Removed Foreign Key Course-Users

--- IUDICO.Database/Schema Objects/Schemas/dbo/Tables/Keys/FK_CourseUsers_User.fkey.sql
+++ IUDICO.Database/Schema Objects/Schemas/dbo/Tables/Keys/FK_CourseUsers_User.fkey.sql
@@ -1,3 +1,3 @@
-﻿ALTER TABLE [dbo].[CourseUsers]
-    ADD CONSTRAINT [FK_CourseUsers_User] FOREIGN KEY ([UserRef]) REFERENCES [dbo].[User] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+﻿/*ALTER TABLE [dbo].[CourseUsers]
+    ADD CONSTRAINT [FK_CourseUsers_User] FOREIGN KEY ([UserRef]) REFERENCES [dbo].[User] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;*/
 
