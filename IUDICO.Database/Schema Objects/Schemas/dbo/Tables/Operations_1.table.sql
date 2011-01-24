a92e74d9cd842657ad73f96d8651fc184972929b
boblox12 <boblox12@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-01-24 23:56:05 +0000

    renamed tables:operation, themetype, timeline, added new field isdeleted updated dbml

--- IUDICO.Database/Schema Objects/Schemas/dbo/Tables/Operations_1.table.sql
+++ IUDICO.Database/Schema Objects/Schemas/dbo/Tables/Operations_1.table.sql
@@ -1,5 +1,5 @@
-﻿/*CREATE TABLE [dbo].[Operation] (
+﻿CREATE TABLE [dbo].[Operations] (
     [Id]   INT        IDENTITY (1, 1) NOT NULL,
     [Name] NCHAR (50) COLLATE Ukrainian_CI_AS NOT NULL
-);*/
+);
 
