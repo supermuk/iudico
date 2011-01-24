dfdb37189da8d22fb46cda8b4a5d9703d00f424c
shopen21 <shopen21@gmail.com>
2011-01-24 18:37:27 +0000

    missing notification files. sorry :)

---
+++ branches/IUDICO/IUDICO.Common/Models/Events/CurriculumNotifications.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IUDICO.Common.Models.Notifications
+{
+    /// <summary>
+    /// Holds Curriculum - related notification's name constants with description.
+    /// </summary>
+    public static class CurriculumNotifications
+    {
+    }
+}
