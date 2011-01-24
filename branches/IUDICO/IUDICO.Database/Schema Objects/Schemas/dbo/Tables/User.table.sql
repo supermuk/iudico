8317f7bee32c4712f9c5e28d361bf48b3f6001cb
sm.lpml <sm.lpml@gmail.com>
2011-01-24 18:51:01 +0000

    Fixed buggy andrew's fix!

--- branches/IUDICO/IUDICO.Database/Schema Objects/Schemas/dbo/Tables/User.table.sql
+++ branches/IUDICO/IUDICO.Database/Schema Objects/Schemas/dbo/Tables/User.table.sql
@@ -7,8 +7,11 @@
     [Name]       NVARCHAR (200)   COLLATE Ukrainian_CI_AS NOT NULL,
     [IsApproved] BIT              NOT NULL,
     [RoleId]     INT              NOT NULL,
-    [Deleted]    BIT              NOT NULL
+    [Deleted]    BIT              NOT NULL,
+    UNIQUE NONCLUSTERED ([Username] ASC) WITH (ALLOW_PAGE_LOCKS = ON, ALLOW_ROW_LOCKS = ON, PAD_INDEX = OFF, IGNORE_DUP_KEY = OFF, STATISTICS_NORECOMPUTE = OFF) ON [PRIMARY]
 );
+
+
 
 
 
