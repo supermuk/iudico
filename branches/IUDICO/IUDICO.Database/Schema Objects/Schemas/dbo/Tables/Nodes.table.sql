8614eb50646a0d074c3e0e659ae6ca669b989295
sm.lpml <sm.lpml@gmail.com>
2011-03-06 12:29:06 +0000

    Added Locked parameter for uploaded Courses. Added SequencingPattern column to Courses and Nodes.

--- branches/IUDICO/IUDICO.Database/Schema Objects/Schemas/dbo/Tables/Nodes.table.sql
+++ branches/IUDICO/IUDICO.Database/Schema Objects/Schemas/dbo/Tables/Nodes.table.sql
@@ -1,9 +1,12 @@
 ﻿CREATE TABLE [dbo].[Nodes] (
-    [Id]       INT           IDENTITY (1, 1) NOT NULL,
-    [Name]     NVARCHAR (50) COLLATE Ukrainian_CI_AS NOT NULL,
-    [CourseId] INT           NOT NULL,
-    [ParentId] INT           NULL,
-    [IsFolder] BIT           NOT NULL,
-    [Position] INT           NOT NULL
+    [Id]                INT           IDENTITY (1, 1) NOT NULL,
+    [Name]              NVARCHAR (50) COLLATE Ukrainian_CI_AS NOT NULL,
+    [CourseId]          INT           NOT NULL,
+    [ParentId]          INT           NULL,
+    [IsFolder]          BIT           NOT NULL,
+    [Position]          INT           NOT NULL,
+    [SequencingPattern] INT           NULL
 );
+
+
 
