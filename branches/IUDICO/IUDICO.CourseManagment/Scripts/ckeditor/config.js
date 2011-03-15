4068acb30964e59afc185c2a56257cb51978c15e
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-03-15 15:09:02 +0000

    ckeditor plugin

--- branches/IUDICO/IUDICO.CourseManagment/Scripts/ckeditor/config.js
+++ branches/IUDICO/IUDICO.CourseManagment/Scripts/ckeditor/config.js
@@ -3,9 +3,32 @@ Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function (config) {
+    // Define changes to default configuration here. For example:
+    // config.language = 'fr';
+    // config.uiColor = '#AADC6E';
+
+    config.extraPlugins = 'iudico,save';
+
+    CKEDITOR.config.toolbar_IUDICO =
+    [
+	    ['Source','-','Save','NewPage','Preview','-','Templates'],
+	    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+	    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	    ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+	    '/',
+	    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+	    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+	    ['BidiLtr', 'BidiRtl' ],
+	    ['Link','Unlink','Anchor'],
+	    ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak'],
+	    '/',
+	    ['Styles','Format','Font','FontSize'],
+	    ['TextColor','BGColor'],
+	    ['Maximize', 'ShowBlocks', '-', 'About'],
+        ['iudico-simple', 'iudico-choice', 'iudico-compile'],
+    ];
+
+    config.toolbar = 'IUDICO';
 };
