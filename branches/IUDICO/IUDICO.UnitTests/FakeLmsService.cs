2b592e263ecda246dcc1458d64c776e68d993eac
boblox12 <boblox12@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-03-21 23:10:55 +0000

    added curriculum storage test template

---
+++ branches/IUDICO/IUDICO.UnitTests/FakeLmsService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IUDICO.Common.Models.Services;
+using IUDICO.Common.Models;
+using System.Data.Common;
+
+namespace IUDICO.UnitTests
+{
+    class FakeLmsService : ILmsService
+    {
+        #region ILmsService Members
+
+        public T FindService<T>() where T : IService
+        {
+            //must return pathes to fake or real services
+            throw new NotImplementedException();
+        }
+
+        public DBDataContext GetDbDataContext()
+        {
+            return new DBDataContext("This must be path to fake repository");
+        }
+
+        public DbConnection GetDbConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Inform(string evt, params object[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
