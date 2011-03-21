69c460dea551897662d7fd8225e0ce3ed4019d6b
boblox12 <boblox12@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-03-21 23:10:55 +0000

    added curriculum storage test template

---
+++ branches/IUDICO/IUDICO.UnitTests/CurriculumStorageTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IUDICO.CurriculumManagement.Models.Storage;
+using IUDICO.Common.Models;
+using IUDICO.Common.Models.Services;
+
+namespace IUDICO.UnitTests
+{
+    [TestClass]
+    public class CurriculumStorageTest
+    {
+        MixedCurriculumStorage storage { get; set; }
+        static ILmsService fakeLmsService { get; set; }
+
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
+        {
+            fakeLmsService = new FakeLmsService();
+            DBDataContext context = fakeLmsService.GetDbDataContext();
+            //clear all tables before test runs!
+            context.Curriculums.DeleteAllOnSubmit(context.Curriculums);
+            context.SubmitChanges();
+            //...
+        }
+
+        [TestInitialize()]
+        public void InitializeTest()
+        {
+            storage = new MixedCurriculumStorage(fakeLmsService);
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Curriculum curriculum = new Curriculum
+            {
+                Name="Bob",
+                IsDeleted=false,
+            };
+            int id = storage.AddCurriculum(curriculum);
+            storage.GetCurriculum(1);
+            AdvAssert.AreEqual(curriculum, storage.GetCurriculum(id));
+        }
+    }
+}
---
+++ branches/IUDICO/IUDICO.UnitTests/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IUDICO.Common.Models;
+
+namespace IUDICO.UnitTests
+{
+    public static class AdvAssert
+    {
+        public static void AreEqual(Curriculum expected, Curriculum actual)
+        {
+            Assert.AreEqual(expected.Name, actual.Name);
+            Assert.AreEqual(expected.IsDeleted, actual.IsDeleted);
+            Assert.AreEqual(expected.Id, actual.IsDeleted);
+        }
+    }
+
+    public static class Extensions
+    {
+
+    }
+}
