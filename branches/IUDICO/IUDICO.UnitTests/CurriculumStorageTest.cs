20493c7f9ccdf1701a8d2ff43af40925be31b70a
SuperKivi007 <SuperKivi007@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-03-22 00:42:56 +0000

    +tests

--- branches/IUDICO/IUDICO.UnitTests/CurriculumStorageTest.cs
+++ branches/IUDICO/IUDICO.UnitTests/CurriculumStorageTest.cs
@@ -32,17 +32,138 @@ namespace IUDICO.UnitTests
             storage = new MixedCurriculumStorage(fakeLmsService);
         }
 
+        #region CurriculumTestMethods
+
         [TestMethod]
         public void TestMethod1()
         {
             Curriculum curriculum = new Curriculum
             {
-                Name="Bob",
-                IsDeleted=false,
+                Name = "Bob",
+                IsDeleted = false,
             };
             int id = storage.AddCurriculum(curriculum);
-            storage.GetCurriculum(1);
+            AdvAssert.AreEqual(curriculum, storage.GetCurriculum(id));
+            //storage.GetCurriculum(1);
+            //викликає Exception оскільки нема такого запису,треба шось підмутити
+            //питання - якщо нема такого,то шо вертати??
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Curriculum curriculum1 = new Curriculum { Name = "FirstTestCurr" };
+            Curriculum curriculum2 = new Curriculum { Name = "SecondTestCurr" };
+            storage.AddCurriculum(curriculum1);
+            storage.AddCurriculum(curriculum2);
+            int expected = 2;
+            int actual = storage.GetCurriculums().ToList().Count;
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Curriculum curriculum = new Curriculum { Name = "CurriculumToUpdate" };
+            int id = storage.AddCurriculum(curriculum);
+            curriculum.Name = "UpdatedCurriculum";
             AdvAssert.AreEqual(curriculum, storage.GetCurriculum(id));
         }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Curriculum curriculum = new Curriculum { Name = "CurriculumToDelete" };
+            int id = storage.AddCurriculum(curriculum);
+            storage.DeleteCurriculum(id);
+            //якщо шо - поправити
+            try
+            {
+                storage.GetCurriculum(id);
+                Assert.AreEqual(true, false);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(true, true);
+            }
+        }
+
+        #endregion
+
+        #region StageTestMethods
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Stage stage = new Stage { Name = "StageToAdd" };
+            int id = storage.AddStage(stage);
+            AdvAssert.AreEqual(storage.GetStage(id), stage);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Curriculum curriculum = new Curriculum { Name = "Curriculum" };
+            int curriculumId = storage.AddCurriculum(curriculum);
+            Stage stage = new Stage { Name = "Stage", CurriculumRef = curriculumId};
+            int stageId = storage.AddStage(stage);
+            List<Stage> listWithStagesForTest = new List<Stage>();
+            listWithStagesForTest.Add(stage);
+            AdvAssert.AreEqual(listWithStagesForTest, storage.GetStages(curriculumId).ToList());
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Stage stage = new Stage() { Name = "StageForUpdate" };
+            int id = storage.AddStage(stage);
+            stage.Name = "UpdatedStage";
+            storage.UpdateStage(stage);
+
+            AdvAssert.AreEqual(stage, storage.GetStage(id));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Stage stage = new Stage() { Name = "StageForDelete" };
+            int id = storage.AddStage(stage);
+            AdvAssert.AreEqual(storage.GetStage(id), stage);
+            storage.DeleteStage(id);
+
+            try
+            {
+                storage.GetStage(id);
+                Assert.AreEqual(true, false);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(true,true);
+            }
+        }
+
+        #endregion
+
+        #region ThemeMethods
+
+        //як розуміти метод private Theme GetTheme(int id, DBDataContext db)???
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Theme theme = new Theme() { Name = "theme" };
+            int id = storage.AddTheme(theme);
+            AdvAssert.AreEqual(theme, storage.GetTheme(id));
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+
+
+        }
+
+
+        #endregion
     }
 }
--- branches/IUDICO/IUDICO.UnitTests/Extensions.cs
+++ branches/IUDICO/IUDICO.UnitTests/Extensions.cs
@@ -13,10 +13,44 @@ namespace IUDICO.UnitTests
         {
             Assert.AreEqual(expected.Name, actual.Name);
             Assert.AreEqual(expected.IsDeleted, actual.IsDeleted);
-            Assert.AreEqual(expected.Id, actual.IsDeleted);
+            Assert.AreEqual(expected.Id, actual.Id);
+        }
+
+        public static void AreEqual(Stage expected, Stage actual)
+        {
+            Assert.AreEqual(expected.Name, actual.Name);
+            Assert.AreEqual(expected.IsDeleted, actual.IsDeleted);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.CurriculumRef, actual.CurriculumRef);
+        }
+
+        public static void AreEqual(List<Stage> expected,List<Stage> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            int i = 0;
+            foreach (Stage item in expected)
+            {
+                Assert.AreEqual(item.Name, actual[i].Name);
+                Assert.AreEqual(item.IsDeleted, actual[i].IsDeleted);
+                Assert.AreEqual(item.Id, actual[i].Id);
+                Assert.AreEqual(item.CurriculumRef, actual[i].CurriculumRef);
+                i++;
+            }
+        }
+
+        public static void AreEqual(Theme expected, Theme actual)
+        {
+            Assert.AreEqual(expected.Name, actual.Name);
+            Assert.AreEqual(expected.IsDeleted, actual.IsDeleted);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.CourseRef,expected.CourseRef);
+            Assert.AreEqual(expected.ThemeTypeRef, expected.ThemeTypeRef);
+            Assert.AreEqual(expected.StageRef, expected.StageRef);
         }
     }
-
+    
+    
     public static class Extensions
     {
 
