502f9a49e8298a5e38abd78e9a74c0768eaef13c
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-03-22 22:32:09 +0000

    ckeditor plugin

--- branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/plugins/iudico/plugin.js
+++ branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/plugins/iudico/plugin.js
@@ -6,6 +6,7 @@ Copyright (c) 2010, IUDICO. All rights reserved.
 * @file IUDICO plugin
 */
 
+
 CKEDITOR.dialog.prototype.addParam = function (element, name, value) {
     var documentObject = element.getDocument(),
         paramNode = documentObject.createElement("param");
@@ -15,7 +16,7 @@ CKEDITOR.dialog.prototype.addParam = function (element, name, value) {
 
     element.append(paramNode);
 };
-
+/*
 CKEDITOR.dialog.prototype.getObject = function (editor) {
     var path = new CKEDITOR.dom.elementPath(editor.getSelection().getStartElement()),
         blockLimit = path.blockLimit,
@@ -23,20 +24,49 @@ CKEDITOR.dialog.prototype.getObject = function (editor) {
 
     return obj;
 }
+*/
 
-CKEDITOR.plugins.addQuestionCommand = function (commandName) {
-    dialogFile = this.path + 'dialogs/' + commandName + '.js';
+function addQuestionCommand(editor, command, path) {
+    var dialogFile = path + 'dialogs/' + command + '.js';
 
-    editor.addCommand(commandName, new CKEDITOR.dialogCommand(commandName));
+    CKEDITOR.dialog.add(command, dialogFile);
 
-    editor.ui.addButton(commandName,
-	{
-		label: lang.iudico[commandName],
-		command: commandName
-	});
+    editor.addCommand(command, {
+        exec: function (editor) {
+            CKEDITOR.plugins.iudicoCmd(editor, command);
+        }
+    });
 
-    CKEDITOR.dialog.add(commandName, dialogFile);
+    editor.ui.addButton(command, {
+        label: editor.lang.iudico[command],
+        icon: path + 'logo_iudico.png',
+        command: command
+    });
+
+    CKEDITOR.dialog.add(command, dialogFile);
 };
+
+function isIudicoObject(element) {
+    var attributes = element.attributes;
+
+    return (attributes.type == 'iudico');
+}
+
+function createFakeElement(editor, realElement) {
+    var fakeElement = editor.createFakeParserElement(realElement, 'cke_iudico', 'div', true),
+		fakeStyle = fakeElement.attributes.style || '';
+
+    var width = realElement.attributes.width,
+		height = realElement.attributes.height;
+
+    if (typeof width != 'undefined')
+        fakeStyle = fakeElement.attributes.style = fakeStyle + 'width:' + cssifyLength(width) + ';';
+
+    if (typeof height != 'undefined')
+        fakeStyle = fakeElement.attributes.style = fakeStyle + 'height:' + cssifyLength(height) + ';';
+
+    return fakeElement;
+}
 
 CKEDITOR.plugins.add('iudico', {
     availableLangs: { en: 1 },
@@ -46,13 +76,12 @@ CKEDITOR.plugins.add('iudico', {
     init: function (editor) {
         var pluginName = 'iudico';
         var lang = editor.lang.iudico;
-
-        console.log('iudico - init');
+        var commands = ['iudico-simple', 'iudico-choice', 'iudico-compile'];
 
         editor.addCss(
 		'.iudico-question' +
 		'{' +
-			'background-image: url(' + CKEDITOR.getUrl(this.path + 'images/logo_iudico.png') + ');' +
+			'background-image: url(' + CKEDITOR.getUrl(this.path + 'logo_iudico.png') + ');' +
 			'background-position: center center;' +
 			'background-repeat: no-repeat;' +
 			'border: 1px solid #a9a9a9;' +
@@ -60,85 +89,24 @@ CKEDITOR.plugins.add('iudico', {
 			'height: 16px !important;' +
 		'}');
 
-        CKEDITOR.dialog.add('iudico-simple', this.path + 'dialogs/iudico-simple.js');
-        CKEDITOR.dialog.add('iudico-choice', this.path + 'dialogs/iudico-choice.js');
-        CKEDITOR.dialog.add('iudico-compile', this.path + 'dialogs/iudico-compile.js');
-
-        editor.addCommand('iudico', CKEDITOR.plugins.iudicoCmd );
-
-        editor.ui.addButton('iudico',
-        {
-            label: editor.lang.iudico.toolbar,
-            icon: this.path + 'logo_iudico.png',
-            command: 'iudico'
-        });
-
-
-        /*this.addButtonCommand('iudico-simple');
-        this.addButtonCommand('iudico-choice');
-        this.addButtonCommand('iudico-compile');*/
-
-        /*
-        // Register simple iudico question
-        CKEDITOR.dialog.add('iudico-simple', this.path + 'dialogs/iudico-simple.js' );
-        editor.addCommand('iudico-simple', new CKEDITOR.dialogCommand('iudico-simple'));
-        editor.ui.addButton('iudico-simple',
-        {
-        label : editor.lang.iudico.toolbar,
-        icon : this.path + 'logo_iudico.png',
-        command: 'iudico-simple'
-        });
-
-        // Register choice iudico question
-        CKEDITOR.dialog.add('iudico-choice', this.path + 'dialogs/iudico-choice.js');
-        editor.addCommand('iudico-choice', new CKEDITOR.dialogCommand('iudico-choice'));
-        editor.ui.addButton('iudico-choice',
-        {
-        label: editor.lang.iudico.toolbar,
-        icon: this.path + 'logo_iudico.png',
-        command: 'iudico-choice'
-        });
-
-        // Register compile iudico question
-        CKEDITOR.dialog.add('iudico-compile', this.path + 'dialogs/iudico-compile.js');
-        editor.addCommand('iudico-compile', new CKEDITOR.dialogCommand('iudico-compile'));
-        editor.ui.addButton('iudico-compile',
-        {
-        label: editor.lang.iudico.toolbar,
-        icon: this.path + 'logo_iudico.png',
-        command: 'iudico-compile'
-        });
-        */
+        for (var i in commands) {
+            addQuestionCommand(editor, commands[i], this.path);
+        }
     },
 
     afterInit: function (editor) {
         // Register a filter to displaying placeholders after mode change.
-
         var dataProcessor = editor.dataProcessor,
         dataFilter = dataProcessor && dataProcessor.dataFilter;
 
         if (dataFilter) {
             dataFilter.addRules({
                 elements: {
-                    div: function (element) {
-                        /*var attributes = element.attributes;
-                        cssClass = element.attributes.class;
-                        //style = attributes && attributes.style;
-                        //child = style && element.children.length == 1 && element.children[ 0 ],
-                        //childStyle = child && ( child.name == 'span' ) && child.attributes.style;
-                        */
-                        if (element.className == 'iudico-question') {
-                            //var fakeDiv
+                    'cke:object': function (element) {
+                        if (!isIudicoObject(element))
+                            return null;
 
-                        }
-
-                        /*if ( childStyle && ( /page-break-after\s*:\s*always/i ).test( style ) && ( /display\s*:\s*none/i ).test( childStyle ) ) {
-                        var fakeImg = editor.createFakeParserElement( element, 'cke_pagebreak', 'div' );
-                        var label = editor.lang.pagebreakAlt;
-                        fakeImg.attributes[ 'alt' ] = label;
-                        fakeImg.attributes[ 'aria-label' ] = label;
-                        return fakeImg;
-                        }*/
+                        return createFakeElement(editor, element);
                     }
                 }
             });
@@ -146,9 +114,11 @@ CKEDITOR.plugins.add('iudico', {
     }
 });
 
-CKEDITOR.plugins.iudicoCmd =
-{
-    exec: function (editor) {
+CKEDITOR.plugins.iudicoCmd = function (editor, command) {
+
+
+    editor.openDialog(command);
+
         // Create the element that represents a print break.
         /*var label = editor.lang.pagebreakAlt;
         var breakObject = CKEDITOR.dom.element.createFromHtml('<div style="page-break-after: always;"><span style="display: none;">&nbsp;</span></div>');
@@ -182,5 +152,4 @@ CKEDITOR.plugins.iudicoCmd =
         }
 
         editor.fire('saveSnapshot');*/
-    }
 };
