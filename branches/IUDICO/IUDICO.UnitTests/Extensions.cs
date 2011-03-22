b3b96e3c2144b7c7a35362fb13a8b1fc0ea8ceea
SuperKivi007 <SuperKivi007@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-03-22 23:50:35 +0000

    + some new tests

--- branches/IUDICO/IUDICO.UnitTests/Extensions.cs
+++ branches/IUDICO/IUDICO.UnitTests/Extensions.cs
@@ -48,6 +48,46 @@ namespace IUDICO.UnitTests
             Assert.AreEqual(expected.ThemeTypeRef, expected.ThemeTypeRef);
             Assert.AreEqual(expected.StageRef, expected.StageRef);
         }
+
+        public static void AreEqual(List<Theme> expected, List<Theme> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            int i = 0;
+            foreach (Theme item in expected)
+            {
+                Assert.AreEqual(item.Name, actual[i].Name);
+                Assert.AreEqual(item.IsDeleted, actual[i].IsDeleted);
+                Assert.AreEqual(item.Id, actual[i].Id);
+                Assert.AreEqual(item.StageRef, actual[i].StageRef);
+                Assert.AreEqual(item.CourseRef, expected[i].CourseRef);
+                Assert.AreEqual(item.ThemeTypeRef, expected[i].ThemeTypeRef);
+                i++;
+            }
+        }
+
+        public static void AreEqual(CurriculumAssignment actual, CurriculumAssignment expected)
+        {
+            Assert.AreEqual(expected.UserGroupRef, actual.UserGroupRef);
+            Assert.AreEqual(expected.Id, actual.Id);
+            Assert.AreEqual(expected.IsDeleted, actual.IsDeleted);
+            Assert.AreEqual(expected.CurriculumRef, actual.CurriculumRef);
+        }
+
+        public static void AreEqual(List<CurriculumAssignment> expected, List<CurriculumAssignment> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+
+            int i = 0;
+            foreach (CurriculumAssignment item in expected)
+            {
+                Assert.AreEqual(item.Id, actual[i].Id);
+                Assert.AreEqual(item.IsDeleted, actual[i].IsDeleted);
+                Assert.AreEqual(item.CurriculumRef, actual[i].CurriculumRef);
+                Assert.AreEqual(item.UserGroupRef, actual[i].UserGroupRef);
+                i++;
+            }
+        }
     }
     
     
