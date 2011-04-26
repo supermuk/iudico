ec4970e3e8e62cbf23fbb7162a22ea8341672901
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-04-26 20:27:23 +0000

    ckeditor plugins course editor

--- branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/plugins/iudico/dialogs/iudico-compile.js
+++ branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/plugins/iudico/dialogs/iudico-compile.js
@@ -1,5 +1,46 @@
 ﻿CKEDITOR.dialog.add('iudico-compile', function (editor) {
 
+    // Add a new option to a SELECT object (combo or list).
+    function addOption(combo, optionText, optionValue, documentObject, index) {
+        combo = getSelect(combo);
+        var oOption;
+        if (documentObject)
+            oOption = documentObject.createElement("OPTION");
+        else
+            oOption = document.createElement("OPTION");
+
+        if (combo && oOption && oOption.getName() == 'option') {
+            if (CKEDITOR.env.ie) {
+                if (!isNaN(parseInt(index, 10)))
+                    combo.$.options.add(oOption.$, index);
+                else
+                    combo.$.options.add(oOption.$);
+
+                oOption.$.innerHTML = optionText.length > 0 ? optionText : '';
+                oOption.$.value = optionValue;
+            }
+            else {
+                if (index !== null && index < combo.getChildCount())
+                    combo.getChild(index < 0 ? 0 : index).insertBeforeMe(oOption);
+                else
+                    combo.append(oOption);
+
+                oOption.setText(optionText.length > 0 ? optionText : '');
+                oOption.setValue(optionValue);
+            }
+        }
+        else
+            return false;
+
+        return oOption;
+    }
+
+    // Remove option from a SELECT object.
+    function removeOption(combo, index) {
+        combo = getSelect(combo);
+        combo.getChild(index).remove();
+    }
+
     return {
         title: editor.lang.iudico.title,
         minWidth: 390,
@@ -15,65 +56,130 @@
         contents: [
             {
                 id: 'compileTab',
-                label: editor.lang.iudico.choiceTab,
-                title: editor.lang.iudico.choiceTab,
+                label: editor.lang.iudico.compileTab,
+                title: editor.lang.iudico.compileTab,
                 padding: 2,
                 elements:
 				[
                     {
-                        id: 'question',
-                        type: 'text',
-                        label: editor.lang.iudico.question,
+                        id: 'language',
+                        type: 'select',
+                        label: editor.lang.iudico.language,
+                        items: [ ['C#'], ['C++'], ['Java'] ],
                         labelLayout: 'horizontal',
                         commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
-                            if (paramMap['question']) {
-                                paramMap['question'].setAttribute("value", this.getValue());
+                            if (paramMap[this.id]) {
+                                paramMap[this.id].setAttribute("value", this.getValue());
                             } else {
-                                paramMap['question'] = this.getDialog().addParam(objectNode, 'question', this.getValue());
+                                paramMap[this.id] = this.getDialog().addParam(objectNode, this.id, this.getValue());
                             }
                         },
                         setup: function (objectNode, embedNode, paramMap) {
-                            this.setValue(paramMap['question']);
+                            this.setValue(paramMap[this.id]);
+                        }
+                    },
+                    {
+                        id: 'memoryLimit',
+                        type: 'text',
+                        label: editor.lang.iudico.memoryLimit,
+                        labelLayout: 'horizontal',
+                        commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
+                            if (paramMap[this.id]) {
+                                paramMap[this.id].setAttribute("value", this.getValue());
+                            } else {
+                                paramMap[this.id] = this.getDialog().addParam(objectNode, this.id, this.getValue());
+                            }
+                        },
+                        setup: function (objectNode, embedNode, paramMap) {
+                            this.setValue(paramMap[this.id]);
+                        }
+                    },
+                    {
+                        id: 'outputLimit',
+                        type: 'text',
+                        label: editor.lang.iudico.outputLimit,
+                        labelLayout: 'horizontal',
+                        commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
+                            if (paramMap[this.id]) {
+                                paramMap[this.id].setAttribute("value", this.getValue());
+                            } else {
+                                paramMap[this.id] = this.getDialog().addParam(objectNode, this.id, this.getValue());
+                            }
+                        },
+                        setup: function (objectNode, embedNode, paramMap) {
+                            this.setValue(paramMap[this.id]);
+                        }
+                    },
+                    {
+                        id: 'timeLimit',
+                        type: 'text',
+                        label: editor.lang.iudico.timeLimit,
+                        labelLayout: 'horizontal',
+                        commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
+                            if (paramMap[this.id]) {
+                                paramMap[this.id].setAttribute("value", this.getValue());
+                            } else {
+                                paramMap[this.id] = this.getDialog().addParam(objectNode, this.id, this.getValue());
+                            }
+                        },
+                        setup: function (objectNode, embedNode, paramMap) {
+                            this.setValue(paramMap[this.id]);
+                        }
+                    },
+                    {
+                        id: 'question',
+                        type: 'textarea',
+                        label: editor.lang.iudico.question,
+                        labelLayout: 'horizontal',
+                        commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
+                            if (paramMap[this.id]) {
+                                paramMap[this.id].setAttribute("value", this.getValue());
+                            } else {
+                                paramMap[this.id] = this.getDialog().addParam(objectNode, this.id, this.getValue());
+                            }
+                        },
+                        setup: function (objectNode, embedNode, paramMap) {
+                            this.setValue(paramMap[this.id]);
                         }
                     },
                     {
                         id: 'preCode',
-                        type: 'text',
-                        label: editor.lang.iudico.question,
+                        type: 'textarea',
+                        label: editor.lang.iudico.preCode,
                         labelLayout: 'horizontal',
                         commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
-                            if (paramMap['question']) {
-                                paramMap['question'].setAttribute("value", this.getValue());
+                            if (paramMap[this.id]) {
+                                paramMap[this.id].setAttribute("value", this.getValue());
                             } else {
-                                paramMap['question'] = this.getDialog().addParam(objectNode, 'question', this.getValue());
+                                paramMap[this.id] = this.getDialog().addParam(objectNode, this.id, this.getValue());
                             }
                         },
                         setup: function (objectNode, embedNode, paramMap) {
-                            this.setValue(paramMap['question']);
+                            this.setValue(paramMap[this.id]);
                         }
                     },
                     {
                         id: 'postCode',
-                        type: 'text',
-                        label: editor.lang.iudico.question,
+                        type: 'textarea',
+                        label: editor.lang.iudico.postCode,
                         labelLayout: 'horizontal',
                         commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
-                            if (paramMap['question']) {
-                                paramMap['question'].setAttribute("value", this.getValue());
+                            if (paramMap[this.id]) {
+                                paramMap[this.id].setAttribute("value", this.getValue());
                             } else {
-                                paramMap['question'] = this.getDialog().addParam(objectNode, 'question', this.getValue());
+                                paramMap[this.id] = this.getDialog().addParam(objectNode, this.id, this.getValue());
                             }
                         },
                         setup: function (objectNode, embedNode, paramMap) {
-                            this.setValue(paramMap['question']);
+                            this.setValue(paramMap[this.id]);
                         }
                     }
 				]
             },
             {
                 id: 'testsTab',
-                label: editor.lang.iudico.choiceTab,
-                title: editor.lang.iudico.choiceTab,
+                label: editor.lang.iudico.testsTab,
+                title: editor.lang.iudico.testsTab,
                 padding: 2,
                 elements:
 				[
@@ -83,81 +189,89 @@
                         children:
                         [
                             {
-                                id: 'choice',
-                                type: 'text',
-                                label: editor.lang.iudico.choice,
-                                labelLayout: 'horizontal',
-                                items: [],
-                                setup: function () {
-                                    this.setValue('');
-                                }
+                                type: 'vbox',
+                                children: [
+                                    {
+                                        id: 'testInput',
+                                        type: 'text',
+                                        label: editor.lang.iudico.input,
+                                        labelLayout: 'horizontal',
+                                        setup: function () {
+                                            this.setValue('');
+                                        }
+                                    },
+                                    {
+                                        id: 'testOutput',
+                                        type: 'text',
+                                        label: editor.lang.iudico.output,
+                                        labelLayout: 'horizontal',
+                                        setup: function () {
+                                            this.setValue('');
+                                        }
+                                    }
+                                ]
                             },
                             {
-                                id: 'addChoice',
+                                id: 'addTest',
                                 type: 'button',
-                                label: editor.lang.iudico.addChoice,
+                                label: editor.lang.iudico.addTest,
                                 onClick: function () {
                                     var dialog = this.getDialog(),
-										choices = dialog.getContentElement('choiceTab', 'cmbChoices'),
-                                        choice = dialog.getContentElement('choiceTab', 'choice'),
-                                        correctAnswer = dialog.getContentElement('choiceTab', 'correctAnswer');
+										tests = dialog.getContentElement('testsTab', 'cmbTests'),
+                                        test = dialog.getContentElement('testsTab', 'test');
 
+                                    addOption(tests, test.getValue(), test.getValue(), dialog.getParentEditor().document);
 
-                                    addOption(choices, choice.getValue(), choice.getValue(), dialog.getParentEditor().document);
-                                    addOption(correctAnswer, choice.getValue(), choice.getValue(), dialog.getParentEditor().document);
-
-                                    choice.setValue('');
+                                    test.setValue('');
                                 }
                             }
                         ]
                     },
 
                     {
-                        id: 'question',
-                        type: 'text',
-                        label: editor.lang.iudico.question,
-                        labelLayout: 'horizontal',
+                        type: 'select',
+                        id: 'cmbTests',
+                        label: editor.lang.iudico.tests,
+                        title: '',
+                        size: 5,
+                        style: 'width: 100%; height: 200px',
+                        items: [],
                         commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
-                            if (paramMap['question']) {
-                                paramMap['question'].setAttribute("value", this.getValue());
-                            } else {
-                                paramMap['question'] = this.getDialog().addParam(objectNode, 'question', this.getValue());
+                            for (var i = 0; paramMap['option' + i]; ++i) {
+                                paramMap['option' + i].remove();
+                                delete paramMap['option' + i];
+                            }
+
+                            var dialog = this.getDialog(),
+										choices = dialog.getContentElement('choiceTab', 'cmbChoices'),
+                                        choice = dialog.getContentElement('choiceTab', 'choice'),
+                                        correctAnswer = dialog.getContentElement('choiceTab', 'correctAnswer');
+
+                            choices = getSelect(choices);
+                            correctAnswer = getSelect(correctAnswer);
+
+                            for (var i = 0; i < choices.getChildCount(); ++i) {
+                                paramMap['option' + i] = this.getDialog().addParam(objectNode, 'option' + i, choices.getChild(i).getText());
                             }
                         },
                         setup: function (objectNode, embedNode, paramMap) {
-                            this.setValue(paramMap['question']);
-                        }
-                    },
-                    {
-                        id: 'preCode',
-                        type: 'text',
-                        label: editor.lang.iudico.question,
-                        labelLayout: 'horizontal',
-                        commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
-                            if (paramMap['question']) {
-                                paramMap['question'].setAttribute("value", this.getValue());
-                            } else {
-                                paramMap['question'] = this.getDialog().addParam(objectNode, 'question', this.getValue());
+                            /*var dialog = this.getDialog(),
+                            choices = dialog.getContentElement('choiceTab', 'cmbChoices'),
+                            choice = dialog.getContentElement('choiceTab', 'choice'),
+                            correctAnswer = dialog.getContentElement('choiceTab', 'correctAnswer');
+
+                            removeAllOptions(choices);
+                            removeAllOptions(correctAnswer);
+
+                            for (var i = 0; paramMap['option' + i]; ++i) {
+                            addOption(choices, paramMap['option' + i], paramMap['option' + i], dialog.getParentEditor().document);
+                            var oOption = addOption(correctAnswer, paramMap['option' + i], paramMap['option' + i], dialog.getParentEditor().document);
+
+                            if (paramMap['option' + i] == paramMap['correct']) {
+                            oOption.setAttribute('selected', 'selected');
+                            oOption.selected = true;
                             }
-                        },
-                        setup: function (objectNode, embedNode, paramMap) {
-                            this.setValue(paramMap['question']);
-                        }
-                    },
-                    {
-                        id: 'postCode',
-                        type: 'text',
-                        label: editor.lang.iudico.question,
-                        labelLayout: 'horizontal',
-                        commit: function (objectNode, paramMap, extraStyles, extraAttributes) {
-                            if (paramMap['question']) {
-                                paramMap['question'].setAttribute("value", this.getValue());
-                            } else {
-                                paramMap['question'] = this.getDialog().addParam(objectNode, 'question', this.getValue());
-                            }
-                        },
-                        setup: function (objectNode, embedNode, paramMap) {
-                            this.setValue(paramMap['question']);
+                            }*/
                         }
                     }
 				]
