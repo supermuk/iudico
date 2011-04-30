7bb3735b90574abf6b28caa31378eea84376ba21
gerasymi <gerasymi@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-04-30 17:01:14 +0000

    integrated log4net logging system

---
+++ branches/IUDICO/IUDICO.Common/Log4NetLogger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using log4net;
+using log4net.Config;
+
+
+namespace IUDICO.Common
+{
+    public interface ILoggerService
+    {
+        void Info(string message);
+        void Warn(string message);
+        void Debug(string message);
+        void Error(string message);
+        void Error(Exception ex);
+        void Fatal(string message);
+        void Fatal(Exception ex);
+    }
+
+    public class Log4NetLoggerService : ILoggerService
+    {
+        private ILog _logger;
+
+        public Log4NetLoggerService()
+        {
+            _logger = LogManager.GetLogger(
+                System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+
+
+
+        public void Info(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            _logger.Warn(message);
+        }
+
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Error(Exception ex)
+        {
+            _logger.Error(ex.Message, ex);
+        }
+
+        public void Fatal(string message)
+        {
+            _logger.Fatal(message);
+        }
+
+        public void Fatal(Exception ex)
+        {
+            _logger.Fatal(ex.Message, ex);
+        }
+    }
+}
+
