e280cceab8dd50832df422b39bf79e387eb1fdce
sm.lpml@gmail.com <sm.lpml@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-05-09 21:12:19 +0000

    Added Sequencing Patterns.

--- branches/IUDICO/IUDICO.CourseManagement/Models/ManifestModels/SequencingPatternManager.cs
+++ branches/IUDICO/IUDICO.CourseManagement/Models/ManifestModels/SequencingPatternManager.cs
@@ -5,7 +5,19 @@ namespace IUDICO.CourseManagement.Models.ManifestModels
 {
     public class SequencingPatternManager
     {
-        public static Sequencing ApplyControlChapterSequncing(Sequencing sequencing)
+        public static Sequencing ApplyDefaultChapterSequencing(Sequencing sequencing)
+        {
+            if (sequencing.ControlMode == null)
+            {
+                sequencing.ControlMode = new ControlMode();
+            }
+            sequencing.ControlMode.Choice = true;
+            sequencing.ControlMode.Flow = true;
+
+            return sequencing;
+        }
+
+        public static Sequencing ApplyControlChapterSequencing(Sequencing sequencing)
         {
             if(sequencing.ControlMode == null)
             {
@@ -22,6 +34,19 @@ namespace IUDICO.CourseManagement.Models.ManifestModels
                 sequencing.LimitConditions = new LimitConditions();
             }
             sequencing.LimitConditions.AttemptLimit = 1;
+
+            return sequencing;
+        }
+
+        public static Sequencing ApplyRandomSetSequencingPattern(Sequencing sequencing, int selectCount)
+        {
+            if (sequencing.RandomizationControls == null)
+            {
+                sequencing.RandomizationControls = new RandomizationControls();
+            }
+            sequencing.RandomizationControls.ReorderChildren = true;
+            sequencing.RandomizationControls.SelectionTiming = Timing.Once;
+            sequencing.RandomizationControls.SelectCount = selectCount;
 
             return sequencing;
         }
