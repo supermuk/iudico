ef9ceef04a486cf50d47cb1c0af8dae35cdb789a
pachva92 <pachva92@gmail.com>
2011-05-10 19:21:21 +0000

    Fixing Localization

--- branches/IUDICO/IUDICO.CurriculumManagement/Localization .cs
+++ branches/IUDICO/IUDICO.CurriculumManagement/Localization .cs
@@ -36,7 +36,14 @@ namespace IUDICO.CurriculumManagement
         }
         public static string getMessage(string search)
         {
-            return resource[Thread.CurrentThread.CurrentUICulture.Name][search];
+            try
+            {
+                return resource[Thread.CurrentThread.CurrentUICulture.Name][search];
+            }
+            catch (Exception)
+            {
+                return search;
+            }
         }
     }
 }
