870d01e5f62ce908a7795fdd07bacf53f0e334e3
pachva92 <pachva92@gmail.com>
2011-05-17 08:02:52 +0000

    Fixing Localization

---
+++ branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/plugins/iudico/lang/uk.js
@@ -0,0 +1,39 @@
+﻿CKEDITOR.plugins.setLang('iudico', 'uk-UA',
+{
+    iudico:
+    {
+        title: 'IUDICO',
+
+        simpleTab: 'Simple',
+        choiceTab: 'Choice',
+        compileTab: 'Compile',
+        testsTab: 'Tests',
+
+        correctAnswer: 'Правильна відповідь:',
+        question: 'Питання:',
+        rank: 'Rank:',
+
+        choice: 'Choice:',
+        addChoice: 'Add Choice',
+        deleteChoice: 'Delete Choice',
+
+        language: 'Language:',
+        memoryLimit: 'Memory limit:',
+        outputLimit: 'Output limit:',
+        timeLimit: 'Time limit:',
+        preCode: 'Code before:',
+        postCode: 'Code after:',
+        test: 'Test:',
+        addTest: 'Add Test',
+        deleteTest: 'Delete Test',
+        input: 'Input:',
+        output: 'Output:',
+        compileUrl: 'Compile URL:',
+
+        type: 'Type',
+        toolbar: 'IUDICO',
+        simple: 'Simple',
+        choice: 'Choice',
+        compile: 'Compile'
+    }
+});
