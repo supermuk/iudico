a5f5e790a0392cf27190ccb9230b8e06f4b312e9
osst.os <osst.os@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-10-02 18:49:06 +0000

    New file uploader (not working)

--- branches/IUDICO.RedTeam/IUDICO.CourseManagement/Controllers/NodeController.cs
+++ branches/IUDICO.RedTeam/IUDICO.CourseManagement/Controllers/NodeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Script.Serialization;
 using System.Web.UI.WebControls;
 using System.Xml.Linq;
 using System.Xml.Serialization;
@@ -379,23 +380,53 @@ namespace IUDICO.CourseManagement.Controllers
         }
 
         [HttpPost]
-        public string FileUploader(HttpPostedFileBase datafile)
+        public void FileUploader(HttpContext context)
         {
+            context.Response.ContentType = "text/plain";//"application/json";
+            var r = new System.Collections.Generic.List<ViewDataUploadFilesResult>();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            foreach (string file in context.Request.Files)
+            {
+                HttpPostedFile hpf = context.Request.Files[file] as HttpPostedFile;
+                string FileName = string.Empty;
+                if (HttpContext.Request.Browser.Browser.ToUpper() == "IE")
+                {
+                    string[] files = hpf.FileName.Split(new char[] { '\\' });
+                    FileName = files[files.Length - 1];
+                }
+                else
+                {
+                    FileName = hpf.FileName;
+                }
+                if (hpf.ContentLength == 0)
+                    continue;
+                string savedFileName = "c:\\tmp\\" + FileName;
+                hpf.SaveAs(savedFileName);
 
-            var path = HttpContext.Request.PhysicalApplicationPath;
-
-            path = Path.Combine(path, @"Data\WorkFolder");
-            path = Path.Combine(path, Guid.NewGuid().ToString());
-
-            Directory.CreateDirectory(path);
-
-            path = Path.Combine(path, datafile.FileName.Split('\\').Last());
-
-            datafile.SaveAs(path);
-
-            //_Storage.Import(path, _UserService.GetCurrentUser().Username);
-
-            return path;
+                r.Add(new ViewDataUploadFilesResult()
+                {
+                    //Thumbnail_url = savedFileName,
+                    Name = FileName,
+                    Length = hpf.ContentLength,
+                    Type = hpf.ContentType
+                });
+                var uploadedFiles = new
+                {
+                    files = r.ToArray()
+                };
+                var jsonObj = js.Serialize(uploadedFiles);
+                //jsonObj.ContentEncoding = System.Text.Encoding.UTF8;
+                //jsonObj.ContentType = "application/json;";
+                context.Response.Write(jsonObj.ToString());
+            }
         }
+    }
+
+    public class ViewDataUploadFilesResult
+    {
+        public string Thumbnail_url { get; set; }
+        public string Name { get; set; }
+        public int Length { get; set; }
+        public string Type { get; set; }
     }
 }    
