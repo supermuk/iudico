c1f9f2d1c2c1dbe718b6056885126d84aceb1708
ipetrovych <ipetrovych@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-10-16 15:28:59 +0000

    tests platform

--- branches/IUDICO.GreenTeam/IUDICO.Common/Models/DBDataContext.cs
+++ branches/IUDICO.GreenTeam/IUDICO.Common/Models/DBDataContext.cs
@@ -1,7 +1,30 @@
+using IUDICO.Common.Models.Interfaces;
+
 namespace IUDICO.Common.Models
 {
-    public partial class DBDataContext
+    public partial class DBDataContext : IDataContext
     {
-        
+        IMockableTable<User> IDataContext.Users
+        {
+            get
+            {
+                return new MockableTable<User>(Users);
+            }
+        }
+
+        IMockableTable<Group> IDataContext.Groups
+        {
+            get { return new MockableTable<Group>(Groups); }
+        }
+
+        IMockableTable<GroupUser> IDataContext.GroupUsers
+        {
+            get { return new MockableTable<GroupUser>(GroupUsers); }
+        }
+
+        IMockableTable<UserRole> IDataContext.UserRoles
+        {
+            get { return new MockableTable<UserRole>(UserRoles); }
+        }
     }
 }
