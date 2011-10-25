4f75b3849c0dfe443fdf5ae132fa98200de94293
RomanKravets <RomanKravets@gmail.com>
2011-10-25 18:18:24 +0000

    Analytics plugin added

---
+++ branches/IUDICO/IUDICO.Analytics/Models/Storage/IAnalyticsStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using IUDICO.Common.Models;
+
+namespace IUDICO.Analytics.Models.Storage
+{
+    public interface IAnalyticsStorage
+    {
+        #region Analytics methods
+
+
+        #endregion
+    }
+}
