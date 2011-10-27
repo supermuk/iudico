6189be3cd8f28d09bee6fb87d0428ea669800e28
boblox12 <boblox12@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-10-27 12:20:47 +0000

    Merged Iudico branch to IudicoPinkTeam

--- IUDICO.Common/Models/DBDataContext.cs
+++ IUDICO.Common/Models/DBDataContext.cs
@@ -1,7 +1,25 @@
+using IUDICO.Common.Models.Interfaces;
+
 namespace IUDICO.Common.Models
 {
-    public partial class DBDataContext
+    public partial class DBDataContext : IDataContext
     {
-        
+        IMockableTable<User> IDataContext.Users
+        {
+            get
+            {
+                return new MockableTable<User>(Users);
+            }
+        }
+
+        IMockableTable<Group> IDataContext.Groups
+        {
+            get { return new MockableTable<Group>(Groups); }
+        }
+
+        IMockableTable<GroupUser> IDataContext.GroupUsers
+        {
+            get { return new MockableTable<GroupUser>(GroupUsers); }
+        }
     }
 }
