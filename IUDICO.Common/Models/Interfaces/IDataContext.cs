1391f1c47318216a9756984ce7c3a7bf1649bb5f
akatsyubka <akatsyubka@gmail.com>
2011-10-30 20:33:22 +0000

    Fake Storage for Course Management. Test GetCourse Methods

---
+++ IUDICO.Common/Models/Interfaces/IDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IUDICO.Common.Models.Interfaces
+{
+    public interface IDataContext : IMockableDataContext
+    {
+        IMockableTable<User> Users { get; }
+        IMockableTable<Course> Courses { get; }
+        IMockableTable<CourseUser> CourseUsers { get; }
+        IMockableTable<Node> Nodes { get; }
+        IMockableTable<NodeResource> NodeResources { get; }
+    }
+}
