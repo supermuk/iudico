4cc8e13278f9aef47b18adf9e8b1c0d1c4ef5a79
RomanKravets <RomanKravets@gmail.com>
2011-11-05 18:46:58 +0000

    Add tables for analytics plugin.

---
+++ IUDICO.Database/Schema Objects/Schemas/dbo/Tables/ForecastingResults.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE dbo.ForecastingResults
+	(
+	Id int NOT NULL IDENTITY (1, 1),
+	StudentRef uniqueidentifier NOT NULL,
+	TreeRef int NOT NULL,
+	ForecastingPoint int NULL,
+	ForecastingPercent float(53) NULL
+	)  ON [PRIMARY]
+
+
