b38014bdbf53f5271d8e45bc5e7b86ccb58d1672
ipetrovych <ipetrovych@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-11-07 08:41:11 +0000

    Fixed problems with merging

--- branches/IUDICO/IUDICO.Common/Models/Interfaces/IDataContext.cs
+++ branches/IUDICO/IUDICO.Common/Models/Interfaces/IDataContext.cs
@@ -5,5 +5,6 @@
         IMockableTable<User> Users { get; }
         IMockableTable<Group> Groups { get; }
         IMockableTable<GroupUser> GroupUsers { get; }
+        IMockableTable<UserRole> UserRoles { get; }
     }
 }
