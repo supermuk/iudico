31b95b348e697655c2fb8d9679bee4adc34d87d2
SuperKivi007 <SuperKivi007@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-11-09 23:51:45 +0000

    fixed sql bug

---
+++ branches/IUDICO.PinkTeam/IUDICO.Database/Schema Objects/Schemas/dbo/Tables/Keys/FK_Timelines_CurriculumAssignments.fkey.sql
@@ -0,0 +1,3 @@
+﻿ALTER TABLE [dbo].[Timelines]
+    ADD CONSTRAINT [FK_Timelines_CurriculumAssignments] FOREIGN KEY ([CurriculumAssignmentRef]) REFERENCES [dbo].[CurriculumAssignments] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
