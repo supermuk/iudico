f31529cbf2542250a141b08daded63bc74920c80
2007marsa <2007marsa@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-11-14 15:57:33 +0000

    First tests for curriculum storage

--- branches/IUDICO.PinkTeam/IUDICO.UnitTests/CurriculumManagement/NUnit/CurriculumTests.cs
+++ branches/IUDICO.PinkTeam/IUDICO.UnitTests/CurriculumManagement/NUnit/CurriculumTests.cs
@@ -39,58 +39,10 @@ namespace IUDICO.UnitTests.CurriculumManagement.NUnit
             _Tests.ClearTables();
         }
 
-        [Test]
-        public void SimpleTest()
-        {
-            //заєбашим пару курікулумів. У реальних тестах створення пари курікулумів\стейджів\тем скоріше всього будуть повторюватись тому їх краще
-            //винести в метод типу AddDefaultData();
-            var actualCurriculums = CreateDefaultData();            
-            var expectedCurriculums = new List<Curriculum>()
-            {
-                new Curriculum() { Name = "Curriculum1", Owner="panza", Id=1, IsDeleted=false },
-                new Curriculum() { Name = "Curriculum2", Owner="panza", Id=2, IsDeleted=false },
-                new Curriculum() { Name = "Curriculum3", Owner="panza", Id=3, IsDeleted=false },
-                new Curriculum() { Name = "Curriculum4", Owner="panza", Id=4, IsDeleted=false }
-            };
-
-            //нука перевіримо додавання
-            var ids = actualCurriculums.Select(item => _Storage.AddCurriculum(item)).ToList();
-            actualCurriculums.Select((item, i) => i)
-                .ToList()
-                .ForEach(i => AdvAssert.AreEqual(expectedCurriculums[i], _Storage.GetCurriculum(ids[i])));
-
-            //додамо трохи лоску-стейдж і тему. Проставляйте завжди проперті Curriculum і Stage а не CurriculumRef i StageRef.
-            var aStage = new Stage { Name = "Stage", Curriculum = _Storage.GetCurriculum(expectedCurriculums[0].Id) };//actualStage
-            var eStage = new Stage { Name = "Stage", Curriculum = _Storage.GetCurriculum(expectedCurriculums[0].Id), Id = 1 };//expectedStage
-            _Storage.AddStage(aStage);
-            AdvAssert.AreEqual(eStage, _Storage.GetStage(eStage.Id));
-
-            var aTheme = new Theme { Name = "Theme", Stage = _Storage.GetStage(eStage.Id), ThemeType = _Storage.GetThemeType(1), CourseRef = 1 };
-            var eTheme = new Theme { Name = "Theme", Stage = _Storage.GetStage(eStage.Id), ThemeType = _Storage.GetThemeType(1), CourseRef = 1, Id = 1, };
-            _Storage.AddTheme(aTheme);
-            AdvAssert.AreEqual(eTheme, _Storage.GetTheme(eTheme.Id));
-
-            //нука впіндюримо пару асайментів, тут-то і починається єбздєц. Ніколи не проставляйте проперті CurriculumRef, тільки Curriculum
-            var aCurrAss = new CurriculumAssignment() { Curriculum = _Storage.GetCurriculum(expectedCurriculums[0].Id), UserGroupRef = 1 };
-            var eCurrAss = new CurriculumAssignment() { Curriculum = _Storage.GetCurriculum(expectedCurriculums[0].Id), UserGroupRef = 1, Id = 1 };
-            _Storage.AddCurriculumAssignment(aCurrAss);
-            AdvAssert.AreEqual(eCurrAss, _Storage.GetCurriculumAssignment(eCurrAss.Id));
-
-            //заціним чи проставились максимальні оціночки за тему(див реалізацію додавання курікулум асаймента)
-            var eThemeAss = new ThemeAssignment()
-            {
-                CurriculumAssignment = _Storage.GetCurriculumAssignment(eCurrAss.Id),
-                MaxScore = 1,
-                Theme = _Storage.GetTheme(eTheme.Id),
-                Id = 1
-            };
-            var aThemeAsses = _Storage.GetThemeAssignmentsByCurriculumAssignmentId(eCurrAss.Id).ToList();
-            Assert.AreEqual(1, aThemeAsses.Count);
-            AdvAssert.AreEqual(eThemeAss, aThemeAsses[0]);
-        }
+        
         #region CurriculumMethodsTests
         [Test]
-        public void AddCurriclulum()
+        public void AddCurriculum()
         {
             List<Curriculum> curriculums = CreateDefaultData();
             var ids = curriculums.Select(item => _Storage.AddCurriculum(item)).ToList();
