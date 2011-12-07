f00d447a0a9bed513694b0fd1b56f180839d0d12
sm.lpml@gmail.com <sm.lpml@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-12-07 21:41:56 +0000

    Merged RedTeam branch.

--- branches/IUDICO/IUDICO.Common/Models/Attributes/Interfaces/IMockableTable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Linq;
-using System.Linq;
-
-namespace IUDICO.Common.Models.Interfaces
-{
-    public interface IMockableTable<TEntity> : ITable, IQueryable<TEntity>
-    {
-    }
-}
