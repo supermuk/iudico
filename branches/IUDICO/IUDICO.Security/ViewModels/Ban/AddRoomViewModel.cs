15a9af9b4f1ca0839852b2a6886d61ec699a8220
sm.lpml@gmail.com <sm.lpml@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-12-07 21:27:22 +0000

    Merged RedTeam branch.

---
+++ branches/IUDICO/IUDICO.Security/ViewModels/Ban/AddRoomViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace IUDICO.Security.ViewModels.Ban
+{
+    public class AddRoomViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(11, ErrorMessage = "Must be less than 11 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Allowed is required")]
+        public bool Allowed { get; set; }
+    }
+}
