d2a66adf88a1f5edd75be8ab4dc9b93b59d83b5f
radomba@gmail.com <radomba@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-12-12 19:54:45 +0000

    Fixed 'spoiler' plugin

--- branches/IUDICO.RedTeam/IUDICO.CourseManagement/Scripts/ckeditor/plugins/spoiler/plugin.js
+++ branches/IUDICO.RedTeam/IUDICO.CourseManagement/Scripts/ckeditor/plugins/spoiler/plugin.js
@@ -1,8 +1,3 @@
-/*
-Copyright (c) 2003-2009, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
-
 /**
  * @file Code plugin.
  */
@@ -10,20 +5,20 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 (function()
 {
 var pluginName = 'spoiler';
-// Регистрируем имя плагина .
+
 CKEDITOR.plugins.add( pluginName,
 {
 init : function( editor )
-{//Добавляем команду на нажатие кнопки
+{
 editor.addCommand( pluginName,new CKEDITOR.dialogCommand( 'spoiler' ));
- //Указываем где скрипт окна диалога.
+
 CKEDITOR.dialog.add( pluginName, this.path + 'dialogs/spoiler.js' );
-// Добавляем кнопочку
+
 editor.ui.addButton( 'Spoiler',
 {
-label : 'Добавить Spoiler',//Title кнопки
+label : 'Add Spoiler',
 command : pluginName,
-icon : this.path + 'logo.gif'//Путь к иконке
+icon : this.path + 'logo.gif'
 });
 }
 });
