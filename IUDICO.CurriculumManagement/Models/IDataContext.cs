0bd6d06f76edd50820e19aadeccdc8280547eb96
shopen21 <shopen21@gmail.com>
2011-12-13 22:22:14 +0000

    merged trunk into IUDICO.VioletTeam branch.

---
+++ IUDICO.CurriculumManagement/Models/IDataContext.cs
@@ -0,0 +1,20 @@
+﻿using IUDICO.Common.Models.Shared;
+using IUDICO.Common.Models.Interfaces;
+namespace IUDICO.CurriculumManagement.Models
+{
+    public interface IDataContext : IMockableDataContext
+    {
+        //IMockableTable<Course> Courses { get; }
+        //IMockableTable<User> Users { get; }
+        //IMockableTable<Group> Groups { get; }
+        //IMockableTable<GroupUser> GroupUsers { get; }
+        //IMockableTable<UserRole> UserRoles { get; }
+        IMockableTable<Curriculum> Curriculums { get; }
+        IMockableTable<Stage> Stages { get; }
+        IMockableTable<Theme> Themes { get; }
+        IMockableTable<CurriculumAssignment> CurriculumAssignments { get; }
+        IMockableTable<Timeline> Timelines { get; }
+        IMockableTable<ThemeAssignment> ThemeAssignments { get; }
+        IMockableTable<ThemeType> ThemeTypes { get; }
+    }
+}
