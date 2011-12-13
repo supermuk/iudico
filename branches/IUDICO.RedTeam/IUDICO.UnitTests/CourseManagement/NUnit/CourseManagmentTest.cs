3677dba94a9b18974189270b33f4f26b7667cec1
akatsyubka <akatsyubka@gmail.com>
2011-12-13 23:56:23 +0000

    Fix Fake Storage. Add new Course Managment Tests

--- branches/IUDICO.RedTeam/IUDICO.UnitTests/CourseManagement/NUnit/CourseManagmentTest.cs
+++ branches/IUDICO.RedTeam/IUDICO.UnitTests/CourseManagement/NUnit/CourseManagmentTest.cs
@@ -6,6 +6,7 @@ using IUDICO.Common.Models;
 using IUDICO.Common.Models.Interfaces;
 using IUDICO.Common.Models.Services;
 using IUDICO.CourseManagement.Models.Storage;
+using IUDICO.UserManagement.Models;
 using IUDICO.UserManagement.Models.Storage;
 using Moq;
 using Moq.Protected;
@@ -22,23 +23,25 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
 
         #region Public properties
 
-        public Mock<IDataContext> MockDataContext
+        private Mock<IDataContext> MockDataContext
         {
             get;
-            protected set;
+            set;
         }
 
-        public Mock<ILmsService> MockLmsService
+        private Mock<ILmsService> MockLmsService
         {
             get;
-            protected set;
+            set;
         }
 
-        public Mock<DatabaseUserStorage> MockStorage
+        private Mock<MixedCourseStorage> MockStorage
         {
             get;
-            protected set;
+            set;
         }
+
+        private Mock<UserService> MockUserServise { get; set; }
 
         public IDataContext DataContext
         {
@@ -50,40 +53,40 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
             get { return MockLmsService.Object; }
         }
 
-        public IUserStorage Storage
+        public ICourseStorage Storage
         {
             get { return MockStorage.Object; }
         }
 
-        public Mock<ITable> Users
-        {
-            get;
-            protected set;
-        }
+        //public Mock<ITable> Users
+        //{
+        //    get;
+        //    set;
+        //}
 
-        public Mock<ITable> Courses
-        {
-            get;
-            protected set;
-        }
+        //public Mock<ITable> Courses
+        //{
+        //    get;
+        //    set;
+        //}
 
-        public Mock<ITable> CourseUsers
-        {
-            get;
-            protected set;
-        }
+        //public Mock<ITable> CourseUsers
+        //{
+        //    get;
+        //    set;
+        //}
 
-        public Mock<ITable> Nodes
-        {
-            get;
-            protected set;
-        }
+        //public Mock<ITable> Nodes
+        //{
+        //    get;
+        //    set;
+        //}
 
-        public Mock<ITable> NodeResources
-        {
-            get;
-            protected set;
-        }
+        //public Mock<ITable> NodeResources
+        //{
+        //    get;
+        //    set;
+        //}
 
         #endregion
 
@@ -91,15 +94,13 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
         {
             MockDataContext = new Mock<IDataContext>();
             MockLmsService = new Mock<ILmsService>();
-            MockStorage = new Mock<DatabaseUserStorage>(MockLmsService.Object);
-
-            //Storage = new DatabaseUserStorage(MockLmsService.Object);
-
-            Users = new Mock<ITable>();
-            Courses = new Mock<ITable>();
-            CourseUsers = new Mock<ITable>();
-            Nodes = new Mock<ITable>();
-            NodeResources = new Mock<ITable>();
+            MockStorage = new Mock<MixedCourseStorage>(MockLmsService.Object);
+            MockUserServise = new Mock<UserService>();
+            //Users = new Mock<ITable>();
+            //Courses = new Mock<ITable>();
+            //CourseUsers = new Mock<ITable>();
+            //Nodes = new Mock<ITable>();
+            //NodeResources = new Mock<ITable>();
 
             Setup();
             SetupTables();
@@ -113,8 +114,8 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
         public void Setup()
         {
             MockLmsService.Setup(l => l.GetIDataContext()).Returns(MockDataContext.Object);
-            MockStorage.Protected().Setup<IDataContext>("GetDbContext").Returns(MockDataContext.Object);
-
+            MockLmsService.Setup(l => l.FindService<IUserService>().GetCurrentUser()).Returns(new User { Username = "lex" });
+ //           MockUserServise.Setup(l => l.GetCurrentUser()).Returns(new User {Name = "lex"});
         }
 
         public void SetupTables()
@@ -125,21 +126,40 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
                                            {
                                                Id = Guid.NewGuid(), 
                                                Username = "lex", 
-                                               Password = Storage.EncryptPassword("lex"),
                                            },
                                        new User
                                            {
-                                               Id = Guid.NewGuid(), 
+                                               Id = new Guid("11345200-abe8-4f60-90c8-0d43c5f6c0f6"), 
                                                Username = "user1", 
-                                               Password = Storage.EncryptPassword("user1"),
+                                           },
+                                       new User
+                                           {
+                                               Id = new Guid("22345200-abe8-4f60-90c8-0d43c5f6c0f6"), 
+                                               Username = "user2", 
                                            },
                                        new User
                                            {
                                                Id = Guid.NewGuid(), 
-                                               Username = "user2", 
-                                               Password = Storage.EncryptPassword("user2"),
+                                               Username = "user3", 
+                                           },
+                                       new User
+                                           {
+                                               Id = Guid.NewGuid(), 
+                                               Username = "user4", 
+                                           },
+                                           new User
+                                           {
+                                               Id = new Guid("55345200-abe8-4f60-90c8-0d43c5f6c0f6"), 
+                                               Username = "user5", 
+                                           },
+                                       new User
+                                           {
+                                               Id = new Guid("66345200-abe8-4f60-90c8-0d43c5f6c0f6"), 
+                                               Username = "user6", 
                                            },
                                    };
+
+            MockDataContext.SetupGet(s => s.Users).Returns(new MemoryTable<User>(mockUserData));
 
             var mockCourseData = new[]
                                      {
@@ -164,7 +184,7 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
                                                  Locked = null,
                                                  Name = "#2",
                                                  Nodes = new EntitySet<Node>(),
-                                                 Owner = "lex",
+                                                 Owner = "admin",
                                                  Updated = System.DateTime.Now
 
                                                  },
@@ -202,35 +222,50 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
                                                              Locked = null,
                                                              Name = "#4",
                                                              Nodes =  new EntitySet<Node>(),
-                                                             Owner = "user2",
+                                                             Owner = "lex",
                                                              Updated = System.DateTime.Now
                                                          }
                                      };
+
+            MockDataContext.SetupGet(s => s.Courses).Returns(new MemoryTable<Course>(mockCourseData));
+
             var mockCourseUserData = new[]
                                    {
                                        new CourseUser
                                            {
                                                Course = mockCourseData[0], 
                                                CourseRef = mockCourseData[0].Id, 
-                                               UserRef = mockUserData[0].Id
+                                               UserRef = mockUserData[1].Id
+                                           },
+                                       new CourseUser
+                                           {
+                                               Course = mockCourseData[0], 
+                                               CourseRef = mockCourseData[0].Id, 
+                                               UserRef = mockUserData[2].Id
+                                           },
+                                       new CourseUser
+                                           {
+                                               Course = mockCourseData[0], 
+                                               CourseRef = mockCourseData[0].Id, 
+                                               UserRef = mockUserData[3].Id
                                            },
                                        new CourseUser
                                            {
                                                Course = mockCourseData[1], 
                                                CourseRef = mockCourseData[1].Id, 
-                                               UserRef = mockUserData[0].Id
+                                               UserRef = mockUserData[4].Id
                                            },
                                        new CourseUser
                                            {
                                                Course = mockCourseData[2], 
                                                CourseRef = mockCourseData[2].Id, 
-                                               UserRef = mockUserData[0].Id
+                                               UserRef = mockUserData[5].Id
                                            },
                                        new CourseUser
                                            {
                                                Course = mockCourseData[3], 
                                                CourseRef = mockCourseData[3].Id, 
-                                               UserRef = mockUserData[1].Id
+                                               UserRef = mockUserData[2].Id
                                            },
                                        new CourseUser
                                            {
@@ -239,6 +274,8 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
                                                UserRef = mockUserData[2].Id
                                            }
                                    };
+            MockDataContext.SetupGet(s => s.CourseUsers).Returns(new MemoryTable<CourseUser>(mockCourseUserData));
+
             var mockNodeData = new[]
                                    {
                                        new Node
@@ -250,6 +287,8 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
                                                Name = "Node for some cource"
                                            }
                                    };
+            MockDataContext.SetupGet(s => s.Nodes).Returns(new MemoryTable<Node>(mockNodeData));
+
             var mockNodeResourceData = new[]
                                    {
                                        new NodeResource
@@ -258,18 +297,20 @@ namespace IUDICO.UnitTests.CourseManagement.NUnit
                                                Name = "something))"
                                            }
                                    };
+            MockDataContext.SetupGet(s => s.NodeResources).Returns(new MemoryTable<NodeResource>(mockNodeResourceData));
 
-            var mockUsers = new MockableTable<User>(Users.Object, mockUserData.AsQueryable());
-            var mockCourses = new MockableTable<Course>(Courses.Object, mockCourseData.AsQueryable());
-            var mockCourseUsers = new MockableTable<CourseUser>(CourseUsers.Object, mockCourseUserData.AsQueryable());
-            var mockNodes = new MockableTable<Node>(Nodes.Object, mockNodeData.AsQueryable());
-            var mockNodeResources = new MockableTable<NodeResource>(NodeResources.Object, mockNodeResourceData.AsQueryable());
 
-            MockDataContext.SetupGet(c => c.Users).Returns(mockUsers);
-            MockDataContext.SetupGet(c => c.Courses).Returns(mockCourses);
-            MockDataContext.SetupGet(c => c.CourseUsers).Returns(mockCourseUsers);
-            MockDataContext.SetupGet(c => c.Nodes).Returns(mockNodes);
-            MockDataContext.SetupGet(c => c.NodeResources).Returns(mockNodeResources);
+            //var mockUsers = new MockableTable<User>(Users.Object, mockUserData.AsQueryable());
+            //var mockCourses = new MockableTable<Course>(Courses.Object, mockCourseData.AsQueryable());
+            //var mockCourseUsers = new MockableTable<CourseUser>(CourseUsers.Object, mockCourseUserData.AsQueryable());
+            //var mockNodes = new MockableTable<Node>(Nodes.Object, mockNodeData.AsQueryable());
+            //var mockNodeResources = new MockableTable<NodeResource>(NodeResources.Object, mockNodeResourceData.AsQueryable());
+
+            //MockDataContext.SetupGet(c => c.Users).Returns(mockUsers);
+            //MockDataContext.SetupGet(c => c.Courses).Returns(mockCourses);
+            //MockDataContext.SetupGet(c => c.CourseUsers).Returns(mockCourseUsers);
+            //MockDataContext.SetupGet(c => c.Nodes).Returns(mockNodes);
+            //MockDataContext.SetupGet(c => c.NodeResources).Returns(mockNodeResources);
         }
     }
 }
