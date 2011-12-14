7c0ba875391b381df3c03f2aa120dccd0d13c5d6
voka.vip <voka.vip@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-12-14 06:40:17 +0000

    cosmetic changing

--- branches/IUDICO/IUDICO.Security/Models/Storages/Database/DatabaseBanStorage.cs
+++ branches/IUDICO/IUDICO.Security/Models/Storages/Database/DatabaseBanStorage.cs
@@ -162,8 +162,11 @@ namespace IUDICO.Security.Models.Storages.Database
         {
             using (var context = NewContext())
             {
-                context.Rooms.Attach(room, true);
-                context.Rooms.DeleteOnSubmit(room);
+                var _room = context.Rooms.FirstOrDefault(c => c.Name == room.Name);
+                if (_room != null)
+                {
+                    context.Rooms.DeleteOnSubmit(_room);
+                }
 
                 context.SubmitChanges();
             }
@@ -172,10 +175,10 @@ namespace IUDICO.Security.Models.Storages.Database
         public bool ifBanned(string ipAddress)
         {
             var comp = new Computer();
-
             comp.IpAddress = ipAddress;
             using (var context = NewContext())
             {
+                
                 return context.Computers.Contains(comp);
             }
         }
