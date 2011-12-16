d6f4d53062871762a0e6a082fc9e8d3ef8d3e716
shopen21@gmail.com <shopen21@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2011-12-16 22:29:23 +0000

    merged trunk into IUDICO.VioletTeam.ManualTheme branch.

--- branches/IUDICO.VioletTeam.ManualTheme/IUDICO.UnitTests/UserManagement/NUnit/Register.cs
+++ branches/IUDICO.VioletTeam.ManualTheme/IUDICO.UnitTests/UserManagement/NUnit/Register.cs
@@ -9,22 +9,7 @@ namespace IUDICO.UnitTests.UserManagement.NUnit
     public class Register
     {
         protected UserManagementTests _Tests = UserManagementTests.GetInstance();
-        protected class UserComparer : IEqualityComparer<User>
-        {
-            #region Implementation of IEqualityComparer<in User>
-
-            public bool Equals(User x, User y)
-            {
-                return x.Username == y.Username && x.Email == y.Email;
-            }
-
-            public int GetHashCode(User obj)
-            {
-                return (obj.Username + obj.Email).GetHashCode();
-            }
-
-            #endregion
-        }
+        
         [Test]
         public void RegisterUser()
         {
@@ -36,9 +21,11 @@ namespace IUDICO.UnitTests.UserManagement.NUnit
                 Email = "yn@gmail.com",
                 Name = "Nestor"
             };
+
             _Tests.Storage.RegisterUser(model);
 
             User temp = new User { Username = "nestor", Email = "yn@gmail.com", Password = "1234", Name = "Nestor" };
+
             Assert.AreEqual(temp.Username, _Tests.Storage.GetUser(u => u.Username == "nestor").Username);
         }
     }
