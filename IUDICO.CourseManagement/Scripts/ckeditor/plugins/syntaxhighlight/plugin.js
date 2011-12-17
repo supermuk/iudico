61b7231dc398a65d68da3b2cd0a9fb6d24a95056
sm.lpml <sm.lpml@gmail.com>
2011-12-17 14:10:02 +0000

    Merged RedTeam branch (syntax highlight).

---
+++ IUDICO.CourseManagement/Scripts/ckeditor/plugins/syntaxhighlight/plugin.js
@@ -0,0 +1,26 @@
+CKEDITOR.plugins.add("syntaxhighlight", {
+    requires: ["dialog"],
+    lang: ["en"],
+    init: function (a) {
+        var b = "syntaxhighlight";
+        var c = a.addCommand(b, new CKEDITOR.dialogCommand(b));
+        c.modes = {
+            wysiwyg: 1,
+            source: 1
+        };
+        c.canUndo = false;
+        a.ui.addButton("Code", {
+            label: a.lang.syntaxhighlight.title,
+            command: b,
+            icon: this.path + "images/syntaxhighlight.gif"
+        });
+        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(this.path + "scripts/sh_main.js"));
+        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(this.path + "scripts/sh_style.js"));
+        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(this.path + "scripts/sh_csharp.min.js"));
+        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(this.path + "scripts/sh_cpp.min.js"));
+        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(this.path + "scripts/sh_java.min.js"));
+        CKEDITOR.scriptLoader.load(CKEDITOR.getUrl(this.path + "scripts/sh_xml.min.js"));
+
+        CKEDITOR.dialog.add(b, this.path + "dialogs/syntaxhighlight.js");
+    }
+});
---
+++ IUDICO.CourseManagement/Scripts/ckeditor/plugins/syntaxhighlight/scripts/sh_style.js
@@ -0,0 +1,64 @@
+
+var sh_defstyle = {
+  sh_keyword : "color: blue; font-weight: bold; " ,           /* language keywords */
+  sh_type : "color: darkgreen; " ,                            /* basic types */
+  sh_usertype : "color: teal; " ,                             /* user defined types */
+  sh_string : "color: red; font-family: monospace; " ,        /* strings and chars */
+  sh_regexp : "color: orange; font-family: monospace; " ,     /* regular expressions */
+  sh_specialchar : "color: pink; font-family: monospace; " ,  /* e.g.  \n  \t  \\ */
+  sh_comment : "color: brown; font-style: italic; " ,         /* comments */
+  sh_number : "color: purple; " ,                             /* literal numbers */
+  sh_preproc : "color: darkblue; font-weight: bold; " ,       /* e.g.  #include  import */
+  sh_symbol : "color: darkred; " ,                            /* e.g.  <  >  + */
+  sh_function : "color: black; font-weight: bold; " ,         /* function calls and declarations */
+  sh_cbracket : "color: black; " ,                              /* block brackets (e.g.  {  " ,) */
+  sh_todo : "font-weight: bold; background-color: cyan; " ,   /* TODO and FIXME */
+
+/* Predefined variables and functions (for instance glsl) */
+  sh_predef_var : "color: darkblue; " ,
+  sh_predef_func : "color: darkblue; font-weight: bold; " ,
+
+/* for OOP */
+  sh_classname : "color: teal; " ,
+
+/* line numbers (not yet implemented) */
+  sh_linenum : "color: black; font-family: monospace; " ,
+
+/* Internet related */
+  sh_url : "color: blue; text-decoration: underline; font-family: monospace; " ,
+
+/* for ChangeLog and Log files */
+  sh_date : "color: blue; font-weight: bold; " ,
+  sh_time : "color: darkblue; font-weight: bold; " ,
+  sh_file : "color: darkblue; font-weight: bold; " ,
+  sh_ip   : "color: darkgreen; " ,
+  sh_name : "color: darkgreen; " ,
+
+/* for Prolog  Perl... */
+  sh_variable : "color: darkgreen; " ,
+
+/* for LaTeX */
+  sh_italics : "color: darkgreen; font-style: italic; " ,
+  sh_bold : "color: darkgreen; font-weight: bold; " ,
+  sh_underline : "color: darkgreen; text-decoration: underline; " ,
+  sh_fixed : "color: green; font-family: monospace; " ,
+  sh_argument : "color: darkgreen; " ,
+  sh_optionalargument : "color: purple; " ,
+  sh_math : "color: orange; " ,
+  sh_bibtex : "color: blue; " ,
+
+/* for diffs */
+  sh_oldfile : "color: orange; " ,
+  sh_newfile : "color: darkgreen; " ,
+  sh_difflines : "color: blue; " ,
+
+/* for css */
+  sh_selector : "color: purple; " ,
+  sh_property : "color: blue; " ,
+  sh_value : "color: darkgreen; font-style: italic; " ,
+
+/* other */
+  sh_section : "color: black; font-weight: bold; " ,
+  sh_paren : "color: red; " ,
+  sh_attribute : "color: darkgreen; " 
+};
