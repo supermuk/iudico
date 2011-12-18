9aac3c435a6831d104685300d38272dbfe76dcea
Boblox12 <Boblox12@gmail.com>
2011-12-18 22:25:11 +0000

    modified SearchPlugin

--- branches/IUDICO/IUDICO.Search/Models/ViewDataClasses/CheckBoxModel.cs
+++ branches/IUDICO/IUDICO.Search/Models/ViewDataClasses/CheckBoxModel.cs
@@ -10,5 +10,31 @@ namespace IUDICO.Search.Models.ViewDataClasses
         public bool IsChecked { get; set; }
         public string Text { get; set; }
         public SearchType SearchType { get; set; }
+
+        public CheckBoxModel()
+        {
+
+        }
+
+        public CheckBoxModel(SearchType searchType)
+        {
+            SearchType = searchType;
+            IsChecked = true;
+            switch (searchType)
+            {
+                case SearchType.Courses:
+                    Text="Courses";
+                    break;
+                case SearchType.Curriculums:
+                    Text = "Curriculums";
+                    break;
+                case SearchType.Themes:
+                    Text = "Themes";
+                    break;
+                case SearchType.Users:
+                    Text = "Users";
+                    break;
+            }
+        }
     }
 }
