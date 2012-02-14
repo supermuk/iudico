817c40894883f1c9f50a34a50757b9cd23d22c18
ipetrovych <ipetrovych@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-02-14 16:55:51 +0000

    fixing Recommender systems Anomaly Detection for Topics per Group using 2 metrics (difficulty + rating difference)

---
+++ branches/IUDICO/IUDICO.Analytics/Models/TopicSimilarity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IUDICO.Analytics.Models
+{
+    public class TopicSimilarity
+    {
+        public TopicSimilarity(int t1, int t2, double sim)
+        {
+            Topic1Id = t1;
+            Topic2Id = t2;
+            Similarity = sim;
+        }
+
+        public int Topic1Id { get; protected set; }
+        public int Topic2Id { get; protected set; }
+        public double Similarity { get; protected set; }
+    }
+}
