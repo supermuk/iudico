42dd4a8857e87a10d2dda2db9b87a93107e5149c
SuperKivi007 <SuperKivi007@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-02-20 22:22:30 +0000

    added: compile service unit tests

---
+++ branches/IUDICO/IUDICO.UnitTests/CompileService/CompileServiceHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using CompileSystem.Compiling.Compile;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IUDICO.UnitTests.CompileService
+{
+    class CompileServiceHelper
+    {
+        public static bool ValidatePath(string path)
+        {
+            if (path != "")
+            {
+                bool isExist = File.Exists(path);
+                if (isExist)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return false;
+        }
+
+        public static void ValidateCorrectCompilationResult(CompileResult result)
+        {
+            Assert.AreEqual(result.Compiled,true);
+        }
+
+        public static void ValidateIncorrectCompilationResult(CompileResult result)
+        {
+            Assert.AreEqual(result.Compiled, false);
+            Assert.AreNotEqual(result.StandartOutput, "");
+        }
+    }
+}
---
+++ branches/IUDICO/IUDICO.UnitTests/CompileService/UnitTests/MemoryCounterTest.cs
@@ -0,0 +1,80 @@
+﻿using TestingSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
+using System.Diagnostics;
+using System.Timers;
+
+namespace IUDICO.UnitTests
+{
+    /// <summary>
+    ///This is a test class for MemoryCounterTest and is intended
+    ///to contain all MemoryCounterTest Unit Tests
+    ///</summary>
+    [TestClass]
+    public class MemoryCounterTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        /// <summary>
+        ///A test for MemoryCounter Constructor
+        ///</summary>
+        [TestMethod]
+        public void MemoryCounterConstructorTest()
+        {
+            Process process = new Process();
+            int interval = 0;
+
+            //Correct interval && correct process
+            try
+            {
+                MemoryCounter memoryCounter = new MemoryCounter(process, interval);
+                Assert.AreEqual(true, true);
+            }
+            catch(Exception)
+            {
+                Assert.AreEqual(true,false);
+            }
+
+            //Correct interval && null process
+            try
+            {
+                process = null;
+                MemoryCounter memoryCounter = new MemoryCounter(process, interval);
+                Assert.AreEqual(false, true);
+            }
+            catch (Exception)
+            {
+                Assert.AreEqual(true, true);
+            }
+
+            //Incorrect(minus eq 0) interval && correct process
+            try
+            {
+                interval = 0;
+                MemoryCounter memoryCounter = new MemoryCounter(process, interval);
+                Assert.AreEqual(false, true);
+            }
+            catch (Exception)
+            {
+                Assert.AreEqual(true, true);
+            }
+        }
+    }
+}
