26ebc738cf30a1aae944ed9fc3c1327a58886a3c
nobisvitaliy <nobisvitaliy@gmail.com>
2012-03-09 16:43:39 +0000

    Add code.

---
+++ IUDICO.Analytics/Models/AnomalyDetectionAlg/TrainingSet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IUDICO.Analytics.Models.AnomalyDetectionAlg
+{
+    public class TrainingSet
+    {
+        public List<double[]> set;
+
+        public TrainingSet() 
+        {
+            this.set = new List<double[]>();
+        }
+    }
+}
