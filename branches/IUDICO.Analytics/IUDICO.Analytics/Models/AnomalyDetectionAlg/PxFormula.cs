fca5657e8a66fadf94331518b72538906eba8e0a
nobisvitaliy@gmail.com <nobisvitaliy@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-03-09 16:43:39 +0000

    Add code.

---
+++ branches/IUDICO.Analytics/IUDICO.Analytics/Models/AnomalyDetectionAlg/PxFormula.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IUDICO.Analytics.Models.AnomalyDetectionAlg
+{
+    public class PxFormula
+    {
+        // to do
+        private double[] nu;
+        //private Matrix teta;
+        private double e;
+
+        public PxFormula(double[] nu /*, Matrix sigma,*/, double e)
+        {
+            this.nu = nu;
+            this.e = e;
+        }
+
+        public double calculate(double[] x)
+        {
+            // to do
+            return 1;
+        }
+
+        /// <summary>
+        /// This method return true/false answer for question, if passed values are anomalies or not.
+        /// </summary>
+        /// <param name="x">vector of values to check</param>
+        /// <returns> true - is anomaly, false - normal entry</returns>
+        public bool isAnomaly(double[] x)
+        {
+            return calculate(x) < e;
+        }
+    }
+}
