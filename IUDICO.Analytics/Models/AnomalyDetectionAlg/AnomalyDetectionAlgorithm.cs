faf6016e62442c2b11ca5136fba946fe013671c8
nobisvitaliy <nobisvitaliy@gmail.com>
2012-03-15 10:49:00 +0000

    Add Demo code.

--- IUDICO.Analytics/Models/AnomalyDetectionAlg/AnomalyDetectionAlgorithm.cs
+++ IUDICO.Analytics/Models/AnomalyDetectionAlg/AnomalyDetectionAlgorithm.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using IUDICO.Analytics.Models.AnomalyDetectionAlg.Accuracy;
 using Accord.Math;
+using IUDICO.Common.Models.Shared;
+using IUDICO.Common.Models.Shared.CurriculumManagement;
+using IUDICO.Common.Models.Shared.Statistics;
 
 namespace IUDICO.Analytics.Models.AnomalyDetectionAlg
 {
@@ -18,7 +21,7 @@ namespace IUDICO.Analytics.Models.AnomalyDetectionAlg
         /// This field contain object that implement IAccuracy interface.
         /// This object automaticaly calculate value of PxFormula e field.
         /// </summary>
-        private IAccuracy accuracy;
+        private IAccuracy accuracy = new TrueNegativAccuracy();
 
         /// <summary>
         /// This method required training sets for algorithm training and return PxFormula with setted algorithm values.
@@ -41,10 +44,11 @@ namespace IUDICO.Analytics.Models.AnomalyDetectionAlg
         /// <returns>PxFormula class object with setted nu vector and sigma matrix values.</returns>
         private PxFormula calcFormula (TrainingSet trainingSet)
         {
-            int training_set_count = trainingSet.set.Count;
+            int training_set_count = trainingSet.getCountOfRecords();
+
             // calculate nu vector value
             double[] nu = new double[2];
-            foreach (double[] feature_vector in trainingSet.set)
+            foreach (double[] feature_vector in trainingSet.getAllRecords())
             {
                 nu = Matrix.Add(nu, feature_vector);
             }
@@ -52,12 +56,13 @@ namespace IUDICO.Analytics.Models.AnomalyDetectionAlg
 
             // calculate sigme matrix
             double[,] sigma = new double[2,2];
-            foreach (double[] feature_vector in trainingSet.set)
+            foreach (double[] feature_vector in trainingSet.getAllRecords())
             {
                 double[] x_nu = Matrix.Subtract(feature_vector, nu);
                 sigma = Matrix.Add(sigma, Matrix.Multiply(Matrix.ColumnVector(x_nu), Matrix.RowVector(x_nu)));
             }
             sigma = Matrix.Divide(sigma, training_set_count);
+
             // return PxFormula class object with setted nu and sigma values
             return new PxFormula(nu, sigma);
         }
@@ -69,6 +74,58 @@ namespace IUDICO.Analytics.Models.AnomalyDetectionAlg
         public void setAccuracy (IAccuracy newAccuracy)
         {
             accuracy = newAccuracy;
+        }
+
+
+        // For demo
+
+        public static IEnumerable<KeyValuePair<KeyValuePair<User, AttemptResult>, bool>> runAlg(IEnumerable<KeyValuePair<User, AttemptResult>> studentsAndMarks, string[] ts1, string[] ts2n, string[] ts2a)
+        {
+            var trainingSet1Resource = studentsAndMarks.Where(x => ts1.Contains(x.Key.OpenId) == true);
+            var trainingSet2NormalResource = studentsAndMarks.Where(x => ts2n.Contains(x.Key.OpenId) == true);
+            var trainingSet2AnomaliesResource = studentsAndMarks.Where(x => ts2a.Contains(x.Key.OpenId) == true);
+
+            var trainingSet1 = createTrainingSetFromResources(trainingSet1Resource);
+            var trainingSet2Normal = createTrainingSetFromResources(trainingSet2NormalResource);
+            var trainingSet2Anomalies = createTrainingSetFromResources(trainingSet2AnomaliesResource);
+
+            AnomalyDetectionAlgorithm train = new AnomalyDetectionAlgorithm();
+
+            var formula = train.trainAlgorithm(trainingSet1, trainingSet2Normal, trainingSet2Anomalies);
+            List<KeyValuePair<KeyValuePair<User, AttemptResult>, bool>> res = new List<KeyValuePair<KeyValuePair<User, AttemptResult>, bool>>();
+            foreach (KeyValuePair<User, AttemptResult> studentAndMark in studentsAndMarks.Except(trainingSet1Resource).Except(trainingSet2NormalResource).Except(trainingSet2AnomaliesResource))
+            {
+                bool isAnomaly = formula.isAnomaly(getFeaturesValues(studentAndMark));
+
+                res.Add(new KeyValuePair<KeyValuePair<User, AttemptResult>, bool>(studentAndMark, isAnomaly));
+            }
+
+            return res;
+        }
+
+        private static TrainingSet createTrainingSetFromResources(IEnumerable<KeyValuePair<User, AttemptResult>> studentsAndMarks)
+        {
+            TrainingSet resultTrainingSet = new TrainingSet(2);
+            foreach (KeyValuePair<User, AttemptResult> studentAndMark in studentsAndMarks)
+            {
+                double[] newRecord = new double[2];
+                double mark = studentAndMark.Value.Score.ToPercents().Value;
+                double time = studentAndMark.Value.FinishTime.Value.Subtract(studentAndMark.Value.StartTime.Value).TotalSeconds;
+                newRecord[0] = mark;
+                newRecord[1] = time;
+                resultTrainingSet.addRecord(newRecord);
+            }
+            return resultTrainingSet;
+        }
+
+        private static double[] getFeaturesValues(KeyValuePair<User, AttemptResult> studentAndMark)
+        {
+            double[] newRecord = new double[2];
+            double mark = studentAndMark.Value.Score.ToPercents().Value;
+            double time = studentAndMark.Value.FinishTime.Value.Subtract(studentAndMark.Value.StartTime.Value).TotalSeconds;
+            newRecord[0] = mark;
+            newRecord[1] = time;
+            return newRecord;
         }
     }
 }
