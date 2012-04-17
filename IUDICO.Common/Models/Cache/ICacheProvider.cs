70a23b4c314bd2cc0d1bb05f79d49cd65ef6efc0
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-17 23:54:14 +0000

    Initial Cache

---
+++ IUDICO.Common/Models/Cache/ICacheProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IUDICO.Common.Models.Cache
+{
+    public interface ICacheProvider
+    {
+        //private static readonly object CacheLockObject = new object();
+
+        object this[string key]
+        {
+            get;
+            set;
+        }
+
+        void SetKeyPrefix(string keyPrefix);
+
+        void Update(string key, object value);
+        void Expire(string key);
+
+        void AddTag(string keyTag, string key);
+        void DeleteTag(string keyTag, string key);
+        void UpdateTag(string keyTag, List<string> tags);
+        void ExpireTag(string keyTag);
+        List<string> GetTagList(string keyTag);
+    }
+}
