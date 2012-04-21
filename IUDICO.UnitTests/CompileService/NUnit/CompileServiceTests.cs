9e3a83755fe0888ac5c0d33f26d274c704a76dd5
SuperKivi007 <SuperKivi007@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-21 11:49:41 +0000

    added NUnit tests fixed Status.cs

---
+++ IUDICO.UnitTests/CompileService/NUnit/CompileServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using CompileSystem.Classes.Testing;
+using NUnit.Framework;
+
+namespace IUDICO.UnitTests.CompileService.NUnit
+{
+    [TestFixture]
+    public class CompileServiceTests
+    {
+        private readonly CompileSystem.CompileService _compileService = new CompileSystem.CompileService();
+        private readonly string[] _input = new string[0];
+        private readonly string[] _output = new string[0];
+        private readonly string[] _inputStrings = new string[1] { "1 2" };
+        private readonly string[] _outputStrings = new string[1] { "12" };
+        private readonly string _language = "CPP8";
+
+        [Test]
+        public void CompileServiceCompileTest()
+        {
+            string expected = _compileService.Compile(CompileServiceLanguageSourceCode.CPPCorrectSourceCode,
+                                                      _language, _inputStrings, _outputStrings, 1000, 1000);
+            Assert.AreEqual(expected, "Accepted");
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void CompileServiceIncorrectLanguageTest()
+        {
+            _compileService.Compile(CompileServiceLanguageSourceCode.CPPCorrectSourceCode,
+                                    "IncorrectLanguageName", _input, _output, 100, 100);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void CompileServiceEmptyLanguageTest()
+        {
+            _compileService.Compile(CompileServiceLanguageSourceCode.CPPCorrectSourceCode, "", _input, _output, 100, 100);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void CompileServiceIncorrectTimelimitTest()
+        {
+            _compileService.Compile(CompileServiceLanguageSourceCode.CPPCorrectSourceCode, _language, _inputStrings,
+                                    _outputStrings, -5, 100);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void CompileServiceIncorrectMemorylimitTest()
+        {
+            _compileService.Compile(CompileServiceLanguageSourceCode.CPPCorrectSourceCode, _language, _inputStrings,
+                                    _outputStrings, 100, -5);
+        }
+    }
+}
---
+++ IUDICO.UnitTests/CompileService/NUnit/CompilerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using CompileSystem.Classes.Compiling;
+using CompileSystem.Classes.Testing;
+using NUnit.Framework;
+
+namespace IUDICO.UnitTests.CompileService.NUnit
+{
+    [TestFixture]
+    public class CompilerTests
+    {
+        private readonly Compiler _compiler = new Compiler
+        {
+            Name = "CPP",
+            Location = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Compilers\CPP8\Compiler\CL.EXE"),
+            Extension = "cpp",
+            Arguments =
+                "/I\"$CompilerDirectory$\" $SourceFilePath$ /link /LIBPATH:\"$CompilerDirectory$\"",
+            CompiledExtension = "exe",
+            IsNeedShortPath = true
+        };
+
+        [Test]
+        public void CompileTest()
+        {
+            var filePath = CompileSystem.Classes.Helper.CreateFileForCompilation(CompileServiceLanguageSourceCode.CPPCorrectSourceCode, _compiler.Extension);
+
+            try
+            {
+                string output;
+                string error;
+                var result = _compiler.Compile(filePath, out output, out error);
+                Assert.AreEqual(true, result);
+            }
+            catch (Exception)
+            {
+                File.Delete(filePath);
+            }
+
+            //remove file
+            File.Delete(filePath);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void CompilerBadFilePathTest()
+        {
+            string output, error;
+            _compiler.Compile("BadFileName", out output, out error);
+        }
+    }
+}
