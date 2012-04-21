ee1ba39a299d2aef82edcec194c93d71764d6383
nobisvitaliy <nobisvitaliy@gmail.com>
2012-04-21 14:38:39 +0000

    Remove broken test.

--- branches/IUDICO.Analytics/IUDICO.UnitTests/Analytics/AnomalyDetection/Test.cs
+++ branches/IUDICO.Analytics/IUDICO.UnitTests/Analytics/AnomalyDetection/Test.cs
@@ -15,41 +15,41 @@ namespace IUDICO.UnitTests.Analytics.AnomalyDetection
         [TestMethod]
         public void test1()
         {
-            AnomalyDetectionAlgorithm test = new AnomalyDetectionAlgorithm();
-            test.setAccuracy(new TrueNegativAccuracy());
-            TrainingSet set1 = new TrainingSet(2);
-            set1.addRecord(new double[] { 19, 25 });
-            set1.addRecord(new double[] { 12, 40 });
-            set1.addRecord(new double[] { 8, 14 });
-            set1.addRecord(new double[] { 5, 12 });
-            set1.addRecord(new double[] { 15, 15 });
-            set1.addRecord(new double[] { 9, 30 });
-            set1.addRecord(new double[] { 10, 17 });
-            set1.addRecord(new double[] { 18, 13 });
-            set1.addRecord(new double[] { 14, 25 });
+            //AnomalyDetectionAlgorithm test = new AnomalyDetectionAlgorithm();
+            //test.setAccuracy(new TrueNegativAccuracy());
+            //TrainingSet set1 = new TrainingSet(2);
+            //set1.addRecord(new double[] { 19, 25 });
+            //set1.addRecord(new double[] { 12, 40 });
+            //set1.addRecord(new double[] { 8, 14 });
+            //set1.addRecord(new double[] { 5, 12 });
+            //set1.addRecord(new double[] { 15, 15 });
+            //set1.addRecord(new double[] { 9, 30 });
+            //set1.addRecord(new double[] { 10, 17 });
+            //set1.addRecord(new double[] { 18, 13 });
+            //set1.addRecord(new double[] { 14, 25 });
 
-            TrainingSet set2nonAn = new TrainingSet(2);
-            set2nonAn.addRecord(new double[] { 10, 7 });
-            set2nonAn.addRecord(new double[] { 8, 10 });
-            set2nonAn.addRecord(new double[] { 16, 10 });
-            set2nonAn.addRecord(new double[] { 10, 20 });
-            set2nonAn.addRecord(new double[] { 8, 14 });
+            //TrainingSet set2nonAn = new TrainingSet(2);
+            //set2nonAn.addRecord(new double[] { 10, 7 });
+            //set2nonAn.addRecord(new double[] { 8, 10 });
+            //set2nonAn.addRecord(new double[] { 16, 10 });
+            //set2nonAn.addRecord(new double[] { 10, 20 });
+            //set2nonAn.addRecord(new double[] { 8, 14 });
 
-            TrainingSet set2an = new TrainingSet(2);
-            set2an.addRecord(new double[] { 20, 5 });
-            set2an.addRecord(new double[] { 10, 2 });
-            set2an.addRecord(new double[] { 18, 3 });
+            //TrainingSet set2an = new TrainingSet(2);
+            //set2an.addRecord(new double[] { 20, 5 });
+            //set2an.addRecord(new double[] { 10, 2 });
+            //set2an.addRecord(new double[] { 18, 3 });
 
-            PxFormula f = test.trainAlgorithm(set1, set2nonAn, set2an);
-            bool f1 = f.isAnomaly(new double[] { 17, 4 });
-            bool f2 = f.isAnomaly(new double[] { 16, 4 });
-            bool f3 = f.isAnomaly(new double[] { 15, 4 });
-            bool f4 = f.isAnomaly(new double[] { 16, 5 });
-            bool f5 = f.isAnomaly(new double[] { 15, 5 });
+            //PxFormula f = test.trainAlgorithm(set1, set2nonAn, set2an);
+            //bool f1 = f.isAnomaly(new double[] { 17, 4 });
+            //bool f2 = f.isAnomaly(new double[] { 16, 4 });
+            //bool f3 = f.isAnomaly(new double[] { 15, 4 });
+            //bool f4 = f.isAnomaly(new double[] { 16, 5 });
+            //bool f5 = f.isAnomaly(new double[] { 15, 5 });
 
-            bool f6 = f.isAnomaly(new double[] { 10, 5 });
-            bool f7 = f.isAnomaly(new double[] { 10, 6 });
-            bool f8 = f.isAnomaly(new double[] { 10, 7 });
+            //bool f6 = f.isAnomaly(new double[] { 10, 5 });
+            //bool f7 = f.isAnomaly(new double[] { 10, 6 });
+            //bool f8 = f.isAnomaly(new double[] { 10, 7 });
             Assert.Fail();
         }
     }
