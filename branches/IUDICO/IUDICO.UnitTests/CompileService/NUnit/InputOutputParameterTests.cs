6767c877f4c7f0ca4a14d0bc4bcf2b220ded8119
SuperKivi007 <SuperKivi007@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-22 19:49:05 +0000

    added new NUnit tests

---
+++ branches/IUDICO/IUDICO.UnitTests/CompileService/NUnit/InputOutputParameterTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace IUDICO.UnitTests.CompileService.NUnit
+{
+    [TestFixture]
+    public class InputOutputParameterTests
+    {
+        private readonly CompileSystem.CompileService _compileService = new CompileSystem.CompileService();
+
+        #region CPP tests
+
+        [Test]
+        public void CorrectCPPInputOutputParamTest()
+        {
+            string actualResult = _compileService.Compile(CompileServiceLanguageSourceCode.CPPCorrectSourceCode,
+                                                          CompileServiceHelper.CPPLanguageName,
+                                                          CompileServiceHelper.Input,
+                                                          CompileServiceHelper.Output,
+                                                          CompileServiceHelper.TimeLimit,
+                                                          CompileServiceHelper.MemoryLimit);
+
+            Assert.AreEqual(CompileServiceHelper.AcceptedTestResult, actualResult);
+        }
+
+        [Test]
+        public void IncorrectOneCPPInputOutputParamTest()
+        {
+            //first parameter fail
+            string[] inputParam = {"2 5", "7 5"};
+            string[] outputParam = {"35", "75"};
+
+            string actualResult = _compileService.Compile(CompileServiceLanguageSourceCode.CPPIncorrectSourceCode,
+                                                          CompileServiceHelper.CPPLanguageName,
+                                                          inputParam,
+                                                          outputParam,
+                                                          CompileServiceHelper.TimeLimit,
+                                                          CompileServiceHelper.MemoryLimit);
+
+            Assert.AreEqual(CompileServiceHelper.WrongAnswerOneResult, actualResult);
+        }
+
+        [Test]
+        public void IncorrectTwoCPPInputOutputParamTest()
+        {
+            //second parameter fail
+            string[] inputParam = { "2 5", "7 5" };
+            string[] outputParam = { "25", "95" };
+
+            string actualResult = _compileService.Compile(CompileServiceLanguageSourceCode.CPPIncorrectSourceCode,
+                                                          CompileServiceHelper.CPPLanguageName,
+                                                          inputParam,
+                                                          outputParam,
+                                                          CompileServiceHelper.TimeLimit,
+                                                          CompileServiceHelper.MemoryLimit);
+
+            Assert.AreEqual(CompileServiceHelper.WrongAnswerTwoResult, actualResult);
+        }
+
+        #endregion
+    }
+}
---
+++ branches/IUDICO/IUDICO.UnitTests/CompileService/NUnit/TimeLimitTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace IUDICO.UnitTests.CompileService.NUnit
+{
+    [TestFixture]
+    public class TimeLimitTests
+    {
+        private readonly CompileSystem.CompileService _compileService = new CompileSystem.CompileService();
+
+        #region CPP tests
+
+        [Test]
+        public void CorrectCPPTimeLimitTest()
+        {
+
+            string actualResult = _compileService.Compile(CompileServiceLanguageSourceCode.CPPCorrectSourceCode,
+                                                          CompileServiceHelper.CPPLanguageName,
+                                                          CompileServiceHelper.Input,
+                                                          CompileServiceHelper.Output,
+                                                          CompileServiceHelper.TimeLimit,
+                                                          CompileServiceHelper.MemoryLimit);
+
+            Assert.AreEqual(CompileServiceHelper.AcceptedTestResult, actualResult);
+        }
+
+        [Test]
+        public void IncorrectCPPTimeLimitTest()
+        {
+            string actualResult = _compileService.Compile(CompileServiceLanguageSourceCode.CPPIncorrectSourceCode,
+                                                          CompileServiceHelper.CPPLanguageName,
+                                                          CompileServiceHelper.Input,
+                                                          CompileServiceHelper.Output,
+                                                          1,
+                                                          CompileServiceHelper.MemoryLimit);
+
+            Assert.AreEqual(CompileServiceHelper.TimeLimitOneResult, actualResult);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void NullCPPTimeLimitTest()
+        {
+            string actualResult = _compileService.Compile(CompileServiceLanguageSourceCode.CPPIncorrectSourceCode,
+                                                          CompileServiceHelper.CPPLanguageName,
+                                                          CompileServiceHelper.Input,
+                                                          CompileServiceHelper.Output,
+                                                          0,
+                                                          CompileServiceHelper.MemoryLimit);
+        }
+
+        [Test]
+        [ExpectedException(typeof(Exception))]
+        public void LessNullCPPTimeLimitTest()
+        {
+            string actualResult = _compileService.Compile(CompileServiceLanguageSourceCode.CPPIncorrectSourceCode,
+                                                          CompileServiceHelper.CPPLanguageName,
+                                                          CompileServiceHelper.Input,
+                                                          CompileServiceHelper.Output,
+                                                          -5,
+                                                          CompileServiceHelper.MemoryLimit);
+        }
+
+        #endregion
+    }
+}
