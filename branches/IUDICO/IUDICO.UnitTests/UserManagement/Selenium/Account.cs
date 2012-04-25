82500690c910364565ebb32d5eb16ede60543d84
ipetrovych@gmail.com <ipetrovych@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-25 22:08:59 +0000

    UM Selenium fix

--- branches/IUDICO/IUDICO.UnitTests/UserManagement/Selenium/Account.cs
+++ branches/IUDICO/IUDICO.UnitTests/UserManagement/Selenium/Account.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Text;
-using IUDICO.UnitTests.Base;
+﻿using IUDICO.UnitTests.Base;
 using NUnit.Framework;
-using Selenium;
 
 namespace IUDICO.UnitTests.UserManagement.Selenium
 {
@@ -15,18 +12,22 @@ namespace IUDICO.UnitTests.UserManagement.Selenium
             this.DefaultLogin();
 
             Assert.IsTrue(this.selenium.IsElementPresent("//a[contains(@href, '/Account/Index')]"));
-            Assert.IsTrue(this.selenium.IsTextPresent("Logged in as lex"));
+            Assert.IsTrue(this.selenium.IsTextPresent("Logged in as"));
+
+            this.Logout();
         }
 
         [Test]
         public void GetCurrentlyLoggedInUserWhenNotLogged()
         {
             this.selenium.Open("/");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.selenium.WaitForPageToLoad(this.seleniumWait);
 
             Assert.IsFalse(this.selenium.IsElementPresent("//a[contains(@href, '/Account/Index')]"));
             Assert.IsFalse(this.selenium.IsTextPresent("Logged in as"));
             Assert.IsTrue(this.selenium.IsTextPresent("Login"));
+
+            this.Logout();
         }
     }
 }
