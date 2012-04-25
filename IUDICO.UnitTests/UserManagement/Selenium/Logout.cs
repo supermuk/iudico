3131b7a0835662a1a6b3f570ab1971b4532d25c1
yavorskyy.nestor <yavorskyy.nestor@gmail.com>
2012-04-25 18:48:29 +0000

    Fixed Selenium tests for UMngment

--- IUDICO.UnitTests/UserManagement/Selenium/Logout.cs
+++ IUDICO.UnitTests/UserManagement/Selenium/Logout.cs
@@ -1,48 +1,19 @@
 ﻿using System;
 using System.Text;
+using IUDICO.UnitTests.Base;
 using NUnit.Framework;
 using Selenium;
 
 namespace IUDICO.UnitTests.UserManagement.Selenium
 {
     [TestFixture]
-    public class Logout
+    public class Logout : SimpleWebTest
     {
-        private ISelenium selenium;
-
-        private StringBuilder verificationErrors;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            this.selenium = new DefaultSelenium("localhost", 4444, "*chrome", UpgradeSeleniumTester.browserUrl);
-            this.selenium.Start();
-            this.verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                this.selenium.Stop();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-
-            Assert.AreEqual(string.Empty, this.verificationErrors.ToString());
-        }
-
         [Test]
         public void LogoutSuccess()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
+
             this.selenium.Click("//a[contains(@href, '/Account/Logout')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             Assert.IsFalse(this.selenium.IsElementPresent("//a[contains(@href, '/Account/Index')]"));
--- IUDICO.UnitTests/UserManagement/Selenium/Users.cs
+++ IUDICO.UnitTests/UserManagement/Selenium/Users.cs
@@ -1,49 +1,20 @@
 ﻿using System;
 using System.Text;
 using System.Threading;
+using IUDICO.UnitTests.Base;
 using NUnit.Framework;
 using Selenium;
 
 namespace IUDICO.UnitTests.UserManagement.Selenium
 {
     [TestFixture]
-    public class Users
+    public class Users : SimpleWebTest
     {
-        private ISelenium selenium;
-
-        private StringBuilder verificationErrors;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            this.selenium = new DefaultSelenium("localhost", 4444, "*chrome", UpgradeSeleniumTester.browserUrl);
-            this.selenium.Start();
-            this.verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                this.selenium.Stop();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-
-            Assert.AreEqual(string.Empty, this.verificationErrors.ToString());
-        }
-
         [Test]
         public void InitTable()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
+
             this.selenium.Click("//a[contains(@href, '/User/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             Thread.Sleep(3000);
@@ -53,10 +24,8 @@ namespace IUDICO.UnitTests.UserManagement.Selenium
         [Test]
         public void InitTableWithUsers()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
+            this.DefaultLogin();
+
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             this.selenium.Click("//a[contains(@href, '/User/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
@@ -67,10 +36,8 @@ namespace IUDICO.UnitTests.UserManagement.Selenium
         [Test]
         public void InitSortableTable()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
+            this.DefaultLogin();
+
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             this.selenium.Click("//a[contains(@href, '/User/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
@@ -81,11 +48,8 @@ namespace IUDICO.UnitTests.UserManagement.Selenium
         [Test]
         public void InitSearch()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
+
             this.selenium.Click("//a[contains(@href, '/User/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             Thread.Sleep(3000);
@@ -95,11 +59,7 @@ namespace IUDICO.UnitTests.UserManagement.Selenium
         [Test]
         public void InitRolesContainer()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
             this.selenium.Click("//a[contains(@href, '/User/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             Thread.Sleep(3000);
@@ -109,11 +69,7 @@ namespace IUDICO.UnitTests.UserManagement.Selenium
         [Test]
         public void LoadUsersPage()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
             this.selenium.Click("//a[contains(@href, '/User/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             Assert.IsTrue(this.selenium.IsElementPresent("css=h2"));
