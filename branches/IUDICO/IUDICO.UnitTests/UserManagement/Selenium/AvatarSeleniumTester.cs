067682c24e0a1ec625176332ec5887b03a495eb5
yavorskyy.nestor@gmail.com <yavorskyy.nestor@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-25 18:48:29 +0000

    Fixed Selenium tests for UMngment

--- branches/IUDICO/IUDICO.UnitTests/UserManagement/Selenium/AvatarSeleniumTester.cs
+++ branches/IUDICO/IUDICO.UnitTests/UserManagement/Selenium/AvatarSeleniumTester.cs
@@ -1,40 +1,14 @@
 ﻿using System;
 using System.Text;
+using IUDICO.UnitTests.Base;
 using NUnit.Framework;
 using Selenium;
 
 namespace IUDICO.UnitTests.UserManagement.Selenium
 {
     [TestFixture]
-    public class AvatarSeleniumTester
+    public class AvatarSeleniumTester : SimpleWebTest
     {
-        private ISelenium selenium;
-
-        private StringBuilder verificationErrors;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            this.selenium = new DefaultSelenium("localhost", 4444, "*chrome", UpgradeSeleniumTester.browserUrl);
-            this.selenium.Start();
-            this.verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                this.selenium.Stop();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-
-            Assert.AreEqual(string.Empty, this.verificationErrors.ToString());
-        }
-
         /*[Test]
             public void UploadNewAvatar()
             {
@@ -76,11 +50,7 @@ namespace IUDICO.UnitTests.UserManagement.Selenium
         [Test]
         public void DisplayUserAvatar()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
             this.selenium.Click("//a[contains(@href, '/Account/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             this.selenium.Click("//a[contains(@href, '/Account/Edit')]");
--- branches/IUDICO/IUDICO.UnitTests/UserManagement/Selenium/EditAccount.cs
+++ branches/IUDICO/IUDICO.UnitTests/UserManagement/Selenium/EditAccount.cs
@@ -1,67 +1,35 @@
 ﻿using System;
 using System.Text;
+using IUDICO.UnitTests.Base;
 using NUnit.Framework;
 using Selenium;
 
 namespace IUDICO.UnitTests.UserManagement.Selenium
 {
     [TestFixture]
-    public class EditAccount
+    public class EditAccount : SimpleWebTest
     {
-        private ISelenium selenium;
-
-        private StringBuilder verificationErrors;
-
-        [SetUp]
-        public void SetupTest()
-        {
-            this.selenium = new DefaultSelenium("localhost", 4444, "*chrome", UpgradeSeleniumTester.browserUrl);
-            this.selenium.Start();
-            this.verificationErrors = new StringBuilder();
-        }
-
-        [TearDown]
-        public void TeardownTest()
-        {
-            try
-            {
-                this.selenium.Stop();
-            }
-            catch (Exception)
-            {
-                // Ignore errors if unable to close the browser
-            }
-
-            Assert.AreEqual(string.Empty, this.verificationErrors.ToString());
-        }
-
         [Test]
         public void WithValidData()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
+
             this.selenium.Click("//a[contains(@href, '/Account/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             this.selenium.Click("//a[contains(@href, '/Account/Edit')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
-            this.selenium.Type("id=Name", "nestor");
+            this.selenium.Type("id=Name", "lex");
             this.selenium.Type("id=Email", "lex@iudico.com");
             this.selenium.Click("//input[@value='Save']");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
-            Assert.IsTrue(this.selenium.IsTextPresent("nestor"));
+            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait+"0");
+            Assert.IsTrue(this.selenium.IsTextPresent("lex"));
         }
 
         [Test]
         public void WithInvalidData()
         {
-            this.selenium.Open("/");
-            this.selenium.Type("id=loginPassword", "lex");
-            this.selenium.Type("id=loginUsername", "lex");
-            this.selenium.Click("//form[contains(@action, '/Account/LoginDefault')]/input[3]");
-            this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
+            this.DefaultLogin();
+
             this.selenium.Click("//a[contains(@href, '/Account/Index')]");
             this.selenium.WaitForPageToLoad(UpgradeSeleniumTester.BrowserWait);
             this.selenium.Click("//a[contains(@href, '/Account/Edit')]");
