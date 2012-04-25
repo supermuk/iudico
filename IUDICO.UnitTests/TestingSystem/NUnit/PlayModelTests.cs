f8a64f075cd707eb75d4f02e63baa35823a4cf8e
shopen21 <shopen21@gmail.com>
2012-04-25 22:06:10 +0000

    fixed Unit test;

--- IUDICO.UnitTests/TestingSystem/NUnit/PlayModelTests.cs
+++ IUDICO.UnitTests/TestingSystem/NUnit/PlayModelTests.cs
@@ -12,16 +12,21 @@ namespace IUDICO.UnitTests.TestingSystem.NUnit
         [SetUp]
         public void PlayModelTestsSetUp()
         {
-            playModel = new PlayModel {AttemptId = 12345, CurriculumChapterTopicId = 1, TopicType = TopicTypeEnum.TestWithoutCourse};
+            this.playModel = new PlayModel
+                {
+                    AttemptId = 12345,
+                    CurriculumChapterTopicId = 12,
+                    TopicType = TopicTypeEnum.TestWithoutCourse
+                };
         }
 
         [Test]
         public void PlayModelPropertiesTest()
         {
-            PlayModelTestsSetUp();
-            Assert.AreEqual(playModel.AttemptId, 12345);
-            Assert.AreEqual(playModel.CurriculumChapterTopicId, 1);
-            Assert.AreSame(playModel.TopicType, TopicTypeEnum.TestWithoutCourse);
+            this.PlayModelTestsSetUp();
+            Assert.AreEqual(this.playModel.AttemptId, 12345);
+            Assert.AreEqual(this.playModel.CurriculumChapterTopicId, 12);
+            Assert.AreEqual(this.playModel.TopicType, TopicTypeEnum.TestWithoutCourse);
         }
     }
 }
