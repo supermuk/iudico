76d7641c5a44210358ecc72b25b1a7a6ab497cb5
O.Holod <O.Holod@gmail.com>
2012-04-28 18:24:32 +0000

    Fixes to selenium tests for TestingSystem.

---
+++ IUDICO.UnitTests/TestingSystem/Selenium/ExSelenium.cs
@@ -0,0 +1,47 @@
+﻿namespace IUDICO.UnitTests.TestingSystem.Selenium
+{
+    using System;
+    using System.Threading;
+
+    using global::Selenium;
+
+    public static class ExSelenium
+    {
+        public static ISelenium Selenium { get; set; }
+
+        public static string Timeout { get; set; }
+
+        public static bool IsSeleniumStarted { get; private set; }
+
+        static ExSelenium()
+        {
+            IsSeleniumStarted = false;
+        }
+
+        public static void StartSelenium()
+        {
+            Selenium.Start();
+            IsSeleniumStarted = true;
+        }
+
+        public static void StopSelenium()
+        {
+            Selenium.Stop();
+            IsSeleniumStarted = false;
+        }
+
+        public static void WaitForElement(string locator)
+        {
+            int sleepTime = 0;
+            while (!Selenium.IsElementPresent(locator))
+            {
+                if (sleepTime > Convert.ToInt32(Timeout))
+                {
+                    return;
+                }
+                Thread.Sleep(100);
+                sleepTime += 100;
+            }
+        }
+    }
+}
