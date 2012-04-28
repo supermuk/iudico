3aa03088326ec633f2654dd78ac255cdd61eb255
vladykx <vladykx@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-28 09:53:38 +0000

    UserManagement tests using CachedStorage Refactored Search

---
+++ IUDICO.Search/Models/Definitions/CourseDefinition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using IUDICO.Common.Models.Shared;
+using IUDICO.Search.Models.Queries;
+
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+
+namespace IUDICO.Search.Models.Definitions
+{
+    public class CourseDefinition : Definition<Course>
+    {
+        public CourseDefinition()
+        {
+            this.Query = new DefaultQuery(new string[] {"Course"});
+        }
+
+        public override Document Convert(Course entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Term GetIndex(Course entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Course Convert(Document document)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IUDICO.Search/Models/Definitions/GroupDefinition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using IUDICO.Common.Models.Shared;
+using IUDICO.Search.Models.Queries;
+
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+
+using SimpleLucene;
+
+namespace IUDICO.Search.Models.Definitions
+{
+    public class GroupDefinition : Definition<Group>
+    {
+        public GroupDefinition()
+        {
+            this.Query = new GroupQuery();
+        }
+
+        public override Document Convert(Group entity)
+        {
+            var document = new Document();
+
+            document.Add(new Field("ID", entity.Id.ToString(), Field.Store.YES, Field.Index.ANALYZED));
+            document.Add(new Field("Group", entity.Name, Field.Store.YES, Field.Index.ANALYZED, Field.TermVector.YES));
+
+            return document;
+        }
+
+        public override Term GetIndex(Group entity)
+        {
+            return new Term("id", entity.Id.ToString());
+        }
+
+        public override Group Convert(Document document)
+        {
+            var group = new Group
+                {
+                    Id = int.Parse(document.Get("ID")),
+                    Name = document.Get("Group")
+                };
+
+            return group;
+        }
+    }
+}
