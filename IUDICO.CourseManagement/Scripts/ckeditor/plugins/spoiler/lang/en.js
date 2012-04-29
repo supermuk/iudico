a930b7591f4bd7a9e4acdb35719a319f0845c5ac
terminadoor <terminadoor@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-29 18:03:49 +0000

    Spoiler plugin localization fix

---
+++ IUDICO.CourseManagement/Scripts/ckeditor/plugins/spoiler/lang/en.js
@@ -0,0 +1,12 @@
+CKEDITOR.plugins.setLang("spoiler", "en",
+    {
+     spoiler:
+     {
+           SpoilerButtonTitle:"Add Spoiler",
+           SpoilerTitle: "Spoiler",
+           SpoilerContentsTitle:"First Tab",
+           SpoilerElementsLabel:"Enter spoiler title:",
+           SpoilerWarning:"Spoiler title cannot be empty!"
+
+     }
+});
