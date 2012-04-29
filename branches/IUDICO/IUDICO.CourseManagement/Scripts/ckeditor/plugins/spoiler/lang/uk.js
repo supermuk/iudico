5afc6a136f0f18cb42c04c7734ff03c3a0c1d0d8
terminadoor <terminadoor@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-29 18:03:49 +0000

    Spoiler plugin localization fix

---
+++ branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/plugins/spoiler/lang/uk.js
@@ -0,0 +1,12 @@
+﻿CKEDITOR.plugins.setLang("spoiler", "uk",
+    {
+        spoiler:
+     {
+         SpoilerButtonTitle: "Додати підпис",
+         SpoilerTitle: "Підпис",
+         SpoilerContentsTitle: "Перша вкладка",
+         SpoilerElementsLabel: "Текст підпису",
+         SpoilerWarning: "Текст Підпису не можу бути пустим!"
+
+     }
+    });
