6f126703f7a8478300dd86ffdc2153079fe05aa4
yavorskyy.nestor@gmail.com <yavorskyy.nestor@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-04-30 19:45:33 +0000

    Almost done Discipline/Topic Quality.

---
+++ branches/IUDICO/IUDICO.Analytics/Controllers/QualityController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IUDICO.Analytics.Models.Quality;
+using IUDICO.Common.Controllers;
+using IUDICO.Common.Models;
+using IUDICO.Common.Models.Attributes;
+
+namespace IUDICO.Analytics.Controllers
+{
+    public class QualityController : PluginController
+    {
+        [Allow(Role = Role.Teacher)]
+        public ActionResult Index()
+        {
+            DisciplineModel model = new DisciplineModel(LmsService);
+            return View(model);
+        }
+        
+        [Allow(Role = Role.Teacher)]
+        [HttpPost]
+        public ActionResult SelectDiscipline(long selectDisciplineId)
+        {
+            TopicModel model = new TopicModel(LmsService, selectDisciplineId);
+            return View(model);
+        }
+
+    }
+}
