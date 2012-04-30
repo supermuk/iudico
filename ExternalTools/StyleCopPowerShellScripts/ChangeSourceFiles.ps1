270f41f418099e7143acda353c6af4f736553ae1
O.Holod <O.Holod@gmail.com>
2012-04-30 19:39:14 +0000

    Changes to powershell scripts that generates StyleCop reports.

--- ExternalTools/StyleCopPowerShellScripts/ChangeSourceFiles.ps1
+++ ExternalTools/StyleCopPowerShellScripts/ChangeSourceFiles.ps1
@@ -1,5 +1,10 @@
 $original_file = 'FullStyleCopViolations.xml'
 $destination_file =  'FullStyleCopViolationsModified.xml'
+$currentDirectory = Get-Location
+$currentPath = $currentDirectory.Path
+$currentPath = $currentPath.Replace('\', '\\')
+$currentPath = $currentPath += '\\'
+echo $currentPath
 (Get-Content $original_file) | Foreach-Object {
-    $_ -replace 'C:\\Users\\Administrator\\.hudson\\jobs\\IUDICO\\workspace\\', ''
+    $_ -replace $currentPath, ''
     } | Set-Content $destination_file
