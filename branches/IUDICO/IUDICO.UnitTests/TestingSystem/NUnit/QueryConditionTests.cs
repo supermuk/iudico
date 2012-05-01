3890200f2defd73478d6590fea33beaa02111279
shopen21 <shopen21@gmail.com>
2012-05-01 14:45:42 +0000

    fixes, refactoring and styling to Stats and Testing;

---
+++ branches/IUDICO/IUDICO.UnitTests/TestingSystem/NUnit/QueryConditionTests.cs
@@ -0,0 +1,57 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="QueryConditionTests.cs" company="">
+// 
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace IUDICO.UnitTests.TestingSystem.NUnit
+{
+    using System;
+    using global::NUnit.Framework;
+    using IUDICO.TestingSystem.Models.VOs;
+    using Microsoft.LearningComponents.Storage;
+
+    /// <summary>
+    /// Unit tests for <see cref="QueryCondition"/>.
+    /// </summary>
+    [TestFixture]
+    public class QueryConditionTests
+    {
+        [Test]
+        public void CreateQueryConditionWithDefaultConstructor()
+        {
+            var queryCondition = new QueryCondition();
+
+            Assert.IsNull(queryCondition.ColumnName);
+            Assert.IsNull(queryCondition.Value);
+            Assert.AreEqual(LearningStoreConditionOperator.Equal, queryCondition.ConditionOperator);
+        }
+
+        [Test]
+        public void CreateQueryConditionWithFewParameters()
+        {
+            const string ColumnName = "some column";
+            var obj = TimeZone.CurrentTimeZone;
+
+            var queryCondition = new QueryCondition(ColumnName, obj);
+
+            Assert.AreEqual(ColumnName, queryCondition.ColumnName);
+            Assert.AreSame(obj, queryCondition.Value);
+            Assert.AreEqual(LearningStoreConditionOperator.Equal, queryCondition.ConditionOperator);
+        }
+
+        [Test]
+        public void CreateQueryConditionWithAllParameters()
+        {
+            const string ColumnName = "some column 2";
+            const LearningStoreConditionOperator Condition = LearningStoreConditionOperator.LessThan;
+            var obj = TimeZone.CurrentTimeZone;
+
+            var queryCondition = new QueryCondition(ColumnName, Condition, obj);
+
+            Assert.AreEqual(ColumnName, queryCondition.ColumnName);
+            Assert.AreEqual(Condition, queryCondition.ConditionOperator);
+            Assert.AreSame(obj, queryCondition.Value);
+        }
+    }
+}
---
+++ branches/IUDICO/IUDICO.UnitTests/TestingSystem/NUnit/TrainingControllerTests.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TrainingControllerTests.cs" company="">
+// 
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace IUDICO.UnitTests.TestingSystem.NUnit
+{
+    using System;
+
+    using IUDICO.Common.Models.Services;
+    using IUDICO.TestingSystem.Controllers;
+
+    using Moq;
+
+    using global::NUnit.Framework;
+
+    /// <summary>
+    /// This is a bunch of tests for <see cref="TrainingController"/>
+    /// </summary>
+    [TestFixture]
+    public class TrainingControllerTests
+    {
+        [Test]
+        public void PassNotExistingCourseIdToPlayAction()
+        {
+            var curriculumServiceMock = new Mock<ICurriculumService>(MockBehavior.Strict);
+
+            var courseServiceMock = new Mock<ICourseService>(MockBehavior.Strict);
+
+            var lmsServiceMock = new Mock<ILmsService>(MockBehavior.Strict);
+
+            lmsServiceMock.Setup(mock => mock.FindService<ICurriculumService>()).Returns(curriculumServiceMock.Object);
+            lmsServiceMock.Setup(mock => mock.FindService<ICourseService>()).Returns(courseServiceMock.Object);
+
+            // TODO: finish test
+        }
+    }
+}
