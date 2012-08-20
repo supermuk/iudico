8f2c344c3e76ca28f29fff79c8b4fadb490207e8
akatsyubka <akatsyubka@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-08-20 10:13:47 +0000

    fixing... fixing...

--- IUDICO.UnitTests/DataGenerator/NUnit/CourseGenerationTest.cs
+++ IUDICO.UnitTests/DataGenerator/NUnit/CourseGenerationTest.cs
@@ -12,80 +12,80 @@ using IUDICO.Common.Models.Caching;
 
 namespace IUDICO.UnitTests.DataGenerator.NUnit
 {
-	[TestFixture]
-	public class CourseGenerationTest
-	{
-		protected FakeCourseStorage tests = FakeCourseStorage.GetInstance();
-		protected FakeCacheProvider cache = FakeCacheProvider.GetInstance();
-		protected string path = Path.Combine(ConfigurationManager.AppSettings.Get("PathToIUDICO.UnitTests"), @"IUDICO.UnitTests\DataGenerator\Data\Courses\Pascal");
+   [TestFixture]
+   public class CourseGenerationTest
+   {
+      protected FakeCourseStorage tests = FakeCourseStorage.GetInstance();
+      protected FakeCacheProvider cache = FakeCacheProvider.GetInstance();
+      protected string path = Path.Combine(ConfigurationManager.AppSettings.Get("PathToIUDICO.UnitTests"), @"IUDICO.UnitTests\DataGenerator\Data\Courses\Pascal");
 
-		protected ICourseStorage Storage
-		{
-			get
-			{
-				return this.tests.Storage;
-			}
-		}
+      protected ICourseStorage Storage
+      {
+         get
+         {
+            return this.tests.Storage;
+         }
+      }
 
-		protected ICacheProvider CacheProvider
-		{
-			get
-			{
-				return this.cache.CacheProvider;
-			}
-		}
+      protected ICacheProvider CacheProvider
+      {
+         get
+         {
+            return this.cache.CacheProvider;
+         }
+      }
 
-		[SetUp]
-		public void Init()
-		{
-			this.tests.ClearTables();
-		}
+      [SetUp]
+      public void Init()
+      {
+         this.tests.ClearTables();
+      }
 
-		[TearDown]
-		public void OnTearDown()
-		{
-			Storage.DeleteCourses(Storage.GetCourses().Select(c => c.Id).ToList());
-			this.DeleteFiles();
-		}
+      [TearDown]
+      public void OnTearDown()
+      {
+         Storage.DeleteCourses(Storage.GetCourses().Select(c => c.Id).ToList());
+         this.DeleteFiles();
+      }
 
-		private void DeleteFiles()
-		{
-			var files = Directory.GetFiles(Path.Combine(ConfigurationManager.AppSettings.Get("PathToIUDICO.UnitTests"), @"IUDICO.UnitTests\DataGenerator\Data\Tests"), "*.zip");
+      private void DeleteFiles()
+      {
+         var files = Directory.GetFiles(Path.Combine(ConfigurationManager.AppSettings.Get("PathToIUDICO.UnitTests"), @"IUDICO.UnitTests\DataGenerator\Data\Tests"), "*.zip");
 
-			foreach (var file in files)
-			{
-				File.Delete(file);
-			}
-		}
+         foreach (var file in files)
+         {
+            File.Delete(file);
+         }
+      }
 
-		[Test]
-		public void Test1()
-		{
-			var files = Directory.GetFiles(path, "*.zip");
+      [Test]
+      public void Test1()
+      {
+         var files = Directory.GetFiles(path, "*.zip");
 
-			foreach (var file in files)
-			{
-				var name = Path.GetFileNameWithoutExtension(file);
+         foreach (var file in files)
+         {
+            var name = Path.GetFileNameWithoutExtension(file);
 
-				Assert.AreEqual(0, Storage.GetCourses().Where(c => c.Name == name && c.Owner == "OlehVukladachenko").Count());
-			}
+            Assert.AreEqual(0, Storage.GetCourses().Where(c => c.Name == name && c.Owner == "OlehVukladachenko").Count());
+         }
 
-			CourseGenerator.PascalCourse(Storage, CacheProvider, path);
-			foreach (var file in files)
-			{
-				var name = Path.GetFileNameWithoutExtension(file);
+         CourseGenerator.PascalCourse(Storage, CacheProvider, path);
+         foreach (var file in files)
+         {
+            var name = Path.GetFileNameWithoutExtension(file);
 
-				Assert.AreEqual(1, Storage.GetCourses().Where(c => c.Name == name && c.Owner == "OlehVukladachenko").Count());
-			}
-			this.DeleteFiles();
-			CourseGenerator.PascalCourse(Storage, CacheProvider, path);
-			foreach (var file in files)
-			{
-				var name = Path.GetFileNameWithoutExtension(file);
+            Assert.AreEqual(1, Storage.GetCourses().Where(c => c.Name == name && c.Owner == "OlehVukladachenko").Count());
+         }
+         this.DeleteFiles();
+         CourseGenerator.PascalCourse(Storage, CacheProvider, path);
+         foreach (var file in files)
+         {
+            var name = Path.GetFileNameWithoutExtension(file);
 
-				Assert.AreEqual(1, Storage.GetCourses().Where(c => c.Name == name && c.Owner == "OlehVukladachenko").Count());
-			}
-		}
+            Assert.AreEqual(1, Storage.GetCourses().Where(c => c.Name == name && c.Owner == "OlehVukladachenko").Count());
+         }
+      }
 
-	}
+   }
 }
--- IUDICO.UnitTests/DataGenerator/NUnit/DisciplineGenerationTest.cs
+++ IUDICO.UnitTests/DataGenerator/NUnit/DisciplineGenerationTest.cs
@@ -12,34 +12,34 @@ using NUnit.Framework;
 
 namespace IUDICO.UnitTests.DataGenerator.NUnit
 {
-	public class DisciplineGenerationTest
-	{
-		private FakeDisciplineStorage tests = FakeDisciplineStorage.GetInstance();
-		protected string path = Path.Combine(ConfigurationManager.AppSettings.Get("PathToIUDICO.UnitTests"), @"IUDICO.UnitTests\DataGenerator\Data\Courses\Pascal\Pascal.disc");
+   public class DisciplineGenerationTest
+   {
+      private FakeDisciplineStorage tests = FakeDisciplineStorage.GetInstance();
+      protected string path = Path.Combine(ConfigurationManager.AppSettings.Get("PathToIUDICO.UnitTests"), @"IUDICO.UnitTests\DataGenerator\Data\Courses\Pascal\Pascal.disc");
 
-		public IDisciplineStorage Storage
-		{
-			get
-			{
-				return this.tests.CachedStorage;
-			}
-		}
+      public IDisciplineStorage Storage
+      {
+         get
+         {
+            return this.tests.CachedStorage;
+         }
+      }
 
-		[SetUp]
-		public void Init()
-		{
-			tests.ClearTables();
-		}
+      [SetUp]
+      public void Init()
+      {
+         tests.ClearTables();
+      }
 
-		[Test]
-	   [Ignore]
-		public void Test1()
-		{
-			Assert.AreEqual(0,Storage.GetDisciplines().Where(d=>d.Name == "Pascal" && d.Owner == "OlehVukladachenko").Count());
+      [Test]
+      [Ignore]
+      public void Test1()
+      {
+         Assert.AreEqual(0, Storage.GetDisciplines().Where(d => d.Name == "Pascal" && d.Owner == "OlehVukladachenko").Count());
 
-			DisciplineGenerator.PascalDiscipline(Storage, path);
+         DisciplineGenerator.PascalDiscipline(Storage, path);
 
-			Assert.AreEqual(1, Storage.GetDisciplines().Where(d => d.Name == "Pascal" && d.Owner == "OlehVukladachenko").Count());
-		}
-	}
+         Assert.AreEqual(1, Storage.GetDisciplines().Where(d => d.Name == "Pascal" && d.Owner == "OlehVukladachenko").Count());
+      }
+   }
 }
