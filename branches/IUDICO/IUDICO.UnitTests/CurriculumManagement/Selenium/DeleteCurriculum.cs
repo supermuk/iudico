2ee427ad3bc09dbbd2d6329f105f7212d35e691a
katryse4k@gmail.com <katryse4k@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-10-25 21:03:20 +0000

    add selenium tests for CurriculumManagement + modified Analytics

---
+++ branches/IUDICO/IUDICO.UnitTests/CurriculumManagement/Selenium/DeleteCurriculum.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Threading;
+using IUDICO.UnitTests.Base;
+using NUnit.Framework;
+
+namespace IUDICO.UnitTests.CurriculumManagement.Selenium
+{
+    /// <summary>
+    /// Summary description for DeleteCurriculum
+    /// </summary>
+    [TestFixture]
+    public class DeleteCurriculum : SimpleWebTest
+    {
+        private const int SleepTime = 8000;
+
+        /// <summary>
+        /// Author - Mariana Khljebyk
+        /// </summary>
+        [Test]
+        public void DeleteCurriculumTest()
+        {
+            this.DefaultLogin("prof", "prof");
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@onclick, 'addDiscipline();')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Type("id=Name", "Discipline2");
+            this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/Curriculum')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@href,'/Curriculum/Create')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Select("id=DisciplineId", "label=Discipline2");
+            this.selenium.Select("id=GroupId", "value=1");
+            this.selenium.Click("xpath=(//input[@value='Create'])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            var isPresent = this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Демонстраційна група')]")
+                && this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Discipline2')]");
+
+            this.selenium.Click("xpath=//tr[contains(.,'Discipline2')]//input");
+            this.selenium.Click("//a[contains(@id, 'DeleteMany')]");
+            this.selenium.GetConfirmation();
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//table[@id='disciplines']//tr[contains(.,'Discipline2')]//a[contains(text(),'Delete')]");
+            this.selenium.GetConfirmation();
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+            Thread.Sleep(SleepTime);
+            Assert.IsFalse(this.selenium.IsElementPresent("xpath=//tr[contains(.,'Discipline2')]"));
+
+            try
+            {
+                this.Logout();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        /// <summary>
+        /// Author - Mariana Khljebyk
+        /// </summary>
+        [Test]
+        public void DeleteCurriculumTestWithTimeline()
+        {
+            this.DefaultLogin("prof", "prof");
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@onclick, 'addDiscipline();')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Type("id=Name", "Discipline3");
+            this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/Curriculum')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@href,'/Curriculum/Create')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Select("id=DisciplineId", "label=Discipline3");
+            this.selenium.Select("id=GroupId", "value=1");
+
+            this.selenium.Click("xpath=(//input[@name='SetTimeline'])");
+            this.selenium.Click("xpath=(//input[@value='Create'])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            var isPresent = this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Демонстраційна група')]")
+                && this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Discipline3')]");
+
+            this.selenium.Click("xpath=//tr[contains(.,'Discipline3')]//input");
+            this.selenium.Click("//a[contains(@id, 'DeleteMany')]");
+            this.selenium.GetConfirmation();
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//table[@id='disciplines']//tr[contains(.,'Discipline3')]//a[contains(text(),'Delete')]");
+            this.selenium.GetConfirmation();
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+            Thread.Sleep(SleepTime);
+            Assert.IsFalse(this.selenium.IsElementPresent("xpath=//tr[contains(.,'Discipline3')]"));
+
+            try
+            {
+                this.Logout();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        /// <summary>
+        /// Author - Mariana Khljebyk
+        /// </summary>
+        [Test]
+        public void DeleteCurriculumTestWithEditStartDate()
+        {
+            this.DefaultLogin("prof", "prof");
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@onclick, 'addDiscipline();')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Type("id=Name", "Discipline1");
+            this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/Curriculum')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@href,'/Curriculum/Create')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Select("id=DisciplineId", "label=Discipline1");
+            this.selenium.Select("id=GroupId", "value=1");
+
+            this.selenium.Click("xpath=(//input[@name='SetTimeline'])");
+            this.selenium.Type("id=StartDate", "07/20/2012 9:52 AM");
+
+            this.selenium.Click("xpath=(//input[@value='Create'])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            var isPresent = this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Демонстраційна група')]")
+                && this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Discipline1')]");
+
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+
+            this.selenium.Click("//table[@id='disciplines']//tr[contains(.,'Discipline1')]//a[contains(text(),'Delete')]");
+
+            this.selenium.GetConfirmation();
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+            Thread.Sleep(SleepTime);
+            Assert.IsFalse(this.selenium.IsElementPresent("xpath=//tr[contains(.,'Discipline1')]"));
+
+            try
+            {
+                this.Logout();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        /// <summary>
+        /// Author - Mariana Khljebyk
+        /// </summary>
+        [Test]
+        public void DeleteCurriculumTestWithEditEndDate()
+        {
+            this.DefaultLogin("prof", "prof");
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@onclick, 'addDiscipline();')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Type("id=Name", "Discipline1");
+            this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/Curriculum')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@href,'/Curriculum/Create')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Select("id=DisciplineId", "label=Discipline1");
+            this.selenium.Select("id=GroupId", "value=1");
+
+            this.selenium.Click("xpath=(//input[@name='SetTimeline'])");
+
+            this.selenium.Type("id=EndDate", "10/29/2012 08:10 AM");
+            this.selenium.Click("xpath=(//input[@value='Create'])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            var isPresent = this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Демонстраційна група')]")
+                && this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Discipline1')]");
+
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+
+            this.selenium.Click("//table[@id='disciplines']//tr[contains(.,'Discipline1')]//a[contains(text(),'Delete')]");
+
+            this.selenium.GetConfirmation();
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+            Thread.Sleep(SleepTime);
+            Assert.IsFalse(this.selenium.IsElementPresent("xpath=//tr[contains(.,'Discipline1')]"));
+
+            try
+            {
+                this.Logout();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+
+        /// <summary>
+        /// Author - Mariana Khljebyk
+        /// </summary>
+        [Test]
+        public void DeleteCurriculumTestWithEditTimeline()
+        {
+            this.DefaultLogin("prof", "prof");
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@onclick, 'addDiscipline();')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Type("id=Name", "Discipline1");
+            this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/Curriculum')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            this.selenium.Click("//a[contains(@href,'/Curriculum/Create')]");
+            Thread.Sleep(SleepTime);
+            this.selenium.Select("id=DisciplineId", "label=Discipline1");
+            this.selenium.Select("id=GroupId", "value=1");
+
+            this.selenium.Click("xpath=(//input[@name='SetTimeline'])");
+            this.selenium.Type("id=StartDate", "10/20/2012 9:52 AM");
+            this.selenium.Type("id=EndDate", "10/27/2012 9:52 AM");
+            this.selenium.Click("xpath=(//input[@value='Create'])");
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            var isPresent = this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Демонстраційна група')]")
+                && this.selenium.IsElementPresent("//table[@id='curriculumsTable']//tr[contains(.,'Discipline1')]");
+
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+
+            this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+
+            this.selenium.Click("//table[@id='disciplines']//tr[contains(.,'Discipline1')]//a[contains(text(),'Delete')]");
+
+            this.selenium.GetConfirmation();
+            this.selenium.Refresh();
+            this.selenium.WaitForPageToLoad(this.SeleniumWait);
+            while (selenium.IsAlertPresent())
+            {
+                selenium.GetAlert();
+            }
+            Thread.Sleep(SleepTime);
+            Assert.IsFalse(this.selenium.IsElementPresent("xpath=//tr[contains(.,'Discipline1')]"));
+
+            try
+            {
+                this.Logout();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+        }
+    }
+}
