1c120efb6b1bca95be64496eb2c84c312df73255
radomba@gmail.com <radomba@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-11-11 13:51:54 +0000

    Added Calibri font to CKEditor

--- branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/config.js
+++ branches/IUDICO/IUDICO.CourseManagement/Scripts/ckeditor/config.js
@@ -1,4 +1,4 @@
-﻿/*
+ /*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -11,6 +11,17 @@ CKEDITOR.editorConfig = function (config) {
     // config.uiColor = '#AADC6E';
 
     config.extraPlugins = 'iudico,save,syntaxhighlight,spoiler,switchtoolbar';
+    config.font_names =
+            'Arial/Arial, Helvetica, sans-serif;' +
+            'Comic Sans MS/Comic Sans MS, cursive;' +
+            'Courier New/Courier New, Courier, monospace;' +
+            'Georgia/Georgia, serif;' +
+            'Lucida Sans Unicode/Lucida Sans Unicode, Lucida Grande, sans-serif;' +
+            'Tahoma/Tahoma, Geneva, sans-serif;' +
+            'Times New Roman/Times New Roman, Times, serif;' +
+            'Trebuchet MS/Trebuchet MS, Helvetica, sans-serif;' +
+            'Calibri/Calibri, Verdana, Geneva, sans-serif;' + /* font added on demand */
+            'Verdana/Verdana, Geneva, sans-serif';
 
     CKEDITOR.config.toolbar_IUDICO =
     [
