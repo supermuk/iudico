f92bf068cf3e3b97501e6250e334a94c44e7664a
yarkip <yarkip@gmail.com>
2012-12-10 20:53:31 +0000

    Added/fixed tests for UserManagement

--- IUDICO.UnitTests/UserManagement/NUnit/ChangePassword.cs
+++ IUDICO.UnitTests/UserManagement/NUnit/ChangePassword.cs
@@ -5,6 +5,8 @@ using NUnit.Framework;
 
 namespace IUDICO.UnitTests.UserManagement.NUnit
 {
+    using System;
+
     [TestFixture]
     public class ChangePassword
     {
@@ -23,9 +25,77 @@ namespace IUDICO.UnitTests.UserManagement.NUnit
             this.tests.Storage.ChangePassword(model);
 
             Assert.AreEqual(
-                this.tests.Storage.GetUser("name").Password, 
+                this.tests.Storage.GetUser("name").Password,
                 this.tests.Storage.EncryptPassword("321"));
             this.tests.Storage.DeleteUser(u => u.Username == "name");
+        }
+        
+        /// <summary>
+        /// Incorrect old password.
+        /// </summary>
+        [Test]
+        public void ChangePasswordWithInvalidDataTest()
+        {
+            this.tests = UserManagementTests.Update();
+            var model = new ChangePasswordModel { OldPassword = "111", ConfirmPassword = "321", NewPassword = "321" };
+            var temp = new User { Username = "name", Email = "mail@mail.com", Password = "123" };
+
+            this.tests.ChangeCurrentUser("panza");
+            this.tests.Storage.CreateUser(temp);
+            this.tests.ChangeCurrentUser("name");
+
+            try
+            {
+                this.tests.Storage.ChangePassword(model);
+            }
+            catch (Exception e)
+            {
+                this.tests.Storage.DeleteUser(u => u.Username == "name");
+
+                if (e.Message == "Old password is wrong.")
+                {
+                    Assert.Pass();
+                }
+
+                Assert.Fail();
+            }
+
+            this.tests.Storage.DeleteUser(u => u.Username == "name");
+            Assert.Fail();
+        }
+
+        /// <summary>
+        /// Empty old password.
+        /// </summary>
+        [Test]
+        public void ChangePasswordWithBlankDataTest()
+        {
+            this.tests = UserManagementTests.Update();
+            var model = new ChangePasswordModel { OldPassword = string.Empty, ConfirmPassword = "321", NewPassword = "321" };
+            var temp = new User { Username = "name", Email = "mail@mail.com", Password = "123" };
+
+            this.tests.ChangeCurrentUser("panza");
+            this.tests.Storage.CreateUser(temp);
+            this.tests.ChangeCurrentUser("name");
+
+            try
+            {
+                this.tests.Storage.ChangePassword(model);
+            }
+            catch (Exception e)
+            {
+                this.tests.Storage.DeleteUser(u => u.Username == "name");
+
+                if (e.Message == "Old password can't be empty.")
+                {
+                    Assert.Pass();
+                }
+
+                Assert.Fail();
+            }
+
+            this.tests.Storage.DeleteUser(u => u.Username == "name");
+            Assert.Fail();
         }
 
         //[Test]
