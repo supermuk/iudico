26767f1c49b26c5d83eea3b79aed708805a6b4fc
chopvvs@gmail.com <chopvvs@gmail.com@a0fdb904-a088-11dd-a874-8d3a81574e67>
2012-12-10 21:42:07 +0000

    Added new tests for Discipline Management
    NUnit:
    - ExportTest
    - ImportTest
    
    Selenium:
    - CreateDisciplineWithChapterAndTopicTest

--- branches/IUDICO/IUDICO.UnitTests/CurriculumManagement/Selenium/CreateDiscipline.cs
+++ branches/IUDICO/IUDICO.UnitTests/CurriculumManagement/Selenium/CreateDiscipline.cs
@@ -124,5 +124,78 @@ namespace IUDICO.UnitTests.CurriculumManagement.Selenium
                 // Ignore errors if unable to close the browser
             }
         }
+		[Test]
+		public void CreateDisciplineWithChapterAndTopicTest()
+		{
+			this.DefaultLogin("prof", "prof");
+
+			this.selenium.Click("//a[contains(@href,'/DisciplineAction')]");
+			this.selenium.WaitForPageToLoad(this.SeleniumWait);
+			var isPresent = this.selenium.IsElementPresent("//table[@id='disciplines']");
+			Assert.IsTrue(isPresent);
+
+			isPresent = this.selenium.IsElementPresent("//table[@id='disciplines']//tr[contains(.,'MyNewDiscipline')]");
+			if (isPresent)
+			{
+				this.selenium.Click(
+					"//table[@id='disciplines']//tr[contains(.,'MyNewDiscipline')]//a[contains(text(),'Delete')]");
+				this.selenium.GetConfirmation();
+				this.selenium.Refresh();
+				this.selenium.WaitForPageToLoad(this.SeleniumWait);
+				while (selenium.IsAlertPresent())
+				{
+					selenium.GetAlert();
+				}
+				Thread.Sleep(SleepTime);
+			}
+
+			this.selenium.Click("//a[contains(@onclick, 'addDiscipline();')]");
+			Thread.Sleep(SleepTime);
+			this.selenium.Type("id=Name", "MyNewDiscipline");
+			this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+			this.selenium.Refresh();
+			this.selenium.WaitForPageToLoad(this.SeleniumWait);
+			while (selenium.IsAlertPresent())
+			{
+				selenium.GetAlert();
+			}
+			Thread.Sleep(SleepTime);
+
+			isPresent =
+				this.selenium.IsElementPresent("//table[@id='disciplines']//tr//td[contains(.,'MyNewDiscipline')]");
+			Assert.IsTrue(isPresent);
+
+			this.selenium.Click(
+					"//table[@id='disciplines']//tr[contains(.,'MyNewDiscipline')]//a[contains(text(),'Add chapter')]");
+			Thread.Sleep(SleepTime);
+
+			this.selenium.Type("id=Name", "MyNewChapter");
+			this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+			Thread.Sleep(SleepTime);
+
+			isPresent =
+				this.selenium.IsElementPresent("//table[@id='disciplines']//tr//td[contains(.,'MyNewChapter')]");
+			Assert.IsTrue(isPresent);
+
+			this.selenium.Click(
+					"//table[@id='disciplines']//tr[contains(.,'MyNewChapter')]//a[contains(text(),'Add topic')]");
+			this.selenium.Type("id=TopicName", "MyNewTopic");
+			this.selenium.Select("id=TestCourseId", "Test without course");
+			this.selenium.Click("xpath=(//div[contains(@class,'ui-dialog-buttonset')]//button[1])");
+			Thread.Sleep(SleepTime);
+
+			isPresent =
+				this.selenium.IsElementPresent("//table[@id='disciplines']//tr//td[contains(.,'MyNewTopic')]");
+			Assert.IsTrue(isPresent);
+			
+			try
+			{
+				this.Logout();
+			}
+			catch (Exception)
+			{
+				// Ignore errors if unable to close the browser
+			}
+		}
     }
 }
