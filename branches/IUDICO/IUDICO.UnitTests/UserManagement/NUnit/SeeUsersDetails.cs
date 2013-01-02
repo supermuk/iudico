ac96c1230a3a7d9daf93cb02dc215e2d2e5d722f
yarkip <yarkip@gmail.com>
2013-01-02 19:28:42 +0000

    Fixed tests for UserManagement.

--- branches/IUDICO/IUDICO.UnitTests/UserManagement/NUnit/SeeUsersDetails.cs
+++ branches/IUDICO/IUDICO.UnitTests/UserManagement/NUnit/SeeUsersDetails.cs
@@ -14,14 +14,19 @@ namespace IUDICO.UnitTests.UserManagement.NUnit
             this.tests = new UserManagementTests();
         }
 
+        /// <summary>
+        /// Fixed - Yarema Kipetskiy
+        /// </summary>
         [Test]
-        public void GetUserExisting()
+        public void GetUserExistingTest()
         {
+            // Creating new user.
             var temp = new User { Username = "name1", Email = "mail@mail.com", Password = "123" };
-
             this.tests.Storage.CreateUser(temp);
 
+            // Getting user data of created user from storage.
             var user = this.tests.Storage.GetUser(u => u.Username == "name1");
+            // Verifying if user data is correct.
             Assert.AreEqual(temp.Username, user.Username);
             Assert.AreEqual(temp.Email, user.Email);
             Assert.AreEqual(temp.Password, user.Password);
